8fb513f295b0179cd57a7f51b7ff90d304391fd6
Yifan Sun <sunyifan112358@gmail.com>
2017-02-24 01:20:00 -0500

    Adding instructin and lookup of the decode table

--- disasm/disassembler.go
+++ disasm/disassembler.go
@@ -26,6 +26,7 @@ const (
 	mimg
 	exp
 	flat
+	formatTypeCount
 )
 
 type formatType int
@@ -69,39 +70,44 @@ func newDecodeTable() *decodeTable {
 
 // Disassembler is the unit that can decode .hsaco file
 type Disassembler struct {
-	formatTable []Format
+	formatTable map[formatType]*Format
 
 	// Maps from the format to table
-	decodeTables map[formatType]decodeTable
+	decodeTables map[formatType]*decodeTable
 }
 
 func (d *Disassembler) addInstType(info *InstType) {
+	if d.decodeTables[info.Format.formatType] == nil {
+		d.decodeTables[info.Format.formatType] = newDecodeTable()
+	}
 	d.decodeTables[info.Format.formatType].insts[info.Opcode] = info
 }
 
 func (d *Disassembler) initializeFormatTable() {
-	d.formatTable = make([]Format, 0)
-	d.formatTable = append(d.formatTable, Format{sop1, "sop1", 0xBE80, 0xFF80, 8, 15})
-	d.formatTable = append(d.formatTable, Format{sopc, "sopc", 0xBF00, 0xFF80, 16, 22})
-	d.formatTable = append(d.formatTable, Format{sopp, "sopp", 0xBF80, 0xFF80, 16, 22})
-	d.formatTable = append(d.formatTable, Format{vop1, "vop1", 0x7E00, 0xFE00, 9, 16})
-	d.formatTable = append(d.formatTable, Format{vopc, "vopc", 0x7C00, 0xFE00, 17, 24})
-	d.formatTable = append(d.formatTable, Format{smem, "smem", 0xC000, 0xFC00, 18, 25})
-	d.formatTable = append(d.formatTable, Format{vop3, "vop3", 0xD000, 0xFC00, 16, 25})
-	d.formatTable = append(d.formatTable, Format{vintrp, "vintrp", 0xC800, 0xFC00, 16, 17})
-	d.formatTable = append(d.formatTable, Format{ds, "ds", 0xD800, 0xFC00, 17, 24})
-	d.formatTable = append(d.formatTable, Format{mubuf, "mubuf", 0xE000, 0xFC00, 18, 24})
-	d.formatTable = append(d.formatTable, Format{mtbuf, "mtbuf", 0xE800, 0xFC00, 15, 18})
-	d.formatTable = append(d.formatTable, Format{mimg, "mimg", 0xF000, 0xFC00, 18, 24})
-	d.formatTable = append(d.formatTable, Format{exp, "exp", 0xC400, 0xFC00, 0, 0})
-	d.formatTable = append(d.formatTable, Format{flat, "flat", 0xDC00, 0xFC00, 18, 24})
-	d.formatTable = append(d.formatTable, Format{sopk, "sopk", 0xB000, 0xF000, 23, 27})
-	d.formatTable = append(d.formatTable, Format{sop2, "sop2", 0x8000, 0xA000, 23, 29})
-	d.formatTable = append(d.formatTable, Format{vop2, "vop2", 0x0000, 0x8000, 25, 30})
+	d.formatTable = make(map[formatType]*Format)
+	d.formatTable[sop1] = &Format{sop1, "sop1", 0xBE80, 0xFF80, 8, 15}
+	d.formatTable[sopc] = &Format{sopc, "sopc", 0xBF00, 0xFF80, 16, 22}
+	d.formatTable[sopp] = &Format{sopp, "sopp", 0xBF80, 0xFF80, 16, 22}
+	d.formatTable[vop1] = &Format{vop1, "vop1", 0x7E00, 0xFE00, 9, 16}
+	d.formatTable[vopc] = &Format{vopc, "vopc", 0x7C00, 0xFE00, 17, 24}
+	d.formatTable[smem] = &Format{smem, "smem", 0xC000, 0xFC00, 18, 25}
+	d.formatTable[vop3] = &Format{vop3, "vop3", 0xD000, 0xFC00, 16, 25}
+	d.formatTable[vintrp] = &Format{vintrp, "vintrp", 0xC800, 0xFC00, 16, 17}
+	d.formatTable[ds] = &Format{ds, "ds", 0xD800, 0xFC00, 17, 24}
+	d.formatTable[mubuf] = &Format{mubuf, "mubuf", 0xE000, 0xFC00, 18, 24}
+	d.formatTable[mtbuf] = &Format{mtbuf, "mtbuf", 0xE800, 0xFC00, 15, 18}
+	d.formatTable[mimg] = &Format{mimg, "mimg", 0xF000, 0xFC00, 18, 24}
+	d.formatTable[exp] = &Format{exp, "exp", 0xC400, 0xFC00, 0, 0}
+	d.formatTable[flat] = &Format{flat, "flat", 0xDC00, 0xFC00, 18, 24}
+	d.formatTable[sopk] = &Format{sopk, "sopk", 0xB000, 0xF000, 23, 27}
+	d.formatTable[sop2] = &Format{sop2, "sop2", 0x8000, 0xA000, 23, 29}
+	d.formatTable[vop2] = &Format{vop2, "vop2", 0x0000, 0x8000, 25, 30}
 }
 
 func (d *Disassembler) initializeDecodeTable() {
-	d.decodeTables = make(map[formatType]decodeTable)
+	d.decodeTables = make(map[formatType]*decodeTable)
+
+	d.addInstType(&InstType{"s_add_u32", 0, d.formatTable[sop2]})
 }
 
 // NewDisassembler creates a new disassembler
@@ -109,6 +115,7 @@ func NewDisassembler() *Disassembler {
 	d := new(Disassembler)
 
 	d.initializeFormatTable()
+	d.initializeDecodeTable()
 
 	return d
 }
@@ -117,12 +124,23 @@ func (d *Disassembler) matchFormat(firstTwoBytes uint16) (*Format, error) {
 	for _, f := range d.formatTable {
 		if (firstTwoBytes^f.Encoding)&f.Mask == 0 {
 			fmt.Printf("%016b matches %s\n", firstTwoBytes, f.Name)
-			return &f, nil
+			return f, nil
 		}
 	}
 	bytesString := fmt.Sprintf("%04x", firstTwoBytes)
 	return nil, errors.New("cannot find the instruction format, first two " +
 		"bytes are " + bytesString)
+}
+
+func (d *Disassembler) loopUp(format *Format, opcode Opcode) (*InstType, error) {
+	if d.decodeTables[format.formatType] != nil &&
+		d.decodeTables[format.formatType].insts[opcode] != nil {
+		return d.decodeTables[format.formatType].insts[opcode], nil
+	}
+
+	errString := fmt.Sprintf("Instruction format %s, opcode %d not found",
+		format.Name, opcode)
+	return nil, errors.New(errString)
 }
 
 // Decode parses the head of the buffer and returns the next instruction
@@ -134,7 +152,13 @@ func (d *Disassembler) Decode(buf []byte) (*Instruction, error) {
 	}
 
 	opcode := format.retrieveOpcode(binary.LittleEndian.Uint32(buf))
-	fmt.Printf("Format %s matched %08b, opcode %d.\n", format.Name, buf[0:4], opcode)
+	instType, err := d.loopUp(format, opcode)
+	if err != nil {
+		_ = fmt.Errorf("%s", err.Error())
+		return nil, err
+	}
+
+	fmt.Printf("Format %s matched %08b, opcode %d, inst %s.\n", format.Name, buf[0:4], opcode, instType.Name)
 
 	return nil, nil
 }
