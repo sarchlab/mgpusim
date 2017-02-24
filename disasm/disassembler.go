5685d62bb42060dda462478540ddb4a269e97089
Yifan Sun <sunyifan112358@gmail.com>
2017-02-24 00:08:46 -0500

    Minor fix

--- disasm/disassembler.go
+++ disasm/disassembler.go
@@ -18,6 +18,7 @@ const (
 	vop2
 	vop1
 	vop3
+	vopc
 	vintrp
 	ds
 	mubuf
@@ -29,9 +30,10 @@ const (
 
 // Format defines the possible microcode format of instructions
 type Format struct {
-	Name     string
-	Encoding uint16
-	Mask     uint16
+	formatType int
+	Name       string
+	Encoding   uint16
+	Mask       uint16
 }
 
 // Disassembler is the unit that can decode .hsaco file
@@ -41,23 +43,23 @@ type Disassembler struct {
 
 func (d *Disassembler) initializeFormatTable() {
 	d.formatTable = make([]Format, 0)
-	d.formatTable = append(d.formatTable, Format{sop1, 0xBE80, 0xFF80})
-	d.formatTable = append(d.formatTable, Format{sopc, 0xBF00, 0xFF80})
-	d.formatTable = append(d.formatTable, Format{sopp, 0xBF80, 0xFF80})
-	d.formatTable = append(d.formatTable, Format{vop1, 0x7E00, 0xFE00})
-	d.formatTable = append(d.formatTable, Format{vopc, 0x7C00, 0xFE00})
-	d.formatTable = append(d.formatTable, Format{smem, 0xC000, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{vop3, 0xD000, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{vintrp, 0xC800, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{ds, 0xD800, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{mubuf, 0xE000, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{mtbuf, 0xE800, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{ming, 0xF000, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{exp, 0xC400, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{flat, 0xDC00, 0xFC00})
-	d.formatTable = append(d.formatTable, Format{sopk, 0xB000, 0xF000})
-	d.formatTable = append(d.formatTable, Format{sop2, 0x8000, 0xA000})
-	d.formatTable = append(d.formatTable, Format{vop2, 0x0000, 0x8000})
+	d.formatTable = append(d.formatTable, Format{sop1, "sop1", 0xBE80, 0xFF80})
+	d.formatTable = append(d.formatTable, Format{sopc, "sopc", 0xBF00, 0xFF80})
+	d.formatTable = append(d.formatTable, Format{sopp, "sopp", 0xBF80, 0xFF80})
+	d.formatTable = append(d.formatTable, Format{vop1, "vop1", 0x7E00, 0xFE00})
+	d.formatTable = append(d.formatTable, Format{vopc, "vopc", 0x7C00, 0xFE00})
+	d.formatTable = append(d.formatTable, Format{smem, "smem", 0xC000, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{vop3, "vop3", 0xD000, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{vintrp, "vintrp", 0xC800, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{ds, "ds", 0xD800, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{mubuf, "mubuf", 0xE000, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{mtbuf, "mtbuf", 0xE800, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{mimg, "mimg", 0xF000, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{exp, "exp", 0xC400, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{flat, "flat", 0xDC00, 0xFC00})
+	d.formatTable = append(d.formatTable, Format{sopk, "sopk", 0xB000, 0xF000})
+	d.formatTable = append(d.formatTable, Format{sop2, "sop2", 0x8000, 0xA000})
+	d.formatTable = append(d.formatTable, Format{vop2, "vop2", 0x0000, 0x8000})
 }
 
 // NewDisassembler creates a new disassembler
