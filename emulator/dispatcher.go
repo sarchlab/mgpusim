2244208154bb1993c2770d4698ab51d1ec55f3f3
Yifan Sun <sunyifan112358@gmail.com>
2017-03-04 19:57:59 -0500

    The Dispatching process
    Former-commit-id: bfd208b84851154c8fa5759e78048302705673bc

--- emulator/dispatcher.go
+++ emulator/dispatcher.go
@@ -2,7 +2,6 @@ package emulator
 
 import (
 	"fmt"
-	"log"
 	"reflect"
 
 	"gitlab.com/yaotsu/core/conn"
@@ -14,26 +13,76 @@ import (
 //
 //   ToCommandProcessor <=> Receives kernel launching requests and send
 //                          kernel compeletion signal
-//
 //   ToComputeUnits <=> Send MapWorkGroupReq to compute units and
 //                      receives from the compute units about the completion
 //                      of the workgroups.
 type Dispatcher struct {
 	*conn.BasicComponent
+
+	ComputeUnits        []conn.Component
+	ComputeUnitsRunning []bool
+	InFlightGrids       []*Grid
+
+	MapWorkGroupReqFactory MapWorkGroupReqFactory
 }
 
 // NewDispatcher creates a new dispatcher
-func NewDispatcher(name string) *Dispatcher {
+func NewDispatcher(name string,
+	mapWorkGroupReqFactory MapWorkGroupReqFactory) *Dispatcher {
 	d := new(Dispatcher)
 	d.BasicComponent = conn.NewBasicComponent(name)
+	d.MapWorkGroupReqFactory = mapWorkGroupReqFactory
+
+	d.ComputeUnits = make([]conn.Component, 0)
+	d.ComputeUnitsRunning = make([]bool, 0)
+	d.InFlightGrids = make([]*Grid, 0)
 
 	d.AddPort("ToCommandProcessor")
+	d.AddPort("ToComputeUnits")
 
 	return d
 }
 
+func (d *Dispatcher) RegisterComputeUnit(cu conn.Component) {
+	d.ComputeUnits = append(d.ComputeUnits, cu)
+	d.ComputeUnitsRunning = append(d.ComputeUnitsRunning, false)
+}
+
+func (d *Dispatcher) dispatch(cu conn.Component, wg *WorkGroup, time event.VTimeInSec) {
+	req := d.MapWorkGroupReqFactory.Create()
+	req.SetSource(d)
+	req.SetDestination(cu)
+	req.SetSendTime(time)
+	req.WG = wg
+
+	d.GetConnection("ToComputeUnits").Send(req)
+}
+
 func (d *Dispatcher) handleLaunchKernelReq(req *LaunchKernelReq) *conn.Error {
-	log.Println("Dispatching")
+	grid := NewGrid()
+	grid.Packet = req.Packet
+	grid.SpawnWorkGroups()
+
+	d.InFlightGrids = append(d.InFlightGrids, grid)
+
+	// Dispatch workgroups for the first round
+	for i, cu := range d.ComputeUnits {
+		// Skip running CUs
+		if d.ComputeUnitsRunning[i] {
+			continue
+		}
+
+		// No more workgroups to schedule
+		if len(grid.WorkGroups) == 0 {
+			break
+		}
+
+		d.dispatch(cu, grid.WorkGroups[0], req.RecvTime())
+		grid.WorkGroupsRunning = append(grid.WorkGroupsRunning, grid.WorkGroups[0])
+		grid.WorkGroups = grid.WorkGroups[1:]
+
+	}
+
 	return nil
 }
 
