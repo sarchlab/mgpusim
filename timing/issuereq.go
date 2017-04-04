1a4f000a32b8108faf0f933026a3a2f61d5b71e9
Yifan Sun <sunyifan112358@gmail.com>
2017-04-04 16:36:33 -0400

    Timing simulation basic components
    Former-commit-id: cf7bcc3f12167a68a1eb3fd5657690763940ca24

---
+++ timing/issuereq.go
@@ -0,0 +1,12 @@
+package timing
+
+import "gitlab.com/yaotsu/core"
+
+// IssueReq is a request that is sent from the scheduler to the instruction
+// units to issue instructions
+type IssueReq struct {
+	*core.BasicRequest
+
+	Wf   *Wavefront
+	Inst *Inst
+}
--- timing/registercontroller.go
+++ timing/registercontroller.go
@@ -2,10 +2,34 @@ package timing
 
 import (
 	"gitlab.com/yaotsu/core"
+	"gitlab.com/yaotsu/gcn3/insts"
+	"gitlab.com/yaotsu/mem"
 )
 
-// A RegisterController is a Yaotsu component that is responsible for the
-// timing of reading and writing registers
-type RegisterController struct {
+// A RegReadReq is a request to read a set of register
+type RegReadReq struct {
+	*core.BasicRequest
+
+	Reg      *insts.Reg
+	ByteSize int
+	Buf      []byte
+}
+
+// A WriteReadReq is a request to read a set of register
+type WriteReadReq struct {
+	*core.BasicRequest
+
+	Reg *insts.Reg
+	Buf []byte
+}
+
+// A RegCtrl is a Yaotsu component that is responsible for the
+// timing of reading and writing registers.
+//
+//     <=> ToOutside the only port that the RegCtrl use to connect to the
+//         outside world.
+type RegCtrl struct {
 	*core.BasicComponent
+
+	storage *mem.Storage
 }
