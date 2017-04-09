feb20e93ed758f1b055b15ce1d039a5b721944f4
Yifan Sun <sunyifan112358@gmail.com>
2017-04-09 00:33:56 -0400

    Dispatcher process LaunchKernelReq

--- timing/dispatcher.go
+++ timing/dispatcher.go
@@ -1,17 +1,30 @@
 package timing
 
 import (
+	"log"
+	"reflect"
+
 	"gitlab.com/yaotsu/core"
 	"gitlab.com/yaotsu/gcn3/kernels"
 )
 
-type kernelDispatchStatus struct {
-	packet         *kernels.HsaKernelDispatchPacket
-	grid           *kernels.Grid
-	wgs            []*kernels.WorkGroup
-	dispatchingWfs []*kernels.Wavefront
-	dispatchingCU  core.Component
-	cuBusy         map[core.Component]bool
+// KernelDispatchStatus keeps the state of the dispatching process
+type KernelDispatchStatus struct {
+	Packet         *kernels.HsaKernelDispatchPacket
+	Grid           *kernels.Grid
+	WGs            []*kernels.WorkGroup
+	DispatchingWfs []*kernels.Wavefront
+	DispatchingCU  core.Component
+	CUBusy         map[core.Component]bool
+}
+
+// NewKernelDispatchStatus returns a newly created KernelDispatchStatus
+func NewKernelDispatchStatus() *KernelDispatchStatus {
+	s := new(KernelDispatchStatus)
+	s.WGs = make([]*kernels.WorkGroup, 0)
+	s.DispatchingWfs = make([]*kernels.Wavefront, 0)
+	s.CUBusy = make(map[core.Component]bool)
+	return s
 }
 
 // MapWGReq is a request that is send by the Dispatcher to a ComputeUnit to
@@ -23,26 +36,88 @@ type MapWGReq struct {
 	Ok bool
 }
 
+// NewMapWGReq returns a newly created MapWGReq
+func NewMapWGReq() *MapWGReq {
+	r := new(MapWGReq)
+	r.ReqBase = core.NewReqBase()
+	return r
+}
+
+// A KernelDispatchEvent is a event to continue the kernel dispatch process
+type KernelDispatchEvent struct {
+	*core.BasicEvent
+	Status *KernelDispatchStatus
+}
+
+// NewKernelDispatchEvent returne a newly created KernelDispatchEvent
+func NewKernelDispatchEvent() *KernelDispatchEvent {
+	e := new(KernelDispatchEvent)
+	e.BasicEvent = core.NewBasicEvent()
+	return e
+}
+
 // A Dispatcher is a component that can dispatch workgroups and wavefronts
 // to ComputeUnits.
 type Dispatcher struct {
 	*core.BasicComponent
 
-	CUs []core.Component
+	CUs  []core.Component
+	Freq core.Freq
 
+	engine      core.Engine
 	gridBuilder kernels.GridBuilder
 }
 
 // NewDispatcher creates a new dispatcher
-func NewDispatcher(name string, gridBuilder kernels.GridBuilder) *Dispatcher {
+func NewDispatcher(
+	name string,
+	engine core.Engine,
+	gridBuilder kernels.GridBuilder,
+) *Dispatcher {
 	d := new(Dispatcher)
 	d.BasicComponent = core.NewBasicComponent(name)
 	d.CUs = make([]core.Component, 0)
 	d.gridBuilder = gridBuilder
+	d.engine = engine
 	return d
 }
 
-// Receive starts processing incomming requests
-func (d *Dispatcher) Receive(req core.Req) *core.Error {
+// Recv starts processing incomming requests
+//
+// Dispatcher receives
+//     KernelDispatchReq ---- Request the dispatcher to dispatch the requests
+//     to the compute units
+func (d *Dispatcher) Recv(req core.Req) *core.Error {
+	switch req := req.(type) {
+	case *kernels.LaunchKernelReq:
+		return d.processKernelDispatchReq(req)
+	default:
+		log.Panicf("Unable to process request %s", reflect.TypeOf(req))
+	}
+	return nil
+}
+
+func (d *Dispatcher) processKernelDispatchReq(
+	req *kernels.LaunchKernelReq,
+) *core.Error {
+	evt := NewKernelDispatchEvent()
+	status := NewKernelDispatchStatus()
+	evt.Status = status
+
+	status.Packet = req.Packet
+
+	evt.SetTime(d.Freq.NextTick(req.RecvTime()))
+	evt.SetHandler(d)
+
+	d.engine.Schedule(evt)
+
+	return nil
+}
+
+// Handle performe actions when an event is triggered
+//
+// Dispatcher processes
+//     KernalDispatchEvent ---- continues the kernel dispatching process.
+func (d *Dispatcher) Handle(evt core.Event) error {
 	return nil
 }
--- timing/dispatcher_test.go
+++ timing/dispatcher_test.go
@@ -2,6 +2,7 @@ package timing_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"gitlab.com/yaotsu/core"
 	"gitlab.com/yaotsu/gcn3/kernels"
 	"gitlab.com/yaotsu/gcn3/timing"
@@ -17,6 +18,7 @@ func (b *MockGridBuilder) Build(req *kernels.LaunchKernelReq) *kernels.Grid {
 
 var _ = Describe("Dispatcher", func() {
 	var (
+		engine      *core.MockEngine
 		gridBuilder *MockGridBuilder
 		dispatcher  *timing.Dispatcher
 		cu0         *core.MockComponent
@@ -24,8 +26,10 @@ var _ = Describe("Dispatcher", func() {
 	)
 
 	BeforeEach(func() {
+		engine = core.NewMockEngine()
 		gridBuilder = new(MockGridBuilder)
-		dispatcher = timing.NewDispatcher("dispatcher", gridBuilder)
+		dispatcher = timing.NewDispatcher("dispatcher", engine, gridBuilder)
+		dispatcher.Freq = 1 * core.GHz
 		cu0 = core.NewMockComponent("mockCU0")
 		cu1 = core.NewMockComponent("mockCU1")
 
@@ -45,5 +49,14 @@ var _ = Describe("Dispatcher", func() {
 		}
 		gridBuilder.Grid = grid
 
+		req := kernels.NewLaunchKernelReq()
+		req.Packet = new(kernels.HsaKernelDispatchPacket)
+		req.SetRecvTime(0)
+
+		dispatcher.Recv(req)
+
+		Expect(len(engine.ScheduledEvent)).To(Equal(1))
+		evt := engine.ScheduledEvent[0].(*timing.KernelDispatchEvent)
+		Expect(evt.Time()).To(BeNumerically("~", 1e-9, 1e-12))
 	})
 })
