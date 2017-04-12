fdc9c42f26afd0d3e04fbe7ad1f6c289aa3bc5ea
Yifan Sun <sunyifan112358@gmail.com>
2017-04-11 23:48:14 -0400

    start working on the timing scheduler
    Former-commit-id: e5addce03b3751577f5e70f81cd2e5fa22292fc8

--- timing/cu/scheduler.go
+++ timing/cu/scheduler.go
@@ -1,18 +1,54 @@
 package cu
 
 import (
+	"log"
+	"reflect"
+	"sync"
+
 	"gitlab.com/yaotsu/core"
 	"gitlab.com/yaotsu/gcn3/kernels"
+	"gitlab.com/yaotsu/gcn3/timing"
 )
 
 // A Scheduler is responsible for determine which wavefront can fetch, decode,
 // and issue
-//
-//    <=> ToInstMem The port to the instruction memory unit
 type Scheduler struct {
 	*core.BasicComponent
+	sync.Mutex
 
-	InstMem core.Component
+	WfPools []*WavefrontPool
+
+	MappedWGs []*timing.MapWGReq
+}
+
+// NewScheduler creates and returns a new Scheduler
+func NewScheduler(name string) *Scheduler {
+	s := new(Scheduler)
+	s.BasicComponent = core.NewBasicComponent(name)
+	s.WfPools = make([]*WavefrontPool, 0, 4)
+	for i := 0; i < 4; i++ {
+		s.WfPools = append(s.WfPools, NewWavefrontPool())
+	}
+	return s
+}
+
+// Recv function process the incoming requests
+func (s *Scheduler) Recv(req core.Req) *core.Error {
+	s.Lock()
+	defer s.Unlock()
+
+	switch req := req.(type) {
+	case *timing.MapWGReq:
+		return s.processMapWGReq(req)
+	default:
+		log.Panicf("Unable to process req %s", reflect.TypeOf(req))
+	}
+
+	return nil
+}
+
+func (s *Scheduler) processMapWGReq(req *timing.MapWGReq) *core.Error {
+	return nil
 }
 
 // FetchInfo keeps record of the information of a fetch action
@@ -36,6 +72,15 @@ type WavefrontPool struct {
 
 	Wfs         []*Wavefront
 	FetchBuffer []*FetchInfo
+}
+
+// NewWavefrontPool creates and returns a new WavefrontPool
+func NewWavefrontPool() *WavefrontPool {
+	p := new(WavefrontPool)
+
+	p.Wfs = make([]*Wavefront, 0, 0)
+
+	return p
 }
 
 // A FetchArbitrator can decide which wavefront in a scheduler can fetch
