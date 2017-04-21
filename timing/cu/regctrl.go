4bdbc0b4bde18a8d75606b3cca9d130470b2770e
Yifan Sun <sunyifan112358@gmail.com>
2017-04-21 19:45:23 -0400

    Working on the reg controller

--- timing/cu/regctrl.go
+++ timing/cu/regctrl.go
@@ -12,15 +12,21 @@ type RegReadReq struct {
 
 	Reg      *insts.Reg
 	ByteSize int
+	Offset   int
 	Buf      []byte
+}
+
+// NewReadReq returns a newly create RegReadReq
+func NewReadReq(reg *inst.Reg, ByteSize int) *RegReadReq {
 }
 
 // A WriteReadReq is a request to read a set of register
 type WriteReadReq struct {
 	*core.ReqBase
 
-	Reg *insts.Reg
-	Buf []byte
+	Reg    *insts.Reg
+	Offset int
+	Buf    []byte
 }
 
 // A RegCtrl is a Yaotsu component that is responsible for the
@@ -32,4 +38,22 @@ type RegCtrl struct {
 	*core.BasicComponent
 
 	storage *mem.Storage
+}
+
+// NewRegCtrl returns a newly created RegCtrl
+func NewRegCtrl(name string, byteSize uint64) *RegCtrl {
+	c := new(RegCtrl)
+	c.BasicComponent = core.NewBasicComponent(name)
+	c.storage = mem.NewStorage(byteSize)
+	return c
+}
+
+// Recv processes incomming requests
+func (c *RegCtrl) Recv(req core.Req) *core.Error {
+	return nil
+}
+
+// Handle processes the event that is scheduled on the RegCtrl
+func (c *RegCtrl) Handle(evt core.Event) error {
+	return nil
 }
