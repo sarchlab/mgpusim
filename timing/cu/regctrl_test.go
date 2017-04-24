754a28da04593753479efc67a5b630e0c5a8ecc3
Yifan Sun <sunyifan112358@gmail.com>
2017-04-24 10:45:59 -0400

    Done with the reg file design

--- timing/cu/regctrl_test.go
+++ timing/cu/regctrl_test.go
@@ -12,16 +12,22 @@ import (
 var _ = Describe("RegCtrl", func() {
 
 	var (
-		engine  *core.MockEngine
-		regCtrl *cu.RegCtrl
+		engine     *core.MockEngine
+		regCtrl    *cu.RegCtrl
+		storage    *mem.Storage
+		connection *core.MockConnection
 	)
 
 	BeforeEach(func() {
 		engine = core.NewMockEngine()
-		regCtrl = cu.NewRegCtrl("SRegFile", 8*mem.KB, engine)
+		storage = mem.NewStorage(8 * mem.KB)
+		regCtrl = cu.NewRegCtrl("SRegFile", storage, engine)
+		connection = core.NewMockConnection()
+
+		core.PlugIn(regCtrl, "ToOutside", connection)
 	})
 
-	Context("when processing ReadRegEvent", func() {
+	Context("when processing request", func() {
 		It("should schedule ReadRegEvent", func() {
 			req := cu.NewReadRegReq(0, insts.SReg(0), 4, 0)
 			regCtrl.Recv(req)
@@ -40,5 +46,38 @@ var _ = Describe("RegCtrl", func() {
 			evt := engine.ScheduledEvent[0].(*cu.WriteRegEvent)
 			Expect(evt.Req).To(BeIdenticalTo(req))
 		})
+	})
+
+	Context("when handling event", func() {
+		It("shoud read register", func() {
+			req := cu.NewReadRegReq(0, insts.SReg(0), 4, 100)
+			evt := cu.NewReadRegEvent(regCtrl, 0.1, req)
+
+			storage.Write(100, insts.Uint32ToBytes(54))
+			connection.ExpectSend(req, nil)
+
+			regCtrl.Handle(evt)
+
+			Expect(connection.AllExpectedSent()).To(BeTrue())
+			Expect(insts.BytesToUint32(req.Buf)).To(Equal(uint32(54)))
+			Expect(req.SendTime()).To(BeNumerically("~", 0.1, 1e-9))
+		})
+
+		It("shoud write register", func() {
+			offset := 100
+			req := cu.NewWriteRegReq(0, insts.SReg(0), offset,
+				insts.Uint32ToBytes(54))
+			evt := cu.NewWriteRegEvent(regCtrl, 0.1, req)
+
+			connection.ExpectSend(req, nil)
+
+			regCtrl.Handle(evt)
+
+			Expect(connection.AllExpectedSent()).To(BeTrue())
+			data, _ := storage.Read(100, 4)
+			Expect(insts.BytesToUint32(data)).To(Equal(uint32(54)))
+			Expect(req.SendTime()).To(BeNumerically("~", 0.1, 1e-9))
+		})
+
 	})
 })
