2b9059121a54e8877682b09dc73cddacf7e4e110
Yifan Sun <sunyifan112358@gmail.com>
2017-04-26 09:17:15 -0400

    Use the emulator command processor for the timing simulator's command processor
    Former-commit-id: 3a23f3655ecaf13bfb271ff4a9205eca37a18e4e

--- timing/commandprocessor.go
+++ timing/commandprocessor.go
@@ -1,19 +1,56 @@
 package timing
 
 import (
+	"fmt"
+	"reflect"
+
 	"gitlab.com/yaotsu/core"
+	"gitlab.com/yaotsu/gcn3/kernels"
 )
 
-// A CommandProcessor serves as the gateway of a GPU. All the request goes into
-// a GPU first arrives at the CommandProcessor and then dispatched to the
-// function units.
+// CommandProcessor is a Yaotsu component that is responsible for receiving
+// requests from the driver and dispatch the requests to other parts of the
+// GPU.
+//
+//     ToDriver <=> Receive request and send feedback to the driver
+//     ToDispatcher <=> Dispatcher of compute kernels
 type CommandProcessor struct {
 	*core.BasicComponent
+
+	Dispatcher core.Component
 }
 
-// NewCommandProcessor returns a newly created command processor
+// NewCommandProcessor creates a new CommandProcessor
 func NewCommandProcessor(name string) *CommandProcessor {
-	cp := new(CommandProcessor)
-	cp.BasicComponent = core.NewBasicComponent(name)
-	return cp
+	c := new(CommandProcessor)
+	c.BasicComponent = core.NewBasicComponent(name)
+
+	c.AddPort("ToDriver")
+	c.AddPort("ToDispatcher")
+
+	return c
+}
+
+func (p *CommandProcessor) processLaunchKernelReq(
+	req *kernels.LaunchKernelReq,
+) *core.Error {
+	req.SetSrc(p)
+	req.SetDst(p.Dispatcher)
+	return p.GetConnection("ToDispatcher").Send(req)
+}
+
+// Recv processes the incomming requests
+func (p *CommandProcessor) Recv(req core.Req) *core.Error {
+	switch req := req.(type) {
+	case *kernels.LaunchKernelReq:
+		return p.processLaunchKernelReq(req)
+	default:
+		return core.NewError(
+			fmt.Sprintf("cannot process request %s", reflect.TypeOf(req)), false, 0)
+	}
+}
+
+// Handle processes the events that is scheduled for the CommandProcessor
+func (p *CommandProcessor) Handle(e core.Event) error {
+	return nil
 }
