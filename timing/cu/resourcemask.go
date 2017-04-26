56f3d95399326f3a070a2a45cd38eac37105eccf
Yifan Sun <sunyifan112358@gmail.com>
2017-04-26 14:41:07 -0400

    Start to create a generic resource mask class

---
+++ timing/cu/resourcemask.go
@@ -0,0 +1,53 @@
+package cu
+
+// AllocStatus represents the allocation status of SGPRs, VGPRs, or LDS units
+type AllocStatus byte
+
+// A list of possible status for CU binded storage allocation
+const (
+	AllocStatusFree     AllocStatus = iota
+	AllocStatusReserved             // Work-Group mapped, but wavefront not dispatched
+	AllocStatusUsed                 // Currently in use
+)
+
+// A ResourceMask is data structure to mask the status of some resources
+type ResourceMask struct {
+	mask []AllocStatus
+}
+
+// NewResourceMask returns a newly created ResourceMask with a given size.
+func NewResourceMask(size int) *ResourceMask {
+	m := new(ResourceMask)
+	m.mask = make([]AllocStatus, size)
+	return m
+}
+
+// NextRegion finds a region that is masked by the resourceMask in
+// the state define by statusReq. This function returns the offset of the
+// starting point of the region. A negtive value is returned if no such region
+// can be found.
+func (m *ResourceMask) NextRegion(
+	length int,
+	statusReq AllocStatus,
+) int {
+	offset := 0
+	currLength := 0
+	for offset < len(m.mask) {
+		if m.mask[offset] == statusReq {
+			currLength++
+			if currLength == length {
+				return offset - currLength + 1
+			}
+		} else {
+			currLength = 0
+		}
+	}
+	return -1
+}
+
+// SetStatus alters the status from the position of offset to offset + length
+func (m *ResourceMask) SetStatus(offset, length int, status AllocStatus) {
+	for i := 0; i < length; i++ {
+		m.mask[offset+i] = status
+	}
+}
