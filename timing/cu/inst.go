d4967805b461bca5711d850bc3390c80b43bdeba
Yifan Sun <sunyifan112358@gmail.com>
2017-05-12 08:28:34 -0400

    Decode instruction
    Former-commit-id: 9904afde2ea02744b6d5078cf79d3429273a424e

--- timing/cu/inst.go
+++ timing/cu/inst.go
@@ -1,13 +1,17 @@
 package cu
 
 import "gitlab.com/yaotsu/gcn3/insts"
-import "gitlab.com/yaotsu/core"
 
-// Inst in the timing package is a wrapper of the insts.Inst. This Inst has
-// an ID and is hookable
+// Inst in the timing package is a wrapper of the insts.Inst.
 type Inst struct {
 	*insts.Inst
-	*core.HookableBase
 
 	ID int
+}
+
+// NewInst creates a newly created Inst
+func NewInst(raw *insts.Inst) *Inst {
+	i := new(Inst)
+	i.Inst = raw
+	return i
 }
