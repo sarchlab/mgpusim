6c68a54c78293f9487b0962aa6bd911e14cb858d
Yifan Sun <sunyifan112358@gmail.com>
2017-05-22 14:43:17 -0400

    Scalar unit and LDSUnit
    Former-commit-id: 39923f0b9f3fbacf986d6ac6798b6d5b89893907

--- timing/cu/scalarunit.go
+++ timing/cu/scalarunit.go
@@ -15,12 +15,26 @@ import (
 // FromDecoder <=>
 type ScalarUnit struct {
 	*core.ComponentBase
+
+	Freq core.Freq
+
+	engine    core.Engine
+	scheduler core.Component
+	running   bool
+
+	reading   *Wavefront
+	executing *Wavefront
+	writing   *Wavefront
 }
 
 // NewScalarUnit creates and retuns a new ScalarUnit
-func NewScalarUnit(name string) *ScalarUnit {
+func NewScalarUnit(name string, engine core.Engine, scheduler core.Component) *ScalarUnit {
 	u := new(ScalarUnit)
 	u.ComponentBase = core.NewComponentBase(name)
+
+	u.engine = engine
+	u.scheduler = scheduler
+
 	u.AddPort("ToScheduler")
 	u.AddPort("FromDecoder")
 	return u
@@ -30,16 +44,92 @@ func NewScalarUnit(name string) *ScalarUnit {
 func (u *ScalarUnit) Recv(req core.Req) *core.Error {
 	switch req := req.(type) {
 	case *IssueInstReq:
-		replyReq := NewInstCompletionReq(u, req.Scheduler, req.RecvTime(),
-			req.Wf)
-		u.GetConnection("ToScheduler").Send(replyReq)
+		return u.processIssueInstReq(req)
 	default:
 		log.Panicf("cannot process request of type %s", reflect.TypeOf(req))
 	}
 	return nil
 }
 
+func (u *ScalarUnit) processIssueInstReq(req *IssueInstReq) *core.Error {
+	if u.reading != nil {
+		return core.NewError("unit busy", true, u.Freq.NextTick(req.RecvTime()))
+	}
+
+	u.reading = req.Wf
+	u.tryStartTick(req.RecvTime())
+	return nil
+}
+
 // Handle defines how the ScalarUnit handles events
 func (u *ScalarUnit) Handle(evt core.Event) error {
+	switch evt := evt.(type) {
+	case *core.TickEvent:
+		return u.handleTickEvent(evt)
+	default:
+		log.Panicf("cannot handle event of type %s", reflect.TypeOf(evt))
+	}
 	return nil
+}
+
+func (u *ScalarUnit) handleTickEvent(evt *core.TickEvent) error {
+	u.doWrite(evt.Time())
+	u.doExec(evt.Time())
+	u.doRead(evt.Time())
+
+	u.continueTick(evt.Time())
+
+	return nil
+}
+
+func (u *ScalarUnit) doWrite(now core.VTimeInSec) {
+	if u.writing != nil {
+		req := NewInstCompletionReq(u, u.scheduler, now, u.writing)
+		err := u.GetConnection("ToScheduler").Send(req)
+		if err == nil {
+			u.writing = nil
+		}
+	}
+}
+
+func (u *ScalarUnit) doExec(now core.VTimeInSec) {
+	if u.executing != nil {
+		if u.writing == nil {
+			u.writing = u.executing
+			u.executing = nil
+		}
+	}
+}
+
+func (u *ScalarUnit) doRead(now core.VTimeInSec) {
+	if u.reading != nil {
+		if u.executing == nil {
+			u.executing = u.reading
+			u.reading = nil
+		}
+	}
+}
+
+func (u *ScalarUnit) tryStartTick(now core.VTimeInSec) {
+	if !u.running {
+		u.scheduleTick(now)
+	}
+}
+
+func (u *ScalarUnit) continueTick(now core.VTimeInSec) {
+	if u.reading == nil &&
+		u.executing == nil &&
+		u.writing == nil {
+		u.running = false
+	}
+
+	if u.running {
+		u.scheduleTick(now)
+	}
+}
+
+func (u *ScalarUnit) scheduleTick(now core.VTimeInSec) {
+	evt := core.NewTickEvent(now, u)
+	u.engine.Schedule(evt)
+	u.running = true
 }
