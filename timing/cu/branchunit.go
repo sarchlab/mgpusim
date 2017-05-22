9bcf1fd421e2b369e1128cb822381751e4267113
Yifan Sun <sunyifan112358@gmail.com>
2017-05-22 13:08:40 -0400

    Branching unit receive IssueInstReq
    Former-commit-id: 4a24b0b968d093721c529b41fd8a202ab7aa860e

--- timing/cu/branchunit.go
+++ timing/cu/branchunit.go
@@ -13,12 +13,22 @@ import (
 // ToScheduler <=> The port that connects the BranchUnit and the Scheduler
 type BranchUnit struct {
 	*core.ComponentBase
+
+	Freq core.Freq
+
+	engine  core.Engine
+	running bool
+
+	reading   *Wavefront
+	executing *Wavefront
+	writing   *Wavefront
 }
 
 // NewBranchUnit creates and retuns a new BranchUnit
-func NewBranchUnit(name string) *BranchUnit {
+func NewBranchUnit(name string, engine core.Engine) *BranchUnit {
 	u := new(BranchUnit)
 	u.ComponentBase = core.NewComponentBase(name)
+	u.engine = engine
 	u.AddPort("ToScheduler")
 	return u
 }
@@ -27,16 +37,41 @@ func NewBranchUnit(name string) *BranchUnit {
 func (u *BranchUnit) Recv(req core.Req) *core.Error {
 	switch req := req.(type) {
 	case *IssueInstReq:
-		replyReq := NewInstCompletionReq(u, req.Scheduler, req.RecvTime(),
-			req.Wf)
-		u.GetConnection("ToScheduler").Send(replyReq)
+		return u.processIssueInstReq(req)
 	default:
 		log.Panicf("cannot process request of type %s", reflect.TypeOf(req))
 	}
 	return nil
 }
 
+func (u *BranchUnit) processIssueInstReq(req *IssueInstReq) *core.Error {
+	if u.reading != nil {
+		return core.NewError("unit busy", true, u.Freq.NextTick(req.RecvTime()))
+	}
+
+	u.reading = req.Wf
+	return nil
+}
+
 // Handle defines how the BranchUnit handles events
 func (u *BranchUnit) Handle(evt core.Event) error {
 	return nil
+}
+
+func (u *BranchUnit) tryStartTick(now core.VTimeInSec) {
+	if !u.running {
+		u.scheduleTick(now)
+	}
+}
+
+func (u *BranchUnit) continueTick(now core.VTimeInSec) {
+	if u.running {
+		u.scheduleTick(now)
+	}
+}
+
+func (u *BranchUnit) scheduleTick(now core.VTimeInSec) {
+	evt := core.NewTickEvent(now, u)
+	u.engine.Schedule(evt)
+	u.running = true
 }
---
+++ timing/cu/branchunit_test.go
@@ -0,0 +1,40 @@
+package cu
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"gitlab.com/yaotsu/core"
+)
+
+var _ = Describe("Branch Unit", func() {
+	var (
+		engine *core.MockEngine
+		unit   *BranchUnit
+	)
+
+	BeforeEach(func() {
+		engine = core.NewMockEngine()
+		unit = NewBranchUnit("branch_unit", engine)
+		unit.Freq = 1
+	})
+
+	It("should not accept instruction is reading stage is occupied", func() {
+		wf := new(Wavefront)
+		unit.reading = wf
+
+		req := NewIssueInstReq(nil, unit, 10, nil, wf)
+		err := unit.Recv(req)
+
+		Expect(err).NotTo(BeNil())
+	})
+
+	It("should accept instruction is reading stage is not occupied", func() {
+		wf := new(Wavefront)
+
+		req := NewIssueInstReq(nil, unit, 10, nil, wf)
+		err := unit.Recv(req)
+
+		Expect(err).To(BeNil())
+		Expect(unit.reading).To(BeIdenticalTo(wf))
+	})
+})
