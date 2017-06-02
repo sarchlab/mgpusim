d982c2ade425f8b8b594150274b7087a210b46a2
Yifan Sun <sunyifan112358@gmail.com>
2017-06-01 22:59:58 -0400

    A brief server

--- timing/cu/inst.go
+++ timing/cu/inst.go
@@ -3,6 +3,7 @@ package cu
 import (
 	"sync"
 
+	"gitlab.com/yaotsu/core"
 	"gitlab.com/yaotsu/gcn3/insts"
 )
 
@@ -27,4 +28,11 @@ func NewInst(raw *insts.Inst) *Inst {
 	_NextInstIDMutex.Unlock()
 
 	return i
+}
+
+// InstHookInfo is the information that is to be provided to the hooks that
+// hooks to Inst
+type InstHookInfo struct {
+	Now   core.VTimeInSec
+	Stage string
 }
