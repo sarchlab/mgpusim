24be4e93b78ce717def94bfcb6e19cce19859e46
Yifan Sun <sunyifan112358@gmail.com>
2017-06-17 22:25:30 -0400

    Refactor minimap rendering
    Former-commit-id: 90abfcd3cd2f7af45ad55bd3715ef9ee6496ff7e
    Former-commit-id: 519b85de3ffa2761521ca73a69ec3bb0ffbf5f24

--- trace/vis/www/js/minimap.js
+++ trace/vis/www/js/minimap.js
@@ -25,34 +25,39 @@ class Minimap {
     }
 
     render() {
-        let data = this.data;
-        let width = window.innerWidth;
-        let height = 200;
+        this.width = window.innerWidth;
+        this.height = 200;
 
-        let svg = d3.select('#minimap').selectAll('svg')
-            .attr('height', height)
-            .attr('width', width);
+        this.svg = d3.select('#minimap').selectAll('svg')
+            .attr('height', this.height)
+            .attr('width', this.width);
 
-        let startTime = data[0].start_time;
-        let endTime = data[data.length - 1].end_time;
+        this.defineScales();
+        this.drawMinibars();
+        this.applyBrush();
+        this.applyAxises();
+    }
+
+    defineScales() {
+        let startTime = this.data[0].start_time;
+        let endTime = this.data[this.data.length - 1].end_time;
         this.xScale = d3.scaleLinear()
             .domain([startTime, endTime])
-            .range([0, width]);
-        let widthScale = d3.scaleLinear()
+            .range([0, this.width]);
+        this.widthScale = d3.scaleLinear()
             .domain([0, endTime - startTime])
-            .range([0, width]);
-        let xAxis = d3.axisTop(this.xScale);
+            .range([0, this.width]);
 
-        let highestCount = d3.max(data, function(d){ return d.count; });
-        let verticalScale = d3.scaleLinear()
+        let highestCount = d3.max(this.data, function(d){ return d.count; });
+        this.yScale = d3.scaleLinear()
             .domain([0, highestCount])
-            .range([height, 0]);
-        let yAxis = d3.axisRight(verticalScale);
+            .range([this.height, 0]);
+    }
 
-
-        let minimapBars = svg.select('.main-area')
+    drawMinibars() {
+        let minimapBars = this.svg.select('.main-area')
             .selectAll('rect')
-            .data(data);
+            .data(this.data);
 
         let minimapBarsEnter = minimapBars.enter()
             .append('rect')
@@ -61,33 +66,38 @@ class Minimap {
         minimapBars.merge(minimapBarsEnter)
             .transition()
             .attr('x', function (d) {
-                return vis.minimap.xScale(d.start_time);
-            })
+                return this.xScale(d.start_time);
+            }.bind(this))
             .attr('y', function (d) {
-                return verticalScale(d.count);
-            })
+                return this.yScale(d.count);
+            }.bind(this))
             .attr('width', function (d) {
-                return widthScale(d.end_time - d.start_time);
-            })
+                return this.widthScale(d.end_time - d.start_time);
+            }.bind(this))
             .attr('height', function (d) {
-                return height - verticalScale(d.count);
-            });
-
-        // Brush
-        let brush = d3.brushX()
-            .extent([[0, 0], [width, height]])
-            .on('brush end', this.brushed.bind(this));
-        svg.select('.brush')
-            .call(brush)
-            .call(brush.move, [width / 10, width / 5]);
+                return this.height - this.yScale(d.count);
+            }.bind(this));
 
 
-        // Axises
-        svg.selectAll('.x-axis')
+    }
+
+    applyAxises() {
+        let xAxis = d3.axisTop(this.xScale);
+        this.svg.selectAll('.x-axis')
             .attr("transform", "translate(0, 200)")
             .call(xAxis);
-        svg.selectAll('.y-axis')
+        let yAxis = d3.axisRight(this.yScale);
+        this.svg.selectAll('.y-axis')
             .call(yAxis);
+    }
+
+    applyBrush() {
+        let brush = d3.brushX()
+            .extent([[0, 0], [this.width, this.height]])
+            .on('brush end', this.brushed.bind(this));
+        this.svg.select('.brush')
+            .call(brush)
+            .call(brush.move, [this.width / 10, this.width / 8]);
     }
 
     brushed() {
