9a9b64201eb7039e54784a47e30e13222ee85fca
Yifan Sun <sunyifan112358@gmail.com>
2017-06-19 09:21:01 -0400

    vector copy example emulation main file
    Former-commit-id: 8f8523367e33111c6a31c9920d4e3a92938d05a0
    Former-commit-id: 7ad35b6de8cc6756a9c7a8245ac96c1a8ddc48c5

---
+++ samples/vec_copy/emu/main.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"bytes"
+	"debug/elf"
+	"encoding/binary"
+	"fmt"
+	"log"
+	_ "net/http/pprof"
+	"os"
+
+	"flag"
+
+	"gitlab.com/yaotsu/core"
+	"gitlab.com/yaotsu/gcn3"
+	"gitlab.com/yaotsu/gcn3/emu"
+	"gitlab.com/yaotsu/gcn3/insts"
+	"gitlab.com/yaotsu/gcn3/kernels"
+	"gitlab.com/yaotsu/mem"
+)
+
+type hostComponent struct {
+	*core.ComponentBase
+}
+
+func newHostComponent() *hostComponent {
+	h := new(hostComponent)
+	h.ComponentBase = core.NewComponentBase("host")
+	h.AddPort("ToGpu")
+	return h
+}
+
+func (h *hostComponent) Recv(req core.Req) *core.Error {
+	switch req.(type) {
+	case *kernels.LaunchKernelReq:
+		log.Println("Kernel completed.")
+	}
+	return nil
+}
+
+func (h *hostComponent) Handle(evt core.Event) error {
+	return nil
+}
+
+var (
+	engine     core.Engine
+	globalMem  *mem.IdealMemController
+	gpu        *gcn3.Gpu
+	host       *hostComponent
+	connection core.Connection
+	hsaco      *insts.HsaCo
+	logger     *log.Logger
+)
+
+var kernel = flag.String("kernel", "../vector_copy.hsaco",
+	"the kernel hsaco file")
+
+func main() {
+	flag.Parse()
+
+	// log.SetOutput(ioutil.Discard)
+	logger = log.New(os.Stdout, "", 0)
+
+	initPlatform()
+	loadProgram()
+	initMem()
+	run()
+	checkResult()
+}
+
+func initPlatform() {
+	// Simulation engine
+	engine = core.NewSerialEngine()
+
+	// Connection
+	connection = core.NewDirectConnection(engine)
+
+	// Memory
+	globalMem = mem.NewIdealMemController("GlobalMem", engine, 4*mem.GB)
+	globalMem.Freq = 1 * core.GHz
+	globalMem.Latency = 1
+
+	// Host
+	host = newHostComponent()
+
+	// Gpu
+	gpu = gcn3.NewGpu("GPU")
+	commandProcessor := gcn3.NewCommandProcessor("GPU.CommandProcessor")
+
+	dispatcher := gcn3.NewDispatcher("GPU.Dispatcher", engine,
+		new(kernels.GridBuilderImpl))
+	dispatcher.Freq = 1 * core.GHz
+	wgCompleteLogger := new(gcn3.WGCompleteLogger)
+	wgCompleteLogger.Logger = logger
+	dispatcher.AcceptHook(wgCompleteLogger)
+
+	gpu.CommandProcessor = commandProcessor
+	gpu.Driver = host
+	commandProcessor.Dispatcher = dispatcher
+	commandProcessor.Driver = gpu
+	disassembler := insts.NewDisassembler()
+	for i := 0; i < 4; i++ {
+		regInterface := new(emu.RegInterfaceImpl)
+		scratchpadPreparer := emu.NewScratchpadPreparerImpl(regInterface)
+		alu := new(emu.ALU)
+		computeUnit := emu.NewComputeUnit(fmt.Sprintf("%s.cu%d", gpu.Name(), i),
+			engine, disassembler, scratchpadPreparer, alu)
+		computeUnit.Freq = 1 * core.GHz
+		computeUnit.GlobalMemStorage = globalMem.Storage
+		dispatcher.CUs = append(dispatcher.CUs, computeUnit)
+		core.PlugIn(computeUnit, "ToDispatcher", connection)
+	}
+
+	// Connection
+	core.PlugIn(gpu, "ToCommandProcessor", connection)
+	core.PlugIn(gpu, "ToDriver", connection)
+	core.PlugIn(commandProcessor, "ToDriver", connection)
+	core.PlugIn(commandProcessor, "ToDispatcher", connection)
+	core.PlugIn(host, "ToGpu", connection)
+	core.PlugIn(dispatcher, "ToCommandProcessor", connection)
+	core.PlugIn(dispatcher, "ToCUs", connection)
+	core.PlugIn(globalMem, "Top", connection)
+}
+
+func loadProgram() {
+	executable, err := elf.Open(*kernel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sec := executable.Section(".text")
+	hsacoData, err := sec.Data()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = globalMem.Storage.Write(0, hsacoData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hsaco = insts.NewHsaCoFromData(hsacoData)
+}
+
+func initMem() {
+	// Write the input
+	inputData := make([]byte, 1024*4)
+	buffer := bytes.NewBuffer(inputData)
+	for i := 0; i < 1024; i++ {
+		binary.Write(buffer, binary.LittleEndian, int32(i))
+	}
+
+	err := globalMem.Storage.Write(8*mem.KB, inputData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func run() {
+	kernelArgsBuffer := bytes.NewBuffer(make([]byte, 36))
+	binary.Write(kernelArgsBuffer, binary.LittleEndian, uint64(8192))      // Input
+	binary.Write(kernelArgsBuffer, binary.LittleEndian, uint64(8192+4096)) // Output
+	err := globalMem.Storage.Write(65536, kernelArgsBuffer.Bytes())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req := kernels.NewLaunchKernelReq()
+	req.HsaCo = hsaco
+	req.Packet = new(kernels.HsaKernelDispatchPacket)
+	req.Packet.GridSizeX = 256 * 4
+	req.Packet.GridSizeY = 1
+	req.Packet.GridSizeZ = 1
+	req.Packet.WorkgroupSizeX = 256
+	req.Packet.WorkgroupSizeY = 1
+	req.Packet.WorkgroupSizeZ = 1
+	req.Packet.KernelObject = 0
+	req.Packet.KernargAddress = 65536
+
+	req.SetSrc(host)
+	req.SetDst(gpu)
+	req.SetSendTime(0)
+	connErr := connection.Send(req)
+	if connErr != nil {
+		log.Fatal(connErr)
+	}
+
+	engine.Run()
+}
+
+func checkResult() {
+	buffer, err := globalMem.Storage.Read(8192, 1024*4)
+	if err != nil {
+		log.Fatal(nil)
+	}
+
+	for i := 0; i < 1024; i++ {
+		copied := binary.LittleEndian.Uint32(buffer[i*4 : i*4+4])
+		fmt.Printf("%d: %d\n", i, copied)
+	}
+}
