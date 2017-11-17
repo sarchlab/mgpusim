888d8042e809f6f1896b8490d37aeef31566d649
Yifan Sun <sunyifan112358@gmail.com>
2017-11-16 19:18:37 -0500

    s_and_b64
    Former-commit-id: e62614b3ef4f39beacc21e43c5039f957ecafeac
    Former-commit-id: b94fd67fdc40b64f43eb141227c39f3f74b954d0

--- emu/alu.go
+++ emu/alu.go
@@ -54,54 +54,6 @@ func (u *ALU) Run(state InstEmuState) {
 
 }
 
-func (u *ALU) runSOP2(state InstEmuState) {
-	inst := state.Inst()
-	switch inst.Opcode {
-	case 0:
-		u.runSADDU32(state)
-	case 4:
-		u.runSADDCU32(state)
-	default:
-		log.Panicf("Opcode %d for SOP2 format is not implemented", inst.Opcode)
-	}
-}
-
-func (u *ALU) runSADDU32(state InstEmuState) {
-	sp := state.Scratchpad()
-
-	src0 := insts.BytesToUint32(sp[0:8])
-	src1 := insts.BytesToUint32(sp[8:16])
-
-	dst := src0 + src1
-	scc := byte(0)
-	if src0 > math.MaxUint32-src1 {
-		scc = 1
-	} else {
-		scc = 0
-	}
-
-	copy(sp[16:24], insts.Uint32ToBytes(dst))
-	sp[24] = scc
-}
-
-func (u *ALU) runSADDCU32(state InstEmuState) {
-	sp := state.Scratchpad()
-
-	src0 := insts.BytesToUint32(sp[0:8])
-	src1 := insts.BytesToUint32(sp[8:16])
-	scc := sp[24]
-
-	dst := src0 + src1 + uint32(scc)
-	if src0 < math.MaxUint32-uint32(scc)-src1 {
-		scc = 0
-	} else {
-		scc = 1
-	}
-
-	copy(sp[16:24], insts.Uint32ToBytes(dst))
-	sp[24] = scc
-}
-
 func (u *ALU) runSOPC(state InstEmuState) {
 	inst := state.Inst()
 	sp := state.Scratchpad().AsSOPC()
