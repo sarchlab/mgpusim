b52ac834333a118b3ff9d74b9751a012b5b95b9a
Yifan Sun <sunyifan112358@gmail.com>
2017-11-24 19:52:27 -0500

    v_rcp_iflag_f32
    Former-commit-id: 81e553f348cb2d2a893d81e86afb2d5194597389
    Former-commit-id: 618ec8d0f912b0e19889d7842551aa1b275e37e1

--- emu/aluvop1_test.go
+++ emu/aluvop1_test.go
@@ -62,4 +62,20 @@ var _ = Describe("ALU", func() {
 		Expect(math.Float32frombits(uint32(sp.DST[0]))).To(Equal(float32(1.0)))
 	})
 
+	It("should run V_RCP_IFLAG_F32", func() {
+		state.inst = insts.NewInst()
+		state.inst.FormatType = insts.Vop1
+		state.inst.Opcode = 35
+
+		sp := state.Scratchpad().AsVOP1()
+		sp.SRC0[0] = uint64(math.Float32bits(1.0))
+		sp.SRC0[1] = uint64(math.Float32bits(2.0))
+		sp.EXEC = 0x3
+
+		alu.Run(state)
+
+		Expect(math.Float32frombits(uint32(sp.DST[0]))).To(Equal(float32(1.0)))
+		Expect(math.Float32frombits(uint32(sp.DST[1]))).To(Equal(float32(0.5)))
+	})
+
 })
