6030423c2da1f646d6b0db03ed6f8cb42a48c10f
Yifan Sun <sunyifan112358@gmail.com>
2017-12-03 14:03:40 -0500

    v_sub_i32
    Former-commit-id: 741c1e0317d32fd16daf274632781109834eaae3
    Former-commit-id: 856d9317ac24529d13fac574120c8edaad706c86

--- emu/aluvop2_.go
+++ emu/aluvop2_.go
@@ -16,6 +16,8 @@ func (u *ALU) runVOP2(state InstEmuState) {
 		u.runVMACF32(state)
 	case 25:
 		u.runVADDI32(state)
+	case 26:
+		u.runVSUBI32(state)
 	case 28:
 		u.runVADDCU32(state)
 	default:
@@ -56,6 +58,27 @@ func (u *ALU) runVMULF32(state InstEmuState) {
 	}
 }
 
+func (u *ALU) runVMACF32(state InstEmuState) {
+	sp := state.Scratchpad().AsVOP2()
+
+	var dst float32
+	var src0 float32
+	var src1 float32
+
+	var i uint
+	for i = 0; i < 64; i++ {
+		if !u.laneMasked(sp.EXEC, i) {
+			continue
+		}
+
+		dst = asFloat32(uint32(sp.DST[i]))
+		src0 = asFloat32(uint32(sp.SRC0[i]))
+		src1 = asFloat32(uint32(sp.SRC1[i]))
+		dst += src0 * src1
+		sp.DST[i] = uint64(float32ToBits(dst))
+	}
+}
+
 func (u *ALU) runVADDI32(state InstEmuState) {
 	sp := state.Scratchpad().AsVOP2()
 
@@ -77,6 +100,26 @@ func (u *ALU) runVADDI32(state InstEmuState) {
 	}
 }
 
+func (u *ALU) runVSUBI32(state InstEmuState) {
+	sp := state.Scratchpad().AsVOP2()
+
+	var i uint
+	for i = 0; i < 64; i++ {
+		if !u.laneMasked(sp.EXEC, i) {
+			continue
+		}
+
+		src0 := uint32(sp.SRC0[i])
+		src1 := uint32(sp.SRC1[i])
+
+		if src0 < src1 {
+			sp.VCC |= 1 << uint32(i)
+		}
+
+		sp.DST[i] = uint64(src0 - src1)
+	}
+}
+
 func (u *ALU) runVADDCU32(state InstEmuState) {
 	sp := state.Scratchpad().AsVOP2()
 
@@ -93,26 +136,5 @@ func (u *ALU) runVADDCU32(state InstEmuState) {
 		}
 
 		sp.DST[i] = sp.SRC0[i] + sp.SRC1[i] + carry
-	}
-}
-
-func (u *ALU) runVMACF32(state InstEmuState) {
-	sp := state.Scratchpad().AsVOP2()
-
-	var dst float32
-	var src0 float32
-	var src1 float32
-
-	var i uint
-	for i = 0; i < 64; i++ {
-		if !u.laneMasked(sp.EXEC, i) {
-			continue
-		}
-
-		dst = asFloat32(uint32(sp.DST[i]))
-		src0 = asFloat32(uint32(sp.SRC0[i]))
-		src1 = asFloat32(uint32(sp.SRC1[i]))
-		dst += src0 * src1
-		sp.DST[i] = uint64(float32ToBits(dst))
 	}
 }
