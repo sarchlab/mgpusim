bff0280e98d4d548e55eef34a8fffd51614256cf
Yifan Sun <sunyifan112358@gmail.com>
2018-01-19 22:37:17 -0500

    Add fetch arbitor and issue arbitor back

---
+++ timing/issuearbiter_test.go
@@ -0,0 +1,59 @@
+package timing
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"gitlab.com/yaotsu/gcn3/insts"
+)
+
+var _ = Describe("IssueArbiter", func() {
+	var (
+		arbiter *IssueArbiter
+		wfPools []*WavefrontPool
+	)
+
+	BeforeEach(func() {
+		arbiter = NewIssueArbiter()
+		wfPools = make([]*WavefrontPool, 0, 4)
+		for i := 0; i < 4; i++ {
+			wfPools = append(wfPools, NewWavefrontPool(10))
+		}
+	})
+
+	It("should decide which wf to issue", func() {
+		wfState := []WfState{
+			WfRunning, WfFetched, WfFetched, WfReady, WfFetched,
+			WfFetched, WfFetched, WfFetched, WfFetched, WfFetched,
+		}
+		exeUnits := []insts.ExeUnit{
+			insts.ExeUnitVALU, insts.ExeUnitScalar, insts.ExeUnitVMem,
+			insts.ExeUnitBranch, insts.ExeUnitLDS, insts.ExeUnitSpecial,
+			insts.ExeUnitVALU, insts.ExeUnitBranch, insts.ExeUnitVALU,
+			insts.ExeUnitVMem,
+		}
+		wfs := make([]*Wavefront, 0)
+
+		for i := 0; i < len(wfState); i++ {
+			wf := new(Wavefront)
+			wf.State = wfState[i]
+			wf.Inst = NewInst(insts.NewInst())
+			wf.Inst.ExeUnit = exeUnits[i]
+			wfs = append(wfs, wf)
+			wfPools[0].AddWf(wf)
+		}
+
+		issueCandidate := arbiter.Arbitrate(wfPools)
+
+		Expect(len(issueCandidate)).To(Equal(6))
+		Expect(issueCandidate).NotTo(ContainElement(BeIdenticalTo(wfs[0])))
+		Expect(issueCandidate).To(ContainElement(BeIdenticalTo(wfs[1])))
+		Expect(issueCandidate).To(ContainElement(BeIdenticalTo(wfs[2])))
+		Expect(issueCandidate).NotTo(ContainElement(BeIdenticalTo(wfs[3])))
+		Expect(issueCandidate).To(ContainElement(BeIdenticalTo(wfs[4])))
+		Expect(issueCandidate).To(ContainElement(BeIdenticalTo(wfs[5])))
+		Expect(issueCandidate).To(ContainElement(BeIdenticalTo(wfs[6])))
+		Expect(issueCandidate).To(ContainElement(BeIdenticalTo(wfs[7])))
+		Expect(issueCandidate).NotTo(ContainElement(BeIdenticalTo(wfs[8])))
+		Expect(issueCandidate).NotTo(ContainElement(BeIdenticalTo(wfs[9])))
+	})
+})
