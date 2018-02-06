ebf9221ce32cf5bd319971d040ab37b520841203
Yifan Sun <sunyifan112358@gmail.com>
2018-02-05 22:15:11 -0500

    Issue
    Former-commit-id: 7e9100f49ced2e50c832b14497fd8544f1f32a99
    Former-commit-id: 5deb171aff21247850a8337c1135ddc9207016d2

--- timing/scheduler.go
+++ timing/scheduler.go
@@ -1,16 +1,20 @@
 package timing
 
 import (
+	"log"
+
 	"gitlab.com/yaotsu/core"
+	"gitlab.com/yaotsu/gcn3/insts"
 	"gitlab.com/yaotsu/mem"
 )
 
 // A Scheduler is the controlling unit of a compute unit. It decides which
 // wavefront to fetch and to issue.
 type Scheduler struct {
-	cu           *ComputeUnit
-	fetchArbiter WfArbiter
-	issueArbiter WfArbiter
+	cu                *ComputeUnit
+	fetchArbiter      WfArbiter
+	issueArbiter      WfArbiter
+	internalExecuting *Wavefront
 }
 
 // NewScheduler returns a newly created scheduler, injecting dependency
@@ -55,5 +59,76 @@ func (s *Scheduler) DoFetch(now core.VTimeInSec) {
 // DoIssue function of the scheduler issues fetched instruction to the decoding
 // units
 func (s *Scheduler) DoIssue(now core.VTimeInSec) {
+	wfs := s.issueArbiter.Arbitrate(s.cu.WfPools)
+	for _, wf := range wfs {
+		if wf.Inst.ExeUnit == insts.ExeUnitSpecial {
+			s.issueToInternal(wf, now)
+			continue
+		}
 
+		unit := s.getUnitToIssueTo(wf.Inst.ExeUnit)
+		if unit.CanAcceptWave() {
+			unit.AcceptWave(wf)
+			wf.State = WfRunning
+			s.cu.InvokeHook(wf, s.cu, core.Any, &InstHookInfo{now, "Issue"})
+		}
+	}
+}
+
+func (s *Scheduler) issueToInternal(wf *Wavefront, now core.VTimeInSec) {
+	if s.internalExecuting == nil {
+		s.internalExecuting = wf
+		wf.State = WfRunning
+	} else {
+		wf.State = WfFetched
+	}
+
+}
+
+func (s *Scheduler) getUnitToIssueTo(u insts.ExeUnit) CUComponent {
+	switch u {
+	case insts.ExeUnitBranch:
+		return s.cu.BranchUnit
+	case insts.ExeUnitLDS:
+		return s.cu.LDSDecoder
+	case insts.ExeUnitVALU:
+		return s.cu.VectorDecoder
+	case insts.ExeUnitVMem:
+		return s.cu.VectorMemDecoder
+	case insts.ExeUnitScalar:
+		return s.cu.ScalarDecoder
+	default:
+		log.Panic("not sure where to dispatch instrcution")
+	}
+	return nil
+}
+
+// EvaluateInternalInst updates the status of the instruction being executed
+// in the scheduler.
+func (s *Scheduler) EvaluateInternalInst(now core.VTimeInSec) {
+	if s.internalExecuting == nil {
+		return
+	}
+
+	executing := s.internalExecuting
+
+	switch s.internalExecuting.Inst.Opcode {
+	case 1: // S_ENDPGM
+		s.evalSEndPgm(s.internalExecuting, now)
+	default:
+		// The program has to make progress
+		s.internalExecuting.State = WfReady
+		s.internalExecuting = nil
+	}
+
+	if s.internalExecuting == nil {
+		s.cu.InvokeHook(executing, s.cu, core.Any,
+			&InstHookInfo{now, "Completed"})
+	}
+}
+
+func (s *Scheduler) evalSEndPgm(wf *Wavefront, now core.VTimeInSec) {
+	wfCompletionEvt := NewWfCompletionEvent(s.cu.Freq.NextTick(now), s.cu, wf)
+	s.cu.engine.Schedule(wfCompletionEvt)
+	s.internalExecuting = nil
 }
--- timing/scheduler_test.go
+++ timing/scheduler_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"gitlab.com/yaotsu/core"
+	"gitlab.com/yaotsu/gcn3/insts"
 	"gitlab.com/yaotsu/mem"
 )
 
@@ -26,19 +27,55 @@ func (m *mockWfArbitor) Arbitrate([]*WavefrontPool) []*Wavefront {
 	return wfs
 }
 
+type mockCUComponent struct {
+	canAccept    bool
+	acceptedWave []*Wavefront
+}
+
+func (c *mockCUComponent) CanAcceptWave() bool {
+	return c.canAccept
+}
+
+func (c *mockCUComponent) AcceptWave(wave *Wavefront) error {
+	c.acceptedWave = append(c.acceptedWave, wave)
+	return nil
+}
+
+func (c *mockCUComponent) Run() {
+}
+
 var _ = Describe("Scheduler", func() {
 	var (
-		toInstMemConn *core.MockConnection
-		cu            *ComputeUnit
-		scheduler     *Scheduler
-		fetchArbitor  *mockWfArbitor
-		issueArbitor  *mockWfArbitor
-		instMem       *core.MockComponent
+		toInstMemConn    *core.MockConnection
+		engine           *core.MockEngine
+		cu               *ComputeUnit
+		branchUnit       *mockCUComponent
+		ldsDecoder       *mockCUComponent
+		vectorMemDecoder *mockCUComponent
+		vectorDecoder    *mockCUComponent
+		scalarDecoder    *mockCUComponent
+		scheduler        *Scheduler
+		fetchArbitor     *mockWfArbitor
+		issueArbitor     *mockWfArbitor
+		instMem          *core.MockComponent
 	)
 
 	BeforeEach(func() {
 		toInstMemConn = core.NewMockConnection()
-		cu = NewComputeUnit("cu", nil)
+		engine = core.NewMockEngine()
+		cu = NewComputeUnit("cu", engine)
+
+		vectorDecoder = new(mockCUComponent)
+		cu.VectorDecoder = vectorDecoder
+		scalarDecoder = new(mockCUComponent)
+		cu.ScalarDecoder = scalarDecoder
+		branchUnit = new(mockCUComponent)
+		cu.BranchUnit = branchUnit
+		vectorMemDecoder = new(mockCUComponent)
+		cu.VectorMemDecoder = vectorMemDecoder
+		ldsDecoder = new(mockCUComponent)
+		cu.LDSDecoder = ldsDecoder
+
 		cu.InstMem = instMem
 		core.PlugIn(cu, "ToInstMem", toInstMemConn)
 
@@ -67,6 +104,75 @@ var _ = Describe("Scheduler", func() {
 
 		Expect(toInstMemConn.AllExpectedSent()).To(BeTrue())
 		Expect(wf.State).To(Equal(WfFetching))
+	})
+
+	It("should issue", func() {
+		wfs := make([]*Wavefront, 0)
+		issueDirs := []insts.ExeUnit{
+			insts.ExeUnitBranch,
+			insts.ExeUnitLDS,
+			insts.ExeUnitVMem,
+			insts.ExeUnitVALU,
+			insts.ExeUnitScalar,
+		}
+		branchUnit.canAccept = true
+		ldsDecoder.canAccept = true
+		vectorDecoder.canAccept = true
+		vectorMemDecoder.canAccept = true
+		scalarDecoder.canAccept = false
+
+		for i := 0; i < 5; i++ {
+			wf := new(Wavefront)
+			wf.State = WfFetched
+			wf.Inst = NewInst(insts.NewInst())
+			wf.Inst.ExeUnit = issueDirs[i]
+			wfs = append(wfs, wf)
+		}
+
+		issueArbitor.wfsToReturn = append(issueArbitor.wfsToReturn, wfs)
+
+		scheduler.DoIssue(10)
+
+		Expect(len(branchUnit.acceptedWave)).To(Equal(1))
+		Expect(len(ldsDecoder.acceptedWave)).To(Equal(1))
+		Expect(len(vectorDecoder.acceptedWave)).To(Equal(1))
+		Expect(len(vectorMemDecoder.acceptedWave)).To(Equal(1))
+		Expect(len(scalarDecoder.acceptedWave)).To(Equal(0))
+
+		Expect(wfs[0].State).To(Equal(WfRunning))
+		Expect(wfs[1].State).To(Equal(WfRunning))
+		Expect(wfs[2].State).To(Equal(WfRunning))
+		Expect(wfs[3].State).To(Equal(WfRunning))
+		Expect(wfs[4].State).To(Equal(WfFetched))
+	})
+
+	It("should issue internal instruction", func() {
+		wfs := make([]*Wavefront, 0)
+		wf := new(Wavefront)
+		wf.Inst = NewInst(insts.NewInst())
+		wf.Inst.ExeUnit = insts.ExeUnitSpecial
+		wf.State = WfFetched
+		wfs = append(wfs, wf)
+
+		issueArbitor.wfsToReturn = append(issueArbitor.wfsToReturn, wfs)
+		scheduler.internalExecuting = nil
+
+		scheduler.DoIssue(10)
+
+		Expect(scheduler.internalExecuting).To(BeIdenticalTo(wf))
+		Expect(wf.State).To(Equal(WfRunning))
+	})
+
+	It("should evaluate internal executing insts", func() {
+		wf := new(Wavefront)
+		wf.Inst = NewInst(insts.NewInst())
+		wf.Inst.Format = insts.FormatTable[insts.Sopp]
+		wf.Inst.Opcode = 1 // S_ENDPGM
+
+		scheduler.internalExecuting = wf
+		scheduler.EvaluateInternalInst(10)
+
+		Expect(len(engine.ScheduledEvent)).To(Equal(1))
 	})
 
 })
