596ccc5b671172769cc7eafd3ece89aec8495804
Shi Dong <shidong1986@gmail.com>
2018-02-06 23:10:41 -0500

    Fix one bug regarding loading float operand
    Former-commit-id: 4b8d2596d4c2891efad5d7657469a743619f11d3
    Former-commit-id: fabfb411ccaad011b022ea0f67c275907a5b3104

--- emu/scratchpadpreparer.go
+++ emu/scratchpadpreparer.go
@@ -2,6 +2,7 @@ package emu
 
 import (
 	"log"
+	"math"
 
 	"gitlab.com/yaotsu/gcn3/insts"
 )
@@ -389,7 +390,7 @@ func (p *ScratchpadPreparerImpl) readOperand(
 	case insts.IntOperand:
 		copy(buf, insts.Uint64ToBytes(uint64(operand.IntValue)))
 	case insts.FloatOperand:
-		copy(buf, insts.Uint64ToBytes(uint64(operand.IntValue)))
+		copy(buf, insts.Uint64ToBytes(uint64(math.Float32bits(float32(operand.FloatValue)))))
 	case insts.LiteralConstant:
 		copy(buf, insts.Uint32ToBytes(operand.LiteralConstant))
 	default:
