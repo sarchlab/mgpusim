188c69319d7d43dd1b56f1ccff5fc16252c41285
Yifan Sun <sunyifan112358@gmail.com>
2018-02-07 16:05:29 -0500

    Finish simd unit

--- timing/simdunit.go
+++ timing/simdunit.go
@@ -2,51 +2,60 @@ package timing
 
 import "gitlab.com/yaotsu/core"
 
-// A BranchUnit performs branch operations
-type BranchUnit struct {
+// A SIMDUnit performs branch operations
+type SIMDUnit struct {
 	cu *ComputeUnit
 
-	toRead  *Wavefront
-	toExec  *Wavefront
-	toWrite *Wavefront
+	toRead        *Wavefront
+	toExec        *Wavefront
+	execCycleLeft int
+	toWrite       *Wavefront
 }
 
-// NewBranchUnit creates a new branch unit, injecting the dependency of
+// NewSIMDUnit creates a new branch unit, injecting the dependency of
 // the compute unit.
-func NewBranchUnit(cu *ComputeUnit) *BranchUnit {
-	u := new(BranchUnit)
+func NewSIMDUnit(cu *ComputeUnit) *SIMDUnit {
+	u := new(SIMDUnit)
 	u.cu = cu
 	return u
 }
 
 // CanAcceptWave checks if the buffer of the read stage is occupied or not
-func (u *BranchUnit) CanAcceptWave() bool {
+func (u *SIMDUnit) CanAcceptWave() bool {
 	return u.toRead == nil
 }
 
 // AcceptWave moves one wavefront into the read buffer of the branch unit
-func (u *BranchUnit) AcceptWave(wave *Wavefront, now core.VTimeInSec) {
+func (u *SIMDUnit) AcceptWave(wave *Wavefront, now core.VTimeInSec) {
 	u.toRead = wave
 }
 
-// Run executes three pipeline stages that are controlled by the BranchUnit
-func (u *BranchUnit) Run(now core.VTimeInSec) {
+// Run executes three pipeline stages that are controlled by the SIMDUnit
+func (u *SIMDUnit) Run(now core.VTimeInSec) {
 	u.runWriteStage(now)
 	u.runExecStage(now)
 	u.runReadStage(now)
 }
 
-func (u *BranchUnit) runReadStage(now core.VTimeInSec) {
+func (u *SIMDUnit) runReadStage(now core.VTimeInSec) {
 	if u.toExec == nil {
 		u.cu.InvokeHook(u.toRead, u.cu, core.Any, &InstHookInfo{now, "ReadEnd"})
 		u.cu.InvokeHook(u.toRead, u.cu, core.Any, &InstHookInfo{now, "ExecStart"})
 
 		u.toExec = u.toRead
+		u.execCycleLeft = 4
 		u.toRead = nil
 	}
 }
 
-func (u *BranchUnit) runExecStage(now core.VTimeInSec) {
+func (u *SIMDUnit) runExecStage(now core.VTimeInSec) {
+	if u.toExec != nil {
+		u.execCycleLeft--
+		if u.execCycleLeft > 0 {
+			return
+		}
+	}
+
 	if u.toWrite == nil {
 		u.cu.InvokeHook(u.toExec, u.cu, core.Any, &InstHookInfo{now, "ExecEnd"})
 		u.cu.InvokeHook(u.toExec, u.cu, core.Any, &InstHookInfo{now, "WriteStart"})
@@ -56,7 +65,7 @@ func (u *BranchUnit) runExecStage(now core.VTimeInSec) {
 	}
 }
 
-func (u *BranchUnit) runWriteStage(now core.VTimeInSec) {
+func (u *SIMDUnit) runWriteStage(now core.VTimeInSec) {
 	u.cu.InvokeHook(u.toWrite, u.cu, core.Any, &InstHookInfo{now, "WriteEnd"})
 	u.cu.InvokeHook(u.toWrite, u.cu, core.Any, &InstHookInfo{now, "Completed"})
 
