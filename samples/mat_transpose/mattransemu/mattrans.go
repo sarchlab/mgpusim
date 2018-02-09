375fbc77297b61db7927db8afab6322211bff969
Yifan Sun <sunyifan112358@gmail.com>
2018-02-09 14:33:17 -0500

    Use GPUBuilder in all the emulator samples

--- samples/mat_transpose/mattransemu/mattrans.go
+++ samples/mat_transpose/mattransemu/mattrans.go
@@ -24,7 +24,7 @@ import (
 	"gitlab.com/yaotsu/core/engines"
 	"gitlab.com/yaotsu/core/util"
 	"gitlab.com/yaotsu/gcn3"
-	"gitlab.com/yaotsu/gcn3/emu"
+	"gitlab.com/yaotsu/gcn3/gpubuilder"
 	"gitlab.com/yaotsu/gcn3/insts"
 	"gitlab.com/yaotsu/gcn3/kernels"
 	"gitlab.com/yaotsu/mem"
@@ -101,64 +101,18 @@ func main() {
 }
 
 func initPlatform() {
-	// Simulation engine
 	engine = engines.NewSerialEngine()
-	// engine.AcceptHook(core.NewLogEventHook(log.New(os.Stdout, "", 0)))
+	engine.AcceptHook(util.NewEventLogger(log.New(os.Stdout, "", 0)))
 
-	// Connection
+	host = newHostComponent()
 	connection = connections.NewDirectConnection(engine)
 
-	// Memory
-	globalMem = mem.NewIdealMemController("GlobalMem", engine, 4*mem.GB)
-	globalMem.Freq = 1 * util.GHz
-	globalMem.Latency = 1
+	gpuBuilder := gpubuilder.NewGPUBuilder(engine)
+	gpuBuilder.Driver = host
+	gpu, globalMem = gpuBuilder.BuildEmulationGPU()
 
-	// Host
-	host = newHostComponent()
-
-	// Gpu
-	gpu = gcn3.NewGPU("GPU")
-	commandProcessor := gcn3.NewCommandProcessor("GPU.CommandProcessor")
-
-	dispatcher := gcn3.NewDispatcher("GPU.Dispatcher", engine,
-		new(kernels.GridBuilderImpl))
-	dispatcher.Freq = 1 * util.GHz
-	wgCompleteLogger := new(gcn3.WGCompleteLogger)
-	wgCompleteLogger.Logger = logger
-	dispatcher.AcceptHook(wgCompleteLogger)
-
-	gpu.CommandProcessor = commandProcessor
-	gpu.Driver = host
-	commandProcessor.Dispatcher = dispatcher
-	commandProcessor.Driver = gpu
-	disassembler := insts.NewDisassembler()
-	isaDebug, err := os.Create("isa.debug")
-	if err != nil {
-		fmt.Print("Isa debug file failed to open\n")
-	}
-	for i := 0; i < 4; i++ {
-		scratchpadPreparer := emu.NewScratchpadPreparerImpl()
-		alu := emu.NewALU(globalMem.Storage)
-		computeUnit := emu.NewComputeUnit(fmt.Sprintf("%s.cu%d", gpu.Name(), i),
-			engine, disassembler, scratchpadPreparer, alu)
-		computeUnit.Freq = 1 * util.GHz
-		computeUnit.GlobalMemStorage = globalMem.Storage
-		dispatcher.RegisterCU(computeUnit)
-		core.PlugIn(computeUnit, "ToDispatcher", connection)
-
-		wfHook := emu.NewWfHook(log.New(isaDebug, "", 0))
-		computeUnit.AcceptHook(wfHook)
-	}
-
-	// Connection
-	core.PlugIn(gpu, "ToCommandProcessor", connection)
 	core.PlugIn(gpu, "ToDriver", connection)
-	core.PlugIn(commandProcessor, "ToDriver", connection)
-	core.PlugIn(commandProcessor, "ToDispatcher", connection)
-	core.PlugIn(host, "ToGpu", connection)
-	core.PlugIn(dispatcher, "ToCommandProcessor", connection)
-	core.PlugIn(dispatcher, "ToCUs", connection)
-	core.PlugIn(globalMem, "Top", connection)
+	gpu.Driver = host
 }
 
 func loadProgram() {
