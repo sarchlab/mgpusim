495850bbd895c6f6ca25d5e2b265a5a8039fbdef
Yifan Sun <sunyifan112358@gmail.com>
2018-02-15 11:20:06 -0500

    Scratchpad preparer of timing can now pass test

--- timing/scratchpadpreparer_test.go
+++ timing/scratchpadpreparer_test.go
@@ -229,242 +229,242 @@ var _ = Describe("ScratchpadPreparer", func() {
 
 	})
 
-	//It("should prepare for SOPP", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Sopp
-	//	inst.SImm16 = insts.NewIntOperand(1, 1)
-	//	wf.EXEC = 0x0f
-	//	wf.PC = 160
-	//	wf.SCC = 1
-	//	wf.inst = NewInst(inst)
-	//
-	//	sp.Prepare(wf, wf)
-	//
-	//	layout := wf.Scratchpad().AsSOPP()
-	//	Expect(layout.EXEC).To(Equal(uint64(0x0f)))
-	//	Expect(layout.IMM).To(Equal(uint64(1)))
-	//	Expect(layout.PC).To(Equal(uint64(160)))
-	//	Expect(layout.SCC).To(Equal(byte(1)))
-	//})
-	//
-	//It("should prepare for SOPC", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Sopc
-	//	inst.Src0 = insts.NewSRegOperand(0, 0, 1)
-	//	wf.WriteReg(insts.SReg(0), 1, 0, insts.Uint32ToBytes(100))
-	//	inst.Src1 = insts.NewIntOperand(192, 64)
-	//	wf.inst = NewInst(inst)
-	//
-	//	sp.Prepare(wf, wf)
-	//
-	//	layout := wf.Scratchpad().AsSOPC()
-	//	Expect(layout.SRC0).To(Equal(uint64(100)))
-	//	Expect(layout.SRC1).To(Equal(uint64(64)))
-	//})
-	//
-	//It("should commit for SOP1", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Sop1
-	//	inst.Dst = insts.NewSRegOperand(0, 0, 1)
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsSOP1()
-	//	layout.DST = 517
-	//	layout.EXEC = 0xffffffff00000000
-	//	layout.SCC = 1
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	Expect(wf.SCC).To(Equal(byte(1)))
-	//	Expect(wf.EXEC).To(Equal(uint64(0xffffffff00000000)))
-	//	Expect(wf.SRegValue(0)).To(Equal(uint32(517)))
-	//})
-	//
-	//It("should commit for SOP2", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Sop2
-	//	inst.Dst = insts.NewSRegOperand(0, 0, 1)
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsSOP2()
-	//	layout.DST = 517
-	//	layout.SCC = 1
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	Expect(wf.SCC).To(Equal(byte(1)))
-	//	Expect(wf.SRegValue(0)).To(Equal(uint32(517)))
-	//})
-	//
-	//It("should commit for VOP1", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Vop1
-	//	inst.Dst = insts.NewVRegOperand(0, 0, 1)
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsVOP1()
-	//	for i := 0; i < 64; i++ {
-	//		layout.DST[i] = uint64(i)
-	//	}
-	//	layout.VCC = uint64(0xffff0000ffff0000)
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	for i := 0; i < 64; i++ {
-	//		Expect(wf.VRegValue(i, 0)).To(Equal(uint32(i)))
-	//	}
-	//	Expect(wf.VCC).To(Equal(uint64(0xffff0000ffff0000)))
-	//})
-	//
-	//It("should commit for VOP2", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Vop2
-	//	inst.Dst = insts.NewVRegOperand(0, 0, 1)
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsVOP2()
-	//	for i := 0; i < 64; i++ {
-	//		layout.DST[i] = uint64(i)
-	//	}
-	//	layout.VCC = uint64(0xffff0000ffff0000)
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	for i := 0; i < 64; i++ {
-	//		Expect(wf.VRegValue(i, 0)).To(Equal(uint32(i)))
-	//	}
-	//	Expect(wf.VCC).To(Equal(uint64(0xffff0000ffff0000)))
-	//})
-	//
-	//It("should commit for VOP3", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Vop3
-	//	inst.Dst = insts.NewVRegOperand(0, 0, 1)
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsVOP3A()
-	//	for i := 0; i < 64; i++ {
-	//		layout.DST[i] = uint64(i)
-	//	}
-	//	layout.VCC = uint64(0xffff0000ffff0000)
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	for i := 0; i < 64; i++ {
-	//		Expect(wf.VRegValue(i, 0)).To(Equal(uint32(i)))
-	//	}
-	//	Expect(wf.VCC).To(Equal(uint64(0xffff0000ffff0000)))
-	//})
-	//
-	//It("should commit VOPC", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Vopc
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsVOPC()
-	//	layout.VCC = uint64(0xff)
-	//	layout.EXEC = uint64(0x01)
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	Expect(wf.VCC).To(Equal(uint64(0xff)))
-	//	Expect(wf.EXEC).To(Equal(uint64(0x01)))
-	//})
-	//
-	//It("should commit for FLAT", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Flat
-	//	inst.Opcode = 20 // Load Dword
-	//	inst.Dst = insts.NewVRegOperand(3, 3, 4)
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsFlat()
-	//	for i := 0; i < 64; i++ {
-	//		layout.DST[i*4+0] = uint32(i)
-	//		layout.DST[i*4+1] = uint32(i)
-	//		layout.DST[i*4+2] = uint32(i)
-	//		layout.DST[i*4+3] = uint32(i)
-	//	}
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	for i := 0; i < 64; i++ {
-	//		Expect(wf.VRegValue(i, 3)).To(Equal(uint32(i)))
-	//		Expect(wf.VRegValue(i, 4)).To(Equal(uint32(i)))
-	//		Expect(wf.VRegValue(i, 5)).To(Equal(uint32(i)))
-	//		Expect(wf.VRegValue(i, 6)).To(Equal(uint32(i)))
-	//	}
-	//})
-	//
-	//It("should not commit for FLAT store operation", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Flat
-	//	inst.Dst = insts.NewVRegOperand(3, 3, 4)
-	//	inst.Opcode = 28 // Store dword
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsFlat()
-	//	for i := 0; i < 64; i++ {
-	//		layout.DST[i*4+0] = uint32(i)
-	//		layout.DST[i*4+1] = uint32(i)
-	//		layout.DST[i*4+2] = uint32(i)
-	//		layout.DST[i*4+3] = uint32(i)
-	//	}
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	for i := 0; i < 64; i++ {
-	//		Expect(wf.VRegValue(i, 3)).To(Equal(uint32(0)))
-	//		Expect(wf.VRegValue(i, 4)).To(Equal(uint32(0)))
-	//		Expect(wf.VRegValue(i, 5)).To(Equal(uint32(0)))
-	//		Expect(wf.VRegValue(i, 6)).To(Equal(uint32(0)))
-	//	}
-	//})
-	//
-	//It("should commit for SMEM", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Smem
-	//	inst.Opcode = 4
-	//	inst.Data = insts.NewSRegOperand(0, 0, 16)
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsSMEM()
-	//	for i := 0; i < 16; i++ {
-	//		layout.DST[i] = uint32(i)
-	//	}
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	for i := 0; i < 16; i++ {
-	//		Expect(wf.SRegValue(i)).To(Equal(uint32(i)))
-	//	}
-	//})
-	//
-	//It("should commit for SOPC", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Sopc
-	//	wf.inst = NewInst(inst)
-	//
-	//	layout := wf.Scratchpad().AsSOPC()
-	//	layout.SCC = 1
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	Expect(wf.SCC).To(Equal(byte(1)))
-	//})
-	//
-	//It("should commit for SOPP", func() {
-	//	inst := insts.NewInst()
-	//	inst.FormatType = insts.Sopp
-	//	wf.inst = NewInst(inst)
-	//	wf.PC = 0
-	//
-	//	layout := wf.Scratchpad().AsSOPP()
-	//	layout.PC = 164
-	//
-	//	sp.Commit(wf, wf)
-	//
-	//	Expect(wf.PC).To(Equal(uint64(164)))
-	//})
+	It("should prepare for SOPP", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Sopp
+		inst.SImm16 = insts.NewIntOperand(1, 1)
+		wf.EXEC = 0x0f
+		wf.PC = 160
+		wf.SCC = 1
+		wf.inst = NewInst(inst)
+
+		sp.Prepare(wf, wf)
+
+		layout := wf.Scratchpad().AsSOPP()
+		Expect(layout.EXEC).To(Equal(uint64(0x0f)))
+		Expect(layout.IMM).To(Equal(uint64(1)))
+		Expect(layout.PC).To(Equal(uint64(160)))
+		Expect(layout.SCC).To(Equal(byte(1)))
+	})
+
+	It("should prepare for SOPC", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Sopc
+		inst.Src0 = insts.NewSRegOperand(0, 0, 1)
+		sp.writeReg(insts.SReg(0), 1, wf, 0, insts.Uint32ToBytes(100))
+		inst.Src1 = insts.NewIntOperand(192, 64)
+		wf.inst = NewInst(inst)
+
+		sp.Prepare(wf, wf)
+
+		layout := wf.Scratchpad().AsSOPC()
+		Expect(layout.SRC0).To(Equal(uint64(100)))
+		Expect(layout.SRC1).To(Equal(uint64(64)))
+	})
+
+	It("should commit for SOP1", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Sop1
+		inst.Dst = insts.NewSRegOperand(0, 0, 1)
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsSOP1()
+		layout.DST = 517
+		layout.EXEC = 0xffffffff00000000
+		layout.SCC = 1
+
+		sp.Commit(wf, wf)
+
+		Expect(wf.SCC).To(Equal(byte(1)))
+		Expect(wf.EXEC).To(Equal(uint64(0xffffffff00000000)))
+		Expect(sp.readRegAsUint32(insts.SReg(0), wf, 0)).To(Equal(uint32(517)))
+	})
+
+	It("should commit for SOP2", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Sop2
+		inst.Dst = insts.NewSRegOperand(0, 0, 1)
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsSOP2()
+		layout.DST = 517
+		layout.SCC = 1
+
+		sp.Commit(wf, wf)
+
+		Expect(wf.SCC).To(Equal(byte(1)))
+		Expect(sp.readRegAsUint32(insts.SReg(0), wf, 0)).To(Equal(uint32(517)))
+	})
+
+	It("should commit for VOP1", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Vop1
+		inst.Dst = insts.NewVRegOperand(0, 0, 1)
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsVOP1()
+		for i := 0; i < 64; i++ {
+			layout.DST[i] = uint64(i)
+		}
+		layout.VCC = uint64(0xffff0000ffff0000)
+
+		sp.Commit(wf, wf)
+
+		for i := 0; i < 64; i++ {
+			Expect(sp.readRegAsUint32(insts.VReg(i), wf, 0)).To(Equal(uint32(0)))
+		}
+		Expect(wf.VCC).To(Equal(uint64(0xffff0000ffff0000)))
+	})
+
+	It("should commit for VOP2", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Vop2
+		inst.Dst = insts.NewVRegOperand(0, 0, 1)
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsVOP2()
+		for i := 0; i < 64; i++ {
+			layout.DST[i] = uint64(i)
+		}
+		layout.VCC = uint64(0xffff0000ffff0000)
+
+		sp.Commit(wf, wf)
+
+		for i := 0; i < 64; i++ {
+			Expect(sp.readRegAsUint32(insts.VReg(i), wf, 0)).To(Equal(uint32(0)))
+		}
+		Expect(wf.VCC).To(Equal(uint64(0xffff0000ffff0000)))
+	})
+
+	It("should commit for VOP3", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Vop3
+		inst.Dst = insts.NewVRegOperand(0, 0, 1)
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsVOP3A()
+		for i := 0; i < 64; i++ {
+			layout.DST[i] = uint64(i)
+		}
+		layout.VCC = uint64(0xffff0000ffff0000)
+
+		sp.Commit(wf, wf)
+
+		for i := 0; i < 64; i++ {
+			Expect(sp.readRegAsUint32(insts.VReg(i), wf, 0)).To(Equal(uint32(0)))
+		}
+		Expect(wf.VCC).To(Equal(uint64(0xffff0000ffff0000)))
+	})
+
+	It("should commit VOPC", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Vopc
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsVOPC()
+		layout.VCC = uint64(0xff)
+		layout.EXEC = uint64(0x01)
+
+		sp.Commit(wf, wf)
+
+		Expect(wf.VCC).To(Equal(uint64(0xff)))
+		Expect(wf.EXEC).To(Equal(uint64(0x01)))
+	})
+
+	It("should commit for FLAT", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Flat
+		inst.Opcode = 20 // Load Dword
+		inst.Dst = insts.NewVRegOperand(3, 3, 4)
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsFlat()
+		for i := 0; i < 64; i++ {
+			layout.DST[i*4+0] = uint32(i)
+			layout.DST[i*4+1] = uint32(i)
+			layout.DST[i*4+2] = uint32(i)
+			layout.DST[i*4+3] = uint32(i)
+		}
+
+		sp.Commit(wf, wf)
+
+		for i := 0; i < 64; i++ {
+			Expect(sp.readRegAsUint32(insts.VReg(3), wf, i)).To(Equal(uint32(i)))
+			Expect(sp.readRegAsUint32(insts.VReg(4), wf, i)).To(Equal(uint32(i)))
+			Expect(sp.readRegAsUint32(insts.VReg(5), wf, i)).To(Equal(uint32(i)))
+			Expect(sp.readRegAsUint32(insts.VReg(6), wf, i)).To(Equal(uint32(i)))
+		}
+	})
+
+	It("should not commit for FLAT store operation", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Flat
+		inst.Dst = insts.NewVRegOperand(3, 3, 4)
+		inst.Opcode = 28 // Store dword
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsFlat()
+		for i := 0; i < 64; i++ {
+			layout.DST[i*4+0] = uint32(i)
+			layout.DST[i*4+1] = uint32(i)
+			layout.DST[i*4+2] = uint32(i)
+			layout.DST[i*4+3] = uint32(i)
+		}
+
+		sp.Commit(wf, wf)
+
+		for i := 0; i < 64; i++ {
+			Expect(sp.readRegAsUint32(insts.VReg(3), wf, i)).To(Equal(uint32(0)))
+			Expect(sp.readRegAsUint32(insts.VReg(4), wf, i)).To(Equal(uint32(0)))
+			Expect(sp.readRegAsUint32(insts.VReg(5), wf, i)).To(Equal(uint32(0)))
+			Expect(sp.readRegAsUint32(insts.VReg(6), wf, i)).To(Equal(uint32(0)))
+		}
+	})
+
+	It("should commit for SMEM", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Smem
+		inst.Opcode = 4
+		inst.Data = insts.NewSRegOperand(0, 0, 16)
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsSMEM()
+		for i := 0; i < 16; i++ {
+			layout.DST[i] = uint32(i)
+		}
+
+		sp.Commit(wf, wf)
+
+		for i := 0; i < 16; i++ {
+			Expect(sp.readRegAsUint32(insts.SReg(i), wf, 0)).To(Equal(uint32(i)))
+		}
+	})
+
+	It("should commit for SOPC", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Sopc
+		wf.inst = NewInst(inst)
+
+		layout := wf.Scratchpad().AsSOPC()
+		layout.SCC = 1
+
+		sp.Commit(wf, wf)
+
+		Expect(wf.SCC).To(Equal(byte(1)))
+	})
+
+	It("should commit for SOPP", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.Sopp
+		wf.inst = NewInst(inst)
+		wf.PC = 0
+
+		layout := wf.Scratchpad().AsSOPP()
+		layout.PC = 164
+
+		sp.Commit(wf, wf)
+
+		Expect(wf.PC).To(Equal(uint64(164)))
+	})
 
 })
