723f684dc82934461b1363e94502dd9023727397
trinayan <trinayan16101993@gmail.com>
2018-03-18 14:37:11 +0100

    Commiting initial microbenchmarks empty kernel

---
+++ microbench/empty_kernel/microbench_empty_kernel.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"debug/elf"
+	"fmt"
+	"log"
+	_ "net/http/pprof"
+
+	"gitlab.com/yaotsu/core/connections"
+
+	"flag"
+
+	"gitlab.com/yaotsu/core"
+	"gitlab.com/yaotsu/core/engines"
+	"gitlab.com/yaotsu/gcn3"
+	"gitlab.com/yaotsu/gcn3/driver"
+	"gitlab.com/yaotsu/gcn3/gpubuilder"
+	"gitlab.com/yaotsu/gcn3/insts"
+	"gitlab.com/yaotsu/mem"
+)
+
+type EmptyKernelArgs struct {
+	hiddenGlobalOffsetX int64
+	hiddenGlobalOffsetY int64
+	hiddenGlobalOffsetZ int64
+}
+
+var (
+	engine     core.Engine
+	globalMem  *mem.IdealMemController
+	gpu        *gcn3.GPU
+	connection core.Connection
+	hsaco      *insts.HsaCo
+	logger     *log.Logger
+	gpuDriver  *driver.Driver
+
+)
+
+var cpuprofile = flag.String("cpuprofile", "prof.prof", "write cpu profile to file")
+var kernel = flag.String("kernel", "kernels.hsaco", "the kernel hsaco file")
+
+func main() {
+	//flag.Parse()
+	//
+	//f, err := os.Create("trace.out")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer f.Close()
+	//
+	//err = trace.Start(f)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer trace.Stop()
+	//
+	//logger = log.New(os.Stdout, "", 0)
+
+	initPlatform()
+	loadProgram()
+	initMem()
+	run()
+	//checkResult()
+}
+
+func initPlatform() {
+	engine = engines.NewSerialEngine()
+	//engine = engines.NewParallelEngine()
+	//engine.AcceptHook(util.NewEventLogger(log.New(os.Stdout, "", 0)))
+
+	gpuDriver = driver.NewDriver(engine)
+	connection = connections.NewDirectConnection(engine)
+
+	gpuBuilder := gpubuilder.NewGPUBuilder(engine)
+	gpuBuilder.Driver = gpuDriver
+	gpuBuilder.EnableISADebug = true
+	gpu, globalMem = gpuBuilder.BuildEmulationGPU()
+
+	core.PlugIn(gpuDriver, "ToGPUs", connection)
+	core.PlugIn(gpu, "ToDriver", connection)
+	gpu.Driver = gpuDriver
+}
+
+func loadProgram() {
+	executable, err := elf.Open(*kernel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sec := executable.Section(".text")
+	hsacoData, err := sec.Data()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hsaco = insts.NewHsaCoFromData(hsacoData)
+	fmt.Println(hsaco.Info())
+}
+
+func initMem() {
+
+}
+
+func run() {
+	kernArg := EmptyKernelArgs{
+		0, 0, 0,
+	}
+
+	gpuDriver.LaunchKernel(hsaco, gpu, globalMem.Storage,
+		[3]uint32{1, 1, 1},
+		[3]uint16{256, 1, 1},
+		&kernArg,
+	)
+}
---
+++ microbench/empty_kernel_register/kernel.s
@@ -0,0 +1,76 @@
+.hsa_code_object_version 2, 1
+.hsa_code_object_isa 8,0,3,"AMD","AMDGPU"
+
+.text
+.p2align 8
+.amdgpu_hsa_kernel empty_kernel_register
+
+empty_kernel_register:
+	
+	.amd_kernel_code_t
+	amd_code_version_major = 1
+		amd_code_version_minor = 1
+		amd_machine_kind = 1
+		amd_machine_version_major = 8
+		amd_machine_version_minor = 0
+		amd_machine_version_stepping = 3
+		kernel_code_entry_byte_offset = 256
+		kernel_code_prefetch_byte_size = 0
+		max_scratch_backing_memory_byte_size = 0
+		granulated_workitem_vgpr_count = 1
+		granulated_wavefront_sgpr_count = 1
+		priority = 0
+		float_mode = 192
+		priv = 0
+		enable_dx10_clamp = 1
+		debug_mode = 0
+		enable_ieee_mode = 1
+		enable_sgpr_private_segment_wave_byte_offset = 0
+		user_sgpr_count = 8
+		enable_trap_handler = 1
+		enable_sgpr_workgroup_id_x = 1
+		enable_sgpr_workgroup_id_y = 0
+		enable_sgpr_workgroup_id_z = 0
+		enable_sgpr_workgroup_info = 0
+		enable_vgpr_workitem_id = 0
+		enable_exception_msb = 0
+		granulated_lds_size = 0
+		enable_exception = 0
+		enable_sgpr_private_segment_buffer = 1
+		enable_sgpr_dispatch_ptr = 1
+		enable_sgpr_queue_ptr = 0
+		enable_sgpr_kernarg_segment_ptr = 1
+		enable_sgpr_dispatch_id = 0
+		enable_sgpr_flat_scratch_init = 0
+		enable_sgpr_private_segment_size = 0
+		enable_sgpr_grid_workgroup_count_x = 0
+		enable_sgpr_grid_workgroup_count_y = 0
+		enable_sgpr_grid_workgroup_count_z = 0
+		enable_ordered_append_gds = 0
+		private_element_size = 1
+		is_ptr64 = 1
+		is_dynamic_callstack = 0
+		is_debug_enabled = 0
+		is_xnack_enabled = 0
+		workitem_private_segment_byte_size = 0
+		workgroup_group_segment_byte_size = 0
+		gds_segment_byte_size = 0
+		kernarg_segment_byte_size = 64
+		workgroup_fbarrier_count = 0
+		wavefront_sgpr_count = 16
+		workitem_vgpr_count = 6
+		reserved_vgpr_first = 0
+		reserved_vgpr_count = 0
+		reserved_sgpr_first = 0
+		reserved_sgpr_count = 0
+		debug_wavefront_private_segment_offset_sgpr = 0
+		debug_private_segment_buffer_sgpr = 0
+		kernarg_segment_alignment = 4
+		group_segment_alignment = 4
+		private_segment_alignment = 4
+		wavefront_size = 6
+		call_convention = -1
+		runtime_loader_kernel_symbol = 0         
+	.end_amd_kernel_code_t
+
+	s_endpgm
