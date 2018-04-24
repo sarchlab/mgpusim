2cb72c60414e643735cd7f7f08798e33d77fad72
Yifan Sun <sunyifan112358@gmail.com>
2018-04-24 10:13:21 -0400

    This should fix the acceptance test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -4,7 +4,7 @@ set -e
 
 cd samples/firsim
 go build
-./firsim -verify -dataSize-8192
+./firsim -verify -dataSize=8192
 ./firsim -timing -verify -dataSize=8192
 ./firsim -parallel -verify -dataSize=8192
 ./firsim -timing -parallel -verify -dataSize=8192
