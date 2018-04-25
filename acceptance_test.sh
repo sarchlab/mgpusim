9c65b9fbd71aecaa39c5da360077cf82394b63f7
Yifan Sun <sunyifan112358@gmail.com>
2018-04-25 14:10:12 -0400

    Fix acceptance test
    Former-commit-id: 94c01c6569d10141a66d7d1358af2ba1c21fb3aa
    Former-commit-id: a0fa0ffdbfe58a8adfc9f32ffc161c61d5b84613

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-#cd samples/firsim
-#go build
-#./firsim -verify -dataSize=8192
-#./firsim -timing -verify -dataSize=8192
-#./firsim -parallel -verify -dataSize=8192
-#./firsim -timing -parallel -verify -dataSize=8192
-#cd ../../
+cd samples/firsim
+go build
+./firsim -verify -dataSize=8192
+./firsim -timing -verify -dataSize=8192
+./firsim -parallel -verify -dataSize=8192
+./firsim -timing -parallel -verify -dataSize=8192
+cd ../../
 
 cd samples/kmeanssim
 go build
