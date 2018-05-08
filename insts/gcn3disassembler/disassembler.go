cdfb454f1dc03263e3186f35236b33b229da7e66
Yifan Sun <sunyifan112358@gmail.com>
2018-05-07 20:34:14 -0400

    branch operation print label in disassembly
    Former-commit-id: ba41b22a539f15038cd995f465850977f35973d4
    Former-commit-id: ce633b8b3f0f470cf6eb9d808646cba30f78fab5

--- insts/gcn3disassembler/disassembler.go
+++ insts/gcn3disassembler/disassembler.go
@@ -19,7 +19,7 @@ func main() {
 
 	_, filename := filepath.Split(path)
 	fmt.Fprintf(os.Stdout, "\n%s:\tfile format ELF64-amdgpu\n", filename)
-	fmt.Fprintf(os.Stdout, "\nDisassembly of section .text:\n")
+	fmt.Fprintf(os.Stdout, "\nDisassembly of section .text:")
 
 	disasm := insts.NewDisassembler()
 
