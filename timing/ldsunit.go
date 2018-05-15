b5131cb66a91b30cec3d7416757a41f3380c9468
Yifan Sun <sunyifan112358@gmail.com>
2018-05-15 15:16:23 -0400

    There are some mistakes

---
+++ timing/ldsunit.go
@@ -0,0 +1,95 @@
+package timing
+
+import (
+	"gitlab.com/yaotsu/core"
+	"gitlab.com/yaotsu/gcn3/emu"
+)
+
+// A LDSUnit performs Scalar operations
+type LDSUnit struct {
+	cu *ComputeUnit
+
+	scratchpadPreparer ScratchpadPreparer
+	alu                *emu.ALUImpl
+
+	toRead  *Wavefront
+	toExec  *Wavefront
+	toWrite *Wavefront
+}
+
+// NewLDSUnit creates a new Scalar unit, injecting the dependency of
+// the compute unit.
+func NewLDSUnit(
+	cu *ComputeUnit,
+	scratchpadPreparer ScratchpadPreparer,
+	alu *emu.ALUImpl,
+) *LDSUnit {
+	u := new(LDSUnit)
+	u.cu = cu
+	u.scratchpadPreparer = scratchpadPreparer
+	u.alu = alu
+	return u
+}
+
+// CanAcceptWave checks if the buffer of the read stage is occupied or not
+func (u *LDSUnit) CanAcceptWave() bool {
+	return u.toRead == nil
+}
+
+// AcceptWave moves one wavefront into the read buffer of the Scalar unit
+func (u *LDSUnit) AcceptWave(wave *Wavefront, now core.VTimeInSec) {
+	u.toRead = wave
+	u.cu.InvokeHook(u.toRead, u.cu, core.Any, &InstHookInfo{now, "ReadStart"})
+}
+
+// Run executes three pipeline stages that are controlled by the LDSUnit
+func (u *LDSUnit) Run(now core.VTimeInSec) {
+	u.runWriteStage(now)
+	u.runExecStage(now)
+	u.runReadStage(now)
+}
+
+func (u *LDSUnit) runReadStage(now core.VTimeInSec) {
+	if u.toRead == nil {
+		return
+	}
+
+	if u.toExec == nil {
+		u.scratchpadPreparer.Prepare(u.toRead, u.toRead)
+		u.cu.InvokeHook(u.toRead, u.cu, core.Any, &InstHookInfo{now, "ReadEnd"})
+		u.cu.InvokeHook(u.toRead, u.cu, core.Any, &InstHookInfo{now, "ExecStart"})
+
+		u.toExec = u.toRead
+		u.toRead = nil
+	}
+}
+
+func (u *LDSUnit) runExecStage(now core.VTimeInSec) {
+	if u.toExec == nil {
+		return
+	}
+
+	if u.toWrite == nil {
+		u.alu.LDS = u.toExec.WG.LDS
+		u.alu.Run(u.toExec)
+		u.cu.InvokeHook(u.toExec, u.cu, core.Any, &InstHookInfo{now, "ExecEnd"})
+		u.cu.InvokeHook(u.toExec, u.cu, core.Any, &InstHookInfo{now, "WriteStart"})
+
+		u.toWrite = u.toExec
+		u.toExec = nil
+	}
+}
+
+func (u *LDSUnit) runWriteStage(now core.VTimeInSec) {
+	if u.toWrite == nil {
+		return
+	}
+
+	u.scratchpadPreparer.Commit(u.toWrite, u.toWrite)
+
+	u.cu.InvokeHook(u.toWrite, u.cu, core.Any, &InstHookInfo{now, "WriteEnd"})
+	u.cu.InvokeHook(u.toWrite, u.cu, core.Any, &InstHookInfo{now, "Completed"})
+
+	u.toWrite.State = WfReady
+	u.toWrite = nil
+}
