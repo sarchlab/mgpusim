9e05b2730c70ce865d4ad10cc2f51efcd9909009
Yuhui Bao <julie.bao32@gmail.com>
2018-06-13 13:20:31 -0400

    ground_truth
    Former-commit-id: 7990ed6a490c11118253616931c04a1029cea819
    Former-commit-id: ce0105c6296028bedd4547d3471aa23f0400a25d

--- microbench/ground_truth.py
+++ microbench/ground_truth.py
@@ -20,24 +20,27 @@ def run_benchmark(cmd, cwd):
 
 def main():
     """ main function """
-    process = subprocess.Popen("make", shell=True, cwd="empty_kernel",
-                               stdout=subprocess.DEVNULL)
-    process.wait()
+    data_columns = ['benchmark','time']
+    df = pd.DataFrame(columns=data_columns)
 
-    data_columns = ['benchmark', 'time']
-    data = pd.DataFrame(columns=data_columns)
+    benchmark_arr = ['empty_kernel','atomics','barrier','bitwise_ops','branching','dp_add','dp_div','dp_mul','valu_add','valu_div','valu_mul']
+    for bm in benchmark_arr:
+        process = subprocess.Popen("make", shell=True, cwd=bm,
+                                stdout=subprocess.DEVNULL)
+        process.wait()
 
-    for i in range(0, 20):
-        time = run_benchmark('./kernel', 'empty_kernel')
-        data = data.append(
-            pd.DataFrame([['empty_kernel', time]], columns=data_columns))
+        for i in range(1, 21):
+            time = run_benchmark('./kernel', bm)
+            df = df.append(
+                pd.DataFrame([[bm, time]], columns=data_columns))
 
-    data = data.reset_index()
 
+    df = df.reset_index()
+    
     plt.figure()
-    data.plot()
+    df.plot()
     plt.savefig('empty_kernel.pdf')
-    data.to_csv('ground_truth.csv')
+    df.to_csv('ground_truth.csv')
 
 
 if __name__ == '__main__':
