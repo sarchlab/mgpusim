7f128ae62a38031b44a6fdf4f9d8eb93c8a00e1f
Yifan Sun <sunyifan112358@gmail.com>
2018-06-20 22:59:12 -0400

    An L1V_Read benchmark
    Former-commit-id: af34fdc7e3a1195cff9df63d3b6fe41d5786c2ff
    Former-commit-id: 0491880316cbdfc6ddf47de0ba3d2eb039aa7497

---
+++ microbench/l1v_read/microbench/kernels.s
@@ -0,0 +1,87 @@
+	.text
+	.hsa_code_object_version 2,1
+	.hsa_code_object_isa 8,0,3,"AMD","AMDGPU"
+	.globl	microbench              ; -- Begin function microbench
+	.p2align	8
+	.type	microbench,@function
+	.amdgpu_hsa_kernel microbench
+microbench:                             ; @microbench
+	.amd_kernel_code_t
+		amd_code_version_major = 1
+		amd_code_version_minor = 1
+		amd_machine_kind = 1
+		amd_machine_version_major = 8
+		amd_machine_version_minor = 0
+		amd_machine_version_stepping = 3
+		kernel_code_entry_byte_offset = 256
+		kernel_code_prefetch_byte_size = 0
+		max_scratch_backing_memory_byte_size = 0
+		granulated_workitem_vgpr_count = 1
+		granulated_wavefront_sgpr_count = 2
+		priority = 0
+		float_mode = 192
+		priv = 0
+		enable_dx10_clamp = 1
+		debug_mode = 0
+		enable_ieee_mode = 1
+		enable_sgpr_private_segment_wave_byte_offset = 1
+		user_sgpr_count = 8
+		enable_trap_handler = 1
+		enable_sgpr_workgroup_id_x = 1
+		enable_sgpr_workgroup_id_y = 0
+		enable_sgpr_workgroup_id_z = 0
+		enable_sgpr_workgroup_info = 0
+		enable_vgpr_workitem_id = 0
+		enable_exception_msb = 0
+		granulated_lds_size = 0
+		enable_exception = 0
+		enable_sgpr_private_segment_buffer = 1
+		enable_sgpr_dispatch_ptr = 0
+		enable_sgpr_queue_ptr = 0
+		enable_sgpr_kernarg_segment_ptr = 1
+		enable_sgpr_dispatch_id = 0
+		enable_sgpr_flat_scratch_init = 1
+		enable_sgpr_private_segment_size = 0
+		enable_sgpr_grid_workgroup_count_x = 0
+		enable_sgpr_grid_workgroup_count_y = 0
+		enable_sgpr_grid_workgroup_count_z = 0
+		enable_ordered_append_gds = 0
+		private_element_size = 1
+		is_ptr64 = 1
+		is_dynamic_callstack = 0
+		is_debug_enabled = 0
+		is_xnack_enabled = 0
+		workitem_private_segment_byte_size = 32
+		workgroup_group_segment_byte_size = 0
+		gds_segment_byte_size = 0
+		kernarg_segment_byte_size = 48
+		workgroup_fbarrier_count = 0
+		wavefront_sgpr_count = 20
+		workitem_vgpr_count = 5
+		reserved_vgpr_first = 0
+		reserved_vgpr_count = 0
+		reserved_sgpr_first = 0
+		reserved_sgpr_count = 0
+		debug_wavefront_private_segment_offset_sgpr = 0
+		debug_private_segment_buffer_sgpr = 0
+		kernarg_segment_alignment = 4
+		group_segment_alignment = 4
+		private_segment_alignment = 4
+		wavefront_size = 6
+		call_convention = -1
+		runtime_loader_kernel_symbol = 0
+	.end_amd_kernel_code_t
+; %bb.0:
+	s_load_dwordx2 s[6:7], s[4:5], 0x0
+	s_load_dword s8, s[4:5], 0x8
+	s_mov_b32 s1, 0
+
+BB0_2:                                  ;   in Loop: Header=BB0_1 Depth=1
+	flat_load_dword v0, v[6:7]
+	s_add_u32 s1, s1, 1
+	s_cmp_eq_u32 s1, s8
+	s_waitcnt vmcnt(0)
+	s_cbranch_scc1 BB0_2
+
+BB0_5:
+	s_endpgm
