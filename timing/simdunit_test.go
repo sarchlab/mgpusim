a33a0f0b84606b6144a9c2fac62123e13b18708d
Yifan Sun <sunyifan112358@gmail.com>
2018-07-11 07:55:25 -0400

    Update the SIMD unit logic
    Former-commit-id: fe5f3ec9e7cd59e40f6e4db93cebb82d5879f710
    Former-commit-id: f3383af2cc6ec702f67b8e61f266aa397c3e67df

--- timing/simdunit_test.go
+++ timing/simdunit_test.go
@@ -23,62 +23,57 @@ var _ = Describe("SIMD Unit", func() {
 
 	It("should allow accepting wavefront", func() {
 		// wave := new(Wavefront)
-		bu.toRead = nil
+		bu.toExec = nil
 		Expect(bu.CanAcceptWave()).To(BeTrue())
 	})
 
-	It("should not allow accepting wavefront is the read stage buffer is occupied", func() {
-		bu.toRead = new(Wavefront)
+	It("should not allow accepting wavefront if the SIMD is executing an instruction", func() {
+		bu.toExec = new(Wavefront)
 		Expect(bu.CanAcceptWave()).To(BeFalse())
 	})
 
 	It("should accept wave", func() {
 		wave := new(Wavefront)
 		bu.AcceptWave(wave, 10)
-		Expect(bu.toRead).To(BeIdenticalTo(wave))
+		Expect(bu.toExec).To(BeIdenticalTo(wave))
+		Expect(bu.cycleLeft).To(Equal(4))
 	})
 
 	It("should run", func() {
-		wave1 := new(Wavefront)
-		wave2 := new(Wavefront)
-		wave3 := new(Wavefront)
-		wave3.State = WfRunning
+		wave := new(Wavefront)
+		wave.State = WfRunning
 
-		bu.toRead = wave1
-		bu.toExec = wave2
-		bu.toWrite = wave3
-		bu.execCycleLeft = 1
+		bu.toExec = wave
+		bu.cycleLeft = 1
 
 		bu.Run(10)
 
-		Expect(wave3.State).To(Equal(WfReady))
-		Expect(bu.toWrite).To(BeIdenticalTo(wave2))
-		Expect(bu.toExec).To(BeIdenticalTo(wave1))
-		Expect(bu.execCycleLeft).To(Equal(4))
-		Expect(bu.toRead).To(BeNil())
+		Expect(wave.State).To(Equal(WfReady))
+		Expect(bu.toExec).To(BeNil())
+		Expect(bu.cycleLeft).To(Equal(0))
 
-		Expect(sp.wfPrepared).To(BeIdenticalTo(wave1))
-		Expect(alu.wfExecuted).To(BeIdenticalTo(wave2))
-		Expect(sp.wfCommitted).To(BeIdenticalTo(wave3))
+		Expect(sp.wfPrepared).To(BeIdenticalTo(wave))
+		Expect(alu.wfExecuted).To(BeIdenticalTo(wave))
+		Expect(sp.wfCommitted).To(BeIdenticalTo(wave))
 	})
 
-	It("should spend 4 cycles in execution", func() {
-		wave1 := new(Wavefront)
-		wave2 := new(Wavefront)
-		wave3 := new(Wavefront)
-		wave3.State = WfRunning
-
-		bu.toRead = wave1
-		bu.toExec = wave2
-		bu.toWrite = wave3
-		bu.execCycleLeft = 4
-
-		bu.Run(10)
-
-		Expect(wave3.State).To(Equal(WfReady))
-		Expect(bu.toWrite).To(BeNil())
-		Expect(bu.toExec).To(BeIdenticalTo(wave2))
-		Expect(bu.execCycleLeft).To(Equal(3))
-		Expect(bu.toRead).To(BeIdenticalTo(wave1))
-	})
+	//It("should spend 4 cycles in execution", func() {
+	//	wave1 := new(Wavefront)
+	//	wave2 := new(Wavefront)
+	//	wave3 := new(Wavefront)
+	//	wave3.State = WfRunning
+	//
+	//	bu.toRead = wave1
+	//	bu.toExec = wave2
+	//	bu.toWrite = wave3
+	//	bu.cycleLeft = 4
+	//
+	//	bu.Run(10)
+	//
+	//	Expect(wave3.State).To(Equal(WfReady))
+	//	Expect(bu.toWrite).To(BeNil())
+	//	Expect(bu.toExec).To(BeIdenticalTo(wave2))
+	//	Expect(bu.cycleLeft).To(Equal(3))
+	//	Expect(bu.toRead).To(BeIdenticalTo(wave1))
+	//})
 })
