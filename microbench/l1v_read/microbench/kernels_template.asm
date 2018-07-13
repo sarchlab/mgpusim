ed90283924227e0762660c10659421761ea56506
Yifan Sun <sunyifan112358@gmail.com>
2018-07-12 21:40:45 -0400

    L1_read
    Former-commit-id: 937f1361a76209662f99882df46c79933479c5ca
    Former-commit-id: 52fac80e9564a9940393bf9da74b324c002a3a60

---
+++ microbench/l1v_read/microbench/kernels_template.asm
@@ -0,0 +1,28 @@
+.hsa_code_object_version 2,1
+.hsa_code_object_isa 8,0,3,"AMD","AMDGPU"
+
+.text
+.p2align        8
+.amdgpu_hsa_kernel microbench
+		
+microbench:                             ; @microbench
+
+.amd_kernel_code_t
+kernarg_segment_byte_size = 32
+wavefront_sgpr_count = 8
+workitem_vgpr_count = 5
+.end_amd_kernel_code_t
+
+BB0_0:
+	s_load_dwordx2 s[6:7], s[4:5], 0x0
+	s_load_dword s8, s[4:5], 0x8
+	s_mov_b32 s1, 0
+	v_mov_b32 v1, s6
+	v_mov_b32 v2, s7
+
+BB0_1:
+	{0}
+
+BB0_5:
+	s_endpgm
+
--- microbench/l1v_read/run.py
+++ microbench/l1v_read/run.py
@@ -1,42 +1,99 @@
 import re
 import subprocess
+import argparse
+import os
+import sys
+sys.path.append(os.getcwd() + '/../common/')
+from benchmarking import *
 
-import numpy as np
+
 import pandas as pd
 
-def run_benchmark(cmd, cwd):
-    process = subprocess.Popen(cmd, shell=True, cwd=cwd,
-                               stdout=subprocess.PIPE)
-    (stdout, _) = process.communicate()
+data_columns = ['benchmark', 'env', 'num_access', 'time']
 
-    m = re.search(r'Kernel: \[([0-9.]+) - ([0-9.]+)]', str(stdout))
-    return float(m.group(2)) - float(m.group(1))
+def generate_benchmark(count):
+    with open('microbench/kernels_template.asm', 'r') as template_file:
+        template = template_file.read()
+
+    insts = ''
+    for i in range(0, count):
+        insts += 'flat_load_dword v0, v[1:2]\n'
+        insts += 's_waitcnt vmcnt(0)\n'
+    kernel = template.format(insts)
+
+    with open('microbench/kernels.asm', 'w') as kernel_file:
+        kernel_file.write(kernel)
+
+    p = subprocess.Popen('make kernels.hsaco', cwd='microbench',
+                         shell=True)
+    p.wait()
 
 
-def run():
+def run_on_simulator(num_access):
     """ run benchmark and retuns a data frame that represents its result """
-    data_columns = ['benchmark', 'arg', 'time']
     data = pd.DataFrame(columns=data_columns)
 
     process = subprocess.Popen("go build", shell=True, cwd='.',
                             stdout=subprocess.DEVNULL)
     process.wait()
 
-    for size in range(10000, 100000, 10000):
-        print(size)
-        time = run_benchmark('./l1v_read -timing -repeat ' + str(size), '.')
+    duration = run_benchmark_on_simulator('./l1v_read -timing', os.getcwd())
+    entry = ['alu', 'sim', num_access , duration]
+    print(entry)
+    data = data.append(
+        pd.DataFrame([entry], columns=data_columns),
+        ignore_index=True,
+    )
+
+    return data
+
+
+def run_on_gpu(num_access, repeat):
+    data = pd.DataFrame(columns=data_columns)
+
+    for i in range(0, repeat):
+        duration = run_benchmark_on_gpu(
+            './kernel', os.getcwd() + '/microbench/')
+        entry = ['alu', 'gpu', num_access, duration]
+        print(entry)
         data = data.append(
-            pd.DataFrame([['l1v_read', size, time]], columns=data_columns),
+            pd.DataFrame([entry], columns=data_columns),
             ignore_index=True,
         )
 
     return data
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='L1_Read microbenchmark')
+    parser.add_argument('--gpu', dest='gpu', action='store_true')
+    parser.add_argument('--sim', dest='sim', action='store_true')
+    parser.add_argument('--repeat', type=int, default=20)
+    args = parser.parse_args()
+    return args
+
+
 def main():
-    data = run()
-    data.to_csv('gcn3sim_r9nano.csv')
-    grouped = data.groupby('arg')
-    print(data.describe())
+    args = parse_args()
+
+    num_access_list = range(0, 129, 4)
+
+    if args.gpu:
+        data = pd.DataFrame(columns=data_columns)
+        for num_access in num_access_list:
+            generate_benchmark(num_access)
+            results = run_on_gpu(num_access, args.repeat)
+            data = data.append(results, ignore_index=True)
+        data.to_csv('gpu.csv')
+
+    if args.sim:
+        data = pd.DataFrame(columns=data_columns)
+        for num_access in num_access_list:
+            generate_benchmark(num_access)
+            results = run_on_simulator(num_access)
+            data = data.append(results, ignore_index=True)
+        data.to_csv('sim.csv')
+
 
 if __name__ == '__main__':
     main()
