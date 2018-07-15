c7f3e89edb393167deef31e33e209fef4621a32d
Yifan Sun <sunyifan112358@gmail.com>
2018-07-15 10:10:26 -0400

    More micro-benchmark result
    Former-commit-id: a73b1de31d11e4a6e4b2fd17e2fb06d20d5b43da
    Former-commit-id: ac0f6db9d432102bf838c95bcadf2233276e6586

--- microbench/l2_read/microbench/kernels_template.asm
+++ microbench/l2_read/microbench/kernels_template.asm
@@ -8,20 +8,19 @@
 microbench:                             ; @microbench
 
 .amd_kernel_code_t
+enable_sgpr_kernarg_segment_ptr = 1
 kernarg_segment_byte_size = 32
 wavefront_sgpr_count = 8
 workitem_vgpr_count = 5
 .end_amd_kernel_code_t
 
 BB0_0:
-	s_load_dwordx2 s[6:7], s[4:5], 0x0
-	s_load_dword s8, s[4:5], 0x8
-	s_mov_b32 s1, 0
+	s_load_dwordx2 s[6:7], s[0:1], 0x0
 	v_mov_b32 v1, s6
 	v_mov_b32 v2, s7
-	v_mov_b32 v3, 64
 	v_mov_b32 v4, s6
 	v_mov_b32 v5, s7
+	v_mov_b32 v3, 64
 
 BB0_1:
 
