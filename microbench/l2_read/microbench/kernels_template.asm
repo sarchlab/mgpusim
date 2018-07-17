103b29818c24fc9d64cc240189acb85aba4949c4
Yifan Sun <sunyifan112358@gmail.com>
2018-07-16 20:43:03 -0400

    Update l2_read benchmark

--- microbench/l2_read/microbench/kernels_template.asm
+++ microbench/l2_read/microbench/kernels_template.asm
@@ -11,7 +11,7 @@ microbench:                             ; @microbench
 enable_sgpr_kernarg_segment_ptr = 1
 kernarg_segment_byte_size = 32
 wavefront_sgpr_count = 8
-workitem_vgpr_count = 5
+workitem_vgpr_count = 8
 .end_amd_kernel_code_t
 
 BB0_0:
--- microbench/l2_read/run.py
+++ microbench/l2_read/run.py
@@ -85,7 +85,7 @@ def parse_args():
 def main():
     args = parse_args()
 
-    num_access_list = range(32, 49, 4)
+    num_access_list = range(0, 129, 1)
 
     if args.gpu:
         data = pd.DataFrame(columns=data_columns)
