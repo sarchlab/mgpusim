26badb9b5cb2aaa4fa4c06a957ba8ef82ccdf085
Yifan Sun <sunyifan112358@gmail.com>
2018-07-23 17:03:54 -0400

    A new benchmark that explores memory coalescing
    Former-commit-id: efd301a8eb35baf4b9b32967e950346db61e55c9
    Former-commit-id: 75300201f096c746bee37e2b15c62726c763e50a

---
+++ microbench/l1v_read_diff_addr_per_wi/microbench/kernel.cpp
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include "dispatch.hpp"
+
+using namespace amd::dispatch;
+
+class L1VRead : public Dispatch {
+private:
+  Buffer* in;
+
+public:
+  L1VRead(int argc, const char **argv)
+    : Dispatch(argc, argv) {
+  }
+
+  bool SetupCodeObject() override {
+    return LoadCodeObjectFromFile("kernels.hsaco");
+  }
+
+  bool Setup() override {
+    if (!AllocateKernarg(32)) { return false; }
+    in  = AllocateBuffer(64 * 4);
+    Kernarg(in);
+    SetGridSize(1);
+    SetWorkgroupSize(1);
+    return true;
+  }
+
+  bool Verify() override {
+    return true;
+  }
+};
+
+int main(int argc, const char** argv)
+{
+  return L1VRead(argc, argv).RunMain();
+}
