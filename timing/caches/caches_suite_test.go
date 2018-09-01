f2ff45e25604294c2213cb4b46e03b3ca92e7f17
Yifan Sun <sunyifan112358@gmail.com>
2018-09-01 09:12:41 -0400

    Starting to work on a new L1v cache
    Former-commit-id: b5e96bed1bb8dc2c02df0f9fe97325595e9b9489
    Former-commit-id: d8cd4a76f157bfaa3d1b17e28ad52bd4905e2fce

---
+++ timing/caches/caches_suite_test.go
@@ -0,0 +1,15 @@
+package caches_test
+
+import (
+	"log"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestCaches(t *testing.T) {
+	log.SetOutput(GinkgoWriter)
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Caches Suite")
+}
