d68408cca7258a381c4225bf0f2c54638898fae5
Yifan Sun <sunyifan112358@gmail.com>
2018-10-18 09:07:03 -0400

    trace in plain csv format
    Former-commit-id: 65ad3cf2d1a4e19e973e54b6609145831b14f8d8
    Former-commit-id: 5c5ca926c0ddf580237be975d9b32e7e5805590e

--- trace/insttracer.go
+++ trace/insttracer.go
@@ -1,32 +1,25 @@
 package trace
 
 import (
+	"fmt"
 	"io"
-	"log"
 	"reflect"
-	"sort"
 	"sync"
 
-	"encoding/binary"
-
-	"github.com/golang/protobuf/proto"
 	"gitlab.com/akita/akita"
 	"gitlab.com/akita/gcn3/timing"
-	"gitlab.com/akita/gcn3/trace/instpb"
 )
 
 // A InstTracer is a LogHook that keep record of instruction execution status
 type InstTracer struct {
-	mutex        sync.Mutex
-	writer       io.Writer
-	tracingInsts map[*timing.Inst]*instpb.Inst
+	mutex  sync.Mutex
+	writer io.Writer
 }
 
 // NewInstTracer creates a new InstTracer
 func NewInstTracer(writer io.Writer) *InstTracer {
 	t := new(InstTracer)
 	t.writer = writer
-	t.tracingInsts = make(map[*timing.Inst]*instpb.Inst)
 	return t
 }
 
@@ -46,130 +39,9 @@ func (t *InstTracer) Func(item interface{}, domain akita.Hookable, info interfac
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 
-	wf := item.(*timing.Wavefront)
 	instInfo := info.(*timing.InstHookInfo)
 	inst := instInfo.Inst
 
-	instTraceItem, ok := t.tracingInsts[inst]
-	if !ok {
-		instTraceItem = new(instpb.Inst)
-		instTraceItem.Events = make([]*instpb.Event, 0)
-		t.tracingInsts[inst] = instTraceItem
-	}
-
-	switch instInfo.Stage {
-	case "FetchStart":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_FetchStart,
-			},
-		)
-	case "FetchDone":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_FetchDone,
-			},
-		)
-	case "Issue":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_Issue,
-			})
-	case "DecodeStart":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_DecodeStart,
-			})
-	case "DecodeDone":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_DecodeDone,
-			})
-	case "ReadStart":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_ReadStart,
-			})
-	case "ReadDone":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_ReadDone,
-			})
-	case "ExecStart":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_ExecStart,
-			})
-	case "ExecDone":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_ExecDone,
-			})
-	case "WriteStart":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_WriteStart,
-			})
-	case "WriteDone":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_WriteDone,
-			})
-	case "WaitMem":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_WaitMem,
-			})
-	case "MemReturn":
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_MemReturn,
-			})
-	case "Completed":
-		instTraceItem.Id = inst.ID
-		instTraceItem.Asm = inst.String(nil)
-		instTraceItem.WavefrontId = uint32(wf.FirstWiFlatID)
-		instTraceItem.SimdId = uint32(wf.SIMDID)
-		instTraceItem.Events = append(instTraceItem.Events,
-			&instpb.Event{
-				Time:  float64(instInfo.Now),
-				Stage: instpb.Stage_Complete,
-			},
-		)
-
-		sort.Slice(instTraceItem.Events, func(i, j int) bool {
-			return instTraceItem.Events[i].Stage < instTraceItem.Events[j].Stage
-		})
-
-		data, err := proto.Marshal(instTraceItem)
-		if err != nil {
-			log.Panic(err)
-		}
-
-		size := uint32(len(data))
-		err = binary.Write(t.writer, binary.LittleEndian, size)
-		if err != nil {
-			log.Panic(err)
-		}
-
-		_, err = t.writer.Write(data)
-		if err != nil {
-			log.Panic(err)
-		}
-
-		delete(t.tracingInsts, inst)
-	}
+	fmt.Fprintf(t.writer, "%.15f,%s,%s,%s\n",
+		instInfo.Now, "", instInfo.Stage, inst.String(nil))
 }
