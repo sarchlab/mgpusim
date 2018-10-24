1ffa8a4015f1229d4ec2bfd0d31c7bab64332589
Yifan Sun <sunyifan112358@gmail.com>
2018-10-24 09:45:29 -0400

    Platform set flag to report kernel time

--- platform/build.go
+++ platform/build.go
@@ -12,6 +12,7 @@ var UseParallelEngine bool
 var DebugISA bool
 var TraceInst bool
 var TraceMem bool
+var ReportKernelTime bool
 
 // BuildEmuPlatform creates a simple platform for emulation purposes
 func BuildEmuPlatform() (
@@ -42,6 +43,11 @@ func BuildEmuPlatform() (
 	}
 	gpu, globalMem := gpuBuilder.BuildEmulationGPU()
 	gpuDriver.RegisterGPU(gpu)
+
+	if ReportKernelTime {
+		kernelTimeCountner := new(driver.KernelTimeCounter)
+		gpuDriver.AcceptHook(kernelTimeCoutner)
+	}
 
 	connection.PlugIn(gpuDriver.ToGPUs)
 	connection.PlugIn(gpu.ToDriver)
@@ -83,6 +89,11 @@ func BuildR9NanoPlatform() (
 
 	gpu, globalMem := gpuBuilder.BuildR9Nano()
 	gpuDriver.RegisterGPU(gpu)
+
+	if ReportKernelTime {
+		kernelTimeCountner := new(driver.KernelTimeCounter)
+		gpuDriver.AcceptHook(kernelTimeCoutner)
+	}
 
 	connection.PlugIn(gpuDriver.ToGPUs)
 	connection.PlugIn(gpu.ToDriver)
