cda103bc44aa1940fe7049d356a06a8e838e612c
Yifan Sun <sunyifan112358@gmail.com>
2018-11-08 18:13:59 -0500

    Matrix multiplication benchmark

---
+++ samples/matrixmultiplication/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+
+	"gitlab.com/akita/gcn3/benchmarks/amdappsdk/matrixmultiplication"
+
+	"gitlab.com/akita/gcn3/driver"
+	"gitlab.com/akita/gcn3/platform"
+)
+
+var (
+	gpuDriver *driver.Driver
+	benchmark *matrixmultiplication.Benchmark
+)
+
+var timing = flag.Bool("timing", false, "Run detailed timing simulation.")
+var parallel = flag.Bool("parallel", false, "Run the simulation in parallel.")
+var isaDebug = flag.Bool("debug-isa", false, "Generate the ISA debugging file.")
+var instTracing = flag.Bool("trace-inst", false, "Generate instruction trace for visualization purposes.")
+var verify = flag.Bool("verify", false, "Verify the emulation result.")
+var memTracing = flag.Bool("trace-mem", false, "Generate memory trace")
+var lengthFlag = flag.Uint("length", 64, "The number of samples to filter.")
+
+func main() {
+	configure()
+	initPlatform()
+	initBenchmark()
+	benchmark.Run()
+	if *verify {
+		benchmark.Verify()
+	}
+
+}
+
+func configure() {
+	flag.Parse()
+
+	if *parallel {
+		platform.UseParallelEngine = true
+	}
+
+	if *isaDebug {
+		platform.DebugISA = true
+	}
+
+	if *instTracing {
+		platform.TraceInst = true
+	}
+
+	if *memTracing {
+		platform.TraceMem = true
+	}
+}
+
+func initPlatform() {
+	if *timing {
+		_, _, gpuDriver, _ = platform.BuildR9NanoPlatform()
+	} else {
+		_, _, gpuDriver, _ = platform.BuildEmuPlatform()
+	}
+}
+
+func initBenchmark() {
+	benchmark = matrixmultiplication.NewBenchmark(gpuDriver)
+	benchmark.X = uint32(*lengthFlag)
+	benchmark.Y = uint32(*lengthFlag)
+	benchmark.Z = uint32(*lengthFlag)
+}
