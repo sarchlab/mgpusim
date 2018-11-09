9efeafbff475a003f810eb766711bf9e2b928163
Yifan Sun <sunyifan112358@gmail.com>
2018-11-08 21:24:01 -0500

    One more instruction
    Former-commit-id: ac07e98047cbc20b4ff06639c0bb9a91df769b21
    Former-commit-id: 3e122528db2bdc26a30f5cf9f3f8a6f4606686de

--- emu/alusop1.go
+++ emu/alusop1.go
@@ -9,6 +9,8 @@ func (u *ALUImpl) runSOP1(state InstEmuState) {
 		u.runSMOVB32(state)
 	case 1:
 		u.runSMOVB64(state)
+	case 4:
+		u.runSNOTU32(state)
 	case 28:
 		u.runSGETPCB64(state)
 	case 32:
@@ -26,6 +28,17 @@ func (u *ALUImpl) runSMOVB32(state InstEmuState) {
 func (u *ALUImpl) runSMOVB64(state InstEmuState) {
 	sp := state.Scratchpad().AsSOP1()
 	sp.DST = sp.SRC0
+}
+
+func (u *ALUImpl) runSNOTU32(state InstEmuState) {
+	sp := state.Scratchpad().AsSOP1()
+	if sp.SRC0 == 0 {
+		sp.DST = 1
+		sp.SCC = 1
+	} else {
+		sp.DST = 0
+		sp.SCC = 0
+	}
 }
 
 func (u *ALUImpl) runSGETPCB64(state InstEmuState) {
