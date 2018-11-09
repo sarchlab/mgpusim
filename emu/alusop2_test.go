46e7568a338a9ca5e44043804a64654e433fa476
Yifan Sun <sunyifan112358@gmail.com>
2018-11-09 15:36:15 -0500

    update
    Former-commit-id: cbe998fac84f3e2ccaa605cc1f517a43c8a2c303
    Former-commit-id: 0b0cd9d07a600bf083bef3b79ceda6bb958cf1bb

--- emu/alusop2_test.go
+++ emu/alusop2_test.go
@@ -305,6 +305,21 @@ var _ = Describe("ALU", func() {
 		Expect(sp.SCC).To(Equal(byte(1)))
 	})
 
+	It("should run S_XOR_B32", func() {
+		state.inst = insts.NewInst()
+		state.inst.FormatType = insts.SOP2
+		state.inst.Opcode = 16
+
+		sp := state.Scratchpad().AsSOP2()
+		sp.SRC0 = 0xf0
+		sp.SRC1 = 0xff
+
+		alu.Run(state)
+
+		Expect(sp.DST).To(Equal(uint64(0x0f)))
+		Expect(sp.SCC).To(Equal(byte(1)))
+	})
+
 	It("should run S_XOR_B64", func() {
 		state.inst = insts.NewInst()
 		state.inst.FormatType = insts.SOP2
