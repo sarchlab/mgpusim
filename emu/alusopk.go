c9a2c2a326f275cd96ea6178067172b93bf73a78
Yifan Sun <sunyifan112358@gmail.com>
2018-11-08 22:28:20 -0500

    More instructions

--- emu/alusopk.go
+++ emu/alusopk.go
@@ -11,6 +11,8 @@ func (u *ALUImpl) runSOPK(state InstEmuState) {
 		u.runSMOVKI32(state)
 	case 3:
 		u.runSCMPKLGI32(state)
+	case 15:
+		u.runSMULKI32(state)
 	default:
 		log.Panicf("Opcode %d for SOPK format is not implemented", inst.Opcode)
 	}
@@ -30,4 +32,13 @@ func (u *ALUImpl) runSCMPKLGI32(state InstEmuState) {
 	} else {
 		sp.SCC = 0
 	}
+}
+
+func (u *ALUImpl) runSMULKI32(state InstEmuState) {
+	sp := state.Scratchpad().AsSOPK()
+	imm := asInt16(uint16(sp.IMM & 0xffff))
+	dst := asInt32(uint32(sp.DST))
+
+	sp.DST = int64ToBits(int64(int32(imm) * dst))
+
 }
