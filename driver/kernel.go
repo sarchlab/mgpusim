7e0d23694e4a79799d496e2264627be8500f4cff
Yifan Sun <sunyifan112358@gmail.com>
2018-11-24 12:42:05 -0500

    Enqueue launch kernel
    Former-commit-id: 113f0192eeb0fb3771bc56520e4e7432d8c478aa
    Former-commit-id: 0e58a4bb38e673783a8f3ac249eb22f071ff677d

--- driver/kernel.go
+++ driver/kernel.go
@@ -1,18 +1,42 @@
 package driver
 
-// func (d *Driver) updateLDSPointers(co *insts.HsaCo, kernelArgs interface{}) {
-// 	ldsSize := uint32(0)
-// 	kernArgStruct := reflect.ValueOf(kernelArgs).Elem()
-// 	for i := 0; i < kernArgStruct.NumField(); i++ {
-// 		arg := kernArgStruct.Field(i).Interface()
-// 		switch ldsPtr := arg.(type) {
-// 		case LocalPtr:
-// 			kernArgStruct.Field(i).SetUint(uint64(ldsSize))
-// 			ldsSize += uint32(ldsPtr)
-// 		}
-// 	}
-// 	co.WGGroupSegmentByteSize = ldsSize
-// }
+import (
+	"encoding/binary"
+	"reflect"
+
+	"gitlab.com/akita/gcn3/insts"
+	"gitlab.com/akita/gcn3/kernels"
+)
+
+// EnqueueLaunchKernel schedules kernel to be launched later
+func (d *Driver) EnqueueLaunchKernel(
+	queue *CommandQueue,
+	co *insts.HsaCo,
+	gridSize [3]uint32,
+	wgSize [3]uint16,
+	kernelArgs interface{},
+) {
+	dCoData := d.enqueueCopyInstructionsToGPU(queue, co)
+	dKernArgData := d.enqueueCopyKernArgsToGPU(queue, co, kernelArgs)
+	packet, dPacket := d.createAQLPacket(
+		queue, gridSize, wgSize, dCoData, dKernArgData)
+	d.enqueueLaunchKernelCommand(queue, co, packet, dPacket)
+	d.enqueueFinalFlush(queue)
+}
+
+func (d *Driver) updateLDSPointers(co *insts.HsaCo, kernelArgs interface{}) {
+	ldsSize := uint32(0)
+	kernArgStruct := reflect.ValueOf(kernelArgs).Elem()
+	for i := 0; i < kernArgStruct.NumField(); i++ {
+		arg := kernArgStruct.Field(i).Interface()
+		switch ldsPtr := arg.(type) {
+		case LocalPtr:
+			kernArgStruct.Field(i).SetUint(uint64(ldsSize))
+			ldsSize += uint32(ldsPtr)
+		}
+	}
+	co.WGGroupSegmentByteSize = ldsSize
+}
 
 // LaunchKernel is an eaiser way to run a kernel on the GCN3 simulator. It
 // launches the kernel immediately.
@@ -29,82 +53,63 @@ package driver
 // 	d.finalFlush()
 // }
 
-// func (d *Driver) copyKernArgsToGPU(co *insts.HsaCo, kernelArgs interface{}) GPUPtr {
-// 	d.updateLDSPointers(co, kernelArgs)
-// 	dKernArgData := d.AllocateMemoryWithAlignment(
-// 		uint64(binary.Size(kernelArgs)), 4096)
-// 	d.MemoryCopyHostToDevice(dKernArgData, kernelArgs)
-// 	return dKernArgData
-// }
+func (d *Driver) enqueueCopyKernArgsToGPU(
+	queue *CommandQueue,
+	co *insts.HsaCo,
+	kernelArgs interface{},
+) GPUPtr {
+	d.updateLDSPointers(co, kernelArgs)
+	dKernArgData := d.AllocateMemoryWithAlignment(
+		uint64(binary.Size(kernelArgs)), 4096)
+	d.EnqueueMemCopyH2D(queue, dKernArgData, kernelArgs)
+	return dKernArgData
+}
 
-// func (d *Driver) copyInstructionsToGPU(
-// 	co *insts.HsaCo,
-// ) GPUPtr {
-// 	dCoData := d.AllocateMemoryWithAlignment(uint64(len(co.Data)), 4096)
-// 	d.MemoryCopyHostToDevice(dCoData, co.Data)
-// 	return dCoData
-// }
+func (d *Driver) enqueueCopyInstructionsToGPU(
+	queue *CommandQueue,
+	co *insts.HsaCo,
+) GPUPtr {
+	dCoData := d.AllocateMemoryWithAlignment(uint64(len(co.Data)), 4096)
+	d.EnqueueMemCopyH2D(queue, dCoData, co.Data)
+	return dCoData
+}
 
-// func (d *Driver) createAQLPacket(
-// 	gridSize [3]uint32,
-// 	wgSize [3]uint16,
-// 	dCoData GPUPtr,
-// 	dKernArgData GPUPtr,
-// ) (*kernels.HsaKernelDispatchPacket, GPUPtr) {
-// 	packet := new(kernels.HsaKernelDispatchPacket)
-// 	packet.GridSizeX = gridSize[0]
-// 	packet.GridSizeY = gridSize[1]
-// 	packet.GridSizeZ = gridSize[2]
-// 	packet.WorkgroupSizeX = wgSize[0]
-// 	packet.WorkgroupSizeY = wgSize[1]
-// 	packet.WorkgroupSizeZ = wgSize[2]
-// 	packet.KernelObject = uint64(dCoData)
-// 	packet.KernargAddress = uint64(dKernArgData)
-// 	dPacket := d.AllocateMemoryWithAlignment(uint64(binary.Size(packet)), 4096)
-// 	d.MemoryCopyHostToDevice(dPacket, packet)
-// 	return packet, dPacket
-// }
+func (d *Driver) createAQLPacket(
+	queue *CommandQueue,
+	gridSize [3]uint32,
+	wgSize [3]uint16,
+	dCoData GPUPtr,
+	dKernArgData GPUPtr,
+) (*kernels.HsaKernelDispatchPacket, GPUPtr) {
+	packet := new(kernels.HsaKernelDispatchPacket)
+	packet.GridSizeX = gridSize[0]
+	packet.GridSizeY = gridSize[1]
+	packet.GridSizeZ = gridSize[2]
+	packet.WorkgroupSizeX = wgSize[0]
+	packet.WorkgroupSizeY = wgSize[1]
+	packet.WorkgroupSizeZ = wgSize[2]
+	packet.KernelObject = uint64(dCoData)
+	packet.KernargAddress = uint64(dKernArgData)
+	dPacket := d.AllocateMemoryWithAlignment(uint64(binary.Size(packet)), 4096)
+	d.EnqueueMemCopyH2D(queue, dPacket, packet)
+	return packet, dPacket
+}
 
-// func (d *Driver) runKernel(
-// 	co *insts.HsaCo,
-// 	packet *kernels.HsaKernelDispatchPacket,
-// 	dPacket GPUPtr,
-// ) {
-// 	gpu := d.gpus[d.usingGPU]
-// 	now := d.engine.CurrentTime() + 1e-8
+func (d *Driver) enqueueLaunchKernelCommand(
+	queue *CommandQueue,
+	co *insts.HsaCo,
+	packet *kernels.HsaKernelDispatchPacket,
+	dPacket GPUPtr,
+) {
+	cmd := &LaunchKernelCommand{
+		CodeObject: co,
+		DPacket:    dPacket,
+		Packet:     packet,
+	}
+	queue.Commands = append(queue.Commands, cmd)
+}
 
-// 	req := gcn3.NewLaunchKernelReq(now, d.ToGPUs, gpu.ToDriver)
-// 	req.HsaCo = co
-// 	req.Packet = packet
-// 	req.PacketAddress = uint64(dPacket)
-
-// 	sendErr := d.ToGPUs.Send(req)
-// 	if sendErr != nil {
-// 		log.Panic(sendErr)
-// 	}
-
-// 	d.InvokeHook(req, d, HookPosReqStart, nil)
-// 	err := d.engine.Run()
-// 	if err != nil {
-// 		log.Panic()
-// 	}
-// 	//endTime := d.engine.CurrentTime()
-// 	//fmt.Printf("Kernel: [%.012f - %.012f]\n", startTime, endTime)
-// 	//return endTime
-// }
-
-// func (d *Driver) finalFlush() {
-// 	gpu := d.gpus[d.usingGPU]
-// 	now := d.engine.CurrentTime() + 1e-8
-// 	flushCommand := gcn3.NewFlushCommand(now, d.ToGPUs, gpu.ToDriver)
-// 	sendErr := d.ToGPUs.Send(flushCommand)
-// 	if sendErr != nil {
-// 		log.Panic(sendErr)
-// 	}
-
-// 	err := d.engine.Run()
-// 	if err != nil {
-// 		log.Panic(err)
-// 	}
-
-// }
+func (d *Driver) enqueueFinalFlush(queue *CommandQueue) {
+	cmd := &FlushCommand{}
+	queue.Commands = append(queue.Commands, cmd)
+}
