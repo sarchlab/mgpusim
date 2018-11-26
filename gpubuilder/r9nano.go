209c131321b76dc1b2899e1769db577581308f4f
Yifan Sun <sunyifan112358@gmail.com>
2018-11-26 10:41:39 -0500

    Updated GPU Builder
    Former-commit-id: c35f6f32ae32a880475cf286ec43e5199a6c009f
    Former-commit-id: ecacdf43fc0ee34d9eef7cf62cf3a4c02fd81906

---
+++ gpubuilder/r9nano.go
@@ -0,0 +1,266 @@
+package gpubuilder
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gitlab.com/akita/akita"
+	"gitlab.com/akita/gcn3"
+	"gitlab.com/akita/gcn3/driver"
+	"gitlab.com/akita/gcn3/insts"
+	"gitlab.com/akita/gcn3/kernels"
+	"gitlab.com/akita/gcn3/timing"
+	"gitlab.com/akita/gcn3/timing/caches"
+	"gitlab.com/akita/gcn3/trace"
+	"gitlab.com/akita/mem"
+	"gitlab.com/akita/mem/cache"
+	memtraces "gitlab.com/akita/mem/trace"
+)
+
+type R9NanoGPUBuilder struct {
+	Engine  akita.Engine
+	Freq    akita.Freq
+	Driver  *driver.Driver
+	GPUName string
+
+	EnableISADebug    bool
+	EnableInstTracing bool
+	EnableMemTracing  bool
+
+	GPU                  *gcn3.GPU
+	InternalConn         *akita.DirectConnection
+	CP                   *gcn3.CommandProcessor
+	ACE                  *gcn3.Dispatcher
+	L1VCaches            []*caches.L1VCache
+	L1SCaches            []*caches.L1VCache
+	L1ICaches            []*caches.L1VCache
+	L2Caches             []*cache.WriteBackCache
+	DRAMs                []*mem.IdealMemController
+	LowModuleFinderForL1 *cache.InterleavedLowModuleFinder
+	LowModuleFinderForL2 *cache.InterleavedLowModuleFinder
+	DMAEngine            *gcn3.DMAEngine
+
+	MemTracer *memtraces.Tracer
+}
+
+// BuildR9 creates a pre-configure GPU similar to the AMD R9 Nano GPU.
+func (b *R9NanoGPUBuilder) Build() *gcn3.GPU {
+	b.Freq = 1000 * akita.MHz
+	b.InternalConn = akita.NewDirectConnection(b.Engine)
+
+	b.GPU = gcn3.NewGPU(b.GPUName, b.Engine)
+
+	b.buildCP()
+	b.buildMemSystem()
+	b.buildCUs()
+	b.buildDMAEngine()
+
+	b.InternalConn.PlugIn(b.GPU.ToCommandProcessor)
+	b.InternalConn.PlugIn(b.DMAEngine.ToCommandProcessor)
+	b.InternalConn.PlugIn(b.DMAEngine.ToMem)
+
+	b.GPU.InternalConnection = b.InternalConn
+
+	return b.GPU
+}
+
+func (b *R9NanoGPUBuilder) buildDMAEngine() {
+	b.DMAEngine = gcn3.NewDMAEngine(
+		fmt.Sprintf("%s.DMA", b.GPUName),
+		b.Engine,
+		b.LowModuleFinderForL2)
+	b.CP.DMAEngine = b.DMAEngine.ToCommandProcessor
+
+}
+
+func (b *R9NanoGPUBuilder) buildCP() {
+	b.CP = gcn3.NewCommandProcessor(b.GPUName+".CommandProcessor", b.Engine)
+	b.CP.Driver = b.GPU.ToCommandProcessor
+	b.GPU.CommandProcessor = b.CP.ToDriver
+
+	b.ACE = gcn3.NewDispatcher(b.GPUName+"Dispatcher", b.Engine,
+		new(kernels.GridBuilderImpl))
+	b.ACE.Freq = b.Freq
+	b.CP.Dispatcher = b.ACE.ToCommandProcessor
+
+	b.InternalConn.PlugIn(b.CP.ToDriver)
+	b.InternalConn.PlugIn(b.CP.ToDispatcher)
+	b.InternalConn.PlugIn(b.ACE.ToCommandProcessor)
+	b.InternalConn.PlugIn(b.ACE.ToCUs)
+}
+
+func (b *R9NanoGPUBuilder) buildMemSystem() {
+	if b.EnableMemTracing {
+		file, err := os.Create("mem.trace")
+		if err != nil {
+			panic(err)
+		}
+		b.MemTracer = memtraces.NewTracer(file)
+	}
+
+	b.L1ICaches = make([]*caches.L1VCache, 0, 16)
+
+	b.buildMemControllers()
+	b.buildL2Caches()
+	b.buildL1VCaches()
+	b.buildL1SCaches()
+	b.buildL1ICaches()
+}
+
+func (b *R9NanoGPUBuilder) buildL1SCaches() {
+	b.L1SCaches = make([]*caches.L1VCache, 0, 16)
+	for i := 0; i < 16; i++ {
+		sCache := caches.BuildL1VCache(
+			fmt.Sprintf("%s.L1K_%02d", b.GPUName, i),
+			b.Engine, b.Freq,
+			1,
+			6, 4, 14,
+			b.LowModuleFinderForL1)
+		b.InternalConn.PlugIn(sCache.ToCU)
+		b.InternalConn.PlugIn(sCache.ToCP)
+		b.InternalConn.PlugIn(sCache.ToL2)
+		b.L1SCaches = append(b.L1SCaches, sCache)
+		b.CP.CachesToReset = append(b.CP.CachesToReset, sCache.ToCP)
+		if b.EnableMemTracing {
+			sCache.AcceptHook(b.MemTracer)
+		}
+	}
+}
+
+func (b *R9NanoGPUBuilder) buildL1ICaches() {
+	b.L1ICaches = make([]*caches.L1VCache, 0, 16)
+	for i := 0; i < 16; i++ {
+		iCache := caches.BuildL1VCache(
+			fmt.Sprintf("%s.L1I_%02d", b.GPUName, i),
+			b.Engine, b.Freq,
+			1,
+			6, 4, 15,
+			b.LowModuleFinderForL1)
+		b.InternalConn.PlugIn(iCache.ToCU)
+		b.InternalConn.PlugIn(iCache.ToCP)
+		b.InternalConn.PlugIn(iCache.ToL2)
+		b.L1SCaches = append(b.L1SCaches, iCache)
+		b.CP.CachesToReset = append(b.CP.CachesToReset, iCache.ToCP)
+		if b.EnableMemTracing {
+			iCache.AcceptHook(b.MemTracer)
+		}
+	}
+}
+
+func (b *R9NanoGPUBuilder) buildL1VCaches() {
+	b.L1VCaches = make([]*caches.L1VCache, 0, 64)
+	cacheBuilder := new(cache.Builder)
+	cacheBuilder.Engine = b.Engine
+	cacheBuilder.LowModuleFinder = b.LowModuleFinderForL1
+	for i := 0; i < 64; i++ {
+		dCache := caches.BuildL1VCache(
+			fmt.Sprintf("%s.L1D_%02d", b.GPUName, i),
+			b.Engine, b.Freq,
+			1,
+			6, 4, 14,
+			b.LowModuleFinderForL1)
+
+		b.InternalConn.PlugIn(dCache.ToCU)
+		b.InternalConn.PlugIn(dCache.ToCP)
+		b.InternalConn.PlugIn(dCache.ToL2)
+		b.L1VCaches = append(b.L1VCaches, dCache)
+
+		b.CP.CachesToReset = append(b.CP.CachesToReset, dCache.ToCP)
+
+		if b.EnableMemTracing {
+			dCache.AcceptHook(b.MemTracer)
+		}
+	}
+}
+
+func (b *R9NanoGPUBuilder) buildL2Caches() {
+	b.L2Caches = make([]*cache.WriteBackCache, 0, 8)
+	cacheBuilder := new(cache.Builder)
+	cacheBuilder.Engine = b.Engine
+	b.LowModuleFinderForL1 = cache.NewInterleavedLowModuleFinder(4096)
+	for i := 0; i < 8; i++ {
+		cacheBuilder.LowModuleFinder = b.LowModuleFinderForL2
+		l2Cache := cacheBuilder.BuildWriteBackCache(
+			fmt.Sprintf("%s.L2_%d", b.GPUName, i), 16, 256*mem.KB, 4096)
+		b.L2Caches = append(b.L2Caches, l2Cache)
+		b.CP.L2Caches = append(b.CP.L2Caches, l2Cache)
+		l2Cache.DirectoryLatency = 0
+		l2Cache.Latency = 70
+		l2Cache.SetNumBanks(4096)
+		l2Cache.Freq = 1 * akita.GHz
+
+		b.LowModuleFinderForL1.LowModules = append(
+			b.LowModuleFinderForL1.LowModules, l2Cache.ToTop)
+		b.InternalConn.PlugIn(l2Cache.ToTop)
+		b.InternalConn.PlugIn(l2Cache.ToBottom)
+
+		if b.EnableMemTracing {
+			l2Cache.AcceptHook(b.MemTracer)
+		}
+	}
+}
+
+func (b *R9NanoGPUBuilder) buildMemControllers() {
+	b.LowModuleFinderForL2 = cache.NewInterleavedLowModuleFinder(4096)
+	for i := 0; i < 8; i++ {
+		memCtrl := mem.NewIdealMemController(
+			fmt.Sprintf("%s.DRAM_%d", b.GPUName, i),
+			b.Engine, 512*mem.GB)
+
+		b.InternalConn.PlugIn(memCtrl.ToTop)
+
+		b.LowModuleFinderForL2.LowModules = append(
+			b.LowModuleFinderForL2.LowModules, memCtrl.ToTop)
+		b.GPU.MemoryControllers = append(
+			b.GPU.MemoryControllers, memCtrl)
+
+		if b.EnableMemTracing {
+			memCtrl.AcceptHook(b.MemTracer)
+		}
+	}
+}
+
+func (b *R9NanoGPUBuilder) buildCUs() {
+	cuBuilder := timing.NewBuilder()
+	cuBuilder.Engine = b.Engine
+	cuBuilder.Freq = b.Freq
+	cuBuilder.Decoder = insts.NewDisassembler()
+	cuBuilder.ConnToInstMem = b.InternalConn
+	cuBuilder.ConnToScalarMem = b.InternalConn
+	cuBuilder.ConnToVectorMem = b.InternalConn
+
+	for i := 0; i < 64; i++ {
+		cuBuilder.CUName = fmt.Sprintf("%s.CU%02d", b.GPUName, i)
+		cuBuilder.InstMem = b.L1ICaches[i/4].ToCU
+		cuBuilder.ScalarMem = b.L1SCaches[i/4].ToCU
+
+		lowModuleFinderForCU := new(cache.SingleLowModuleFinder)
+		lowModuleFinderForCU.LowModule = b.L1VCaches[i].ToCU
+		cuBuilder.VectorMemModules = lowModuleFinderForCU
+
+		cu := cuBuilder.Build()
+		b.GPU.CUs = append(b.GPU.CUs, cu)
+		b.ACE.RegisterCU(cu.ToACE)
+
+		b.InternalConn.PlugIn(cu.ToACE)
+
+		if b.EnableISADebug && i == 0 {
+			isaDebug, err := os.Create(fmt.Sprintf("isa_%s.debug", cu.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			isaDebugger := timing.NewISADebugger(log.New(isaDebug, "", 0))
+			cu.AcceptHook(isaDebugger)
+		}
+
+		if b.EnableInstTracing {
+			isaTraceFile, err := os.Create(fmt.Sprintf("inst_%s.trace", cu.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			isaTracer := trace.NewInstTracer(isaTraceFile)
+			cu.AcceptHook(isaTracer)
+		}
+	}
+}
