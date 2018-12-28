0085b8b64de153a73fe5db10dfd7bd4b1859f5e7
Yifan Sun <sunyifan112358@gmail.com>
2018-12-28 15:21:15 -0500

    One platform, one database

--- samples/fir/fir.go
+++ samples/fir/fir.go
@@ -16,8 +16,8 @@ var (
 var timing = flag.Bool("timing", false, "Run detailed timing simulation.")
 var parallel = flag.Bool("parallel", false, "Run the simulation in parallel.")
 var isaDebug = flag.Bool("debug-isa", false, "Generate the ISA debugging file.")
-var instTracing = flag.Bool("trace-inst", false,
-	"Generate instruction trace for visualization purposes.")
+var visTracing = flag.Bool("trace-vis", false,
+	"Generate trace for visualization purposes.")
 var verify = flag.Bool("verify", false, "Verify the emulation result.")
 var memTracing = flag.Bool("trace-mem", false, "Generate memory trace")
 var numData = flag.Int("length", 4096, "The number of samples to filter.")
@@ -44,8 +44,8 @@ func configure() {
 		platform.DebugISA = true
 	}
 
-	if *instTracing {
-		platform.TraceInst = true
+	if *visTracing {
+		platform.TraceVis = true
 	}
 
 	if *memTracing {
