f3fb24ea96fbf1b79bbe298f5a9d535f4f8891d2
Yifan Sun <sunyifan112358@gmail.com>
2019-01-09 12:23:17 -0500

    trace Wf
    Former-commit-id: 4360ddf3905de97309eca5c8a5e190d3ba74e707
    Former-commit-id: 48c615cc7ae2b9dd3d54b7d1abe83b2a4f45d084

--- trace/wgwftracer.go
+++ trace/wgwftracer.go
@@ -8,34 +8,34 @@ import (
 	"sync"
 )
 
-// A WGTracer is a hook that keep record of work-group start and end time
-type WGTracer struct {
+// A WGWfTracer is a hook that keep record of work-group start and end time
+type WGWfTracer struct {
 	mutex  sync.Mutex
 	tracer *Tracer
 }
 
-// NewWGTracer creates a new WGTracer.
+// NewWGTracer creates a new WGWfTracer.
 func NewWGTracer(
 	tracer *Tracer,
-) *WGTracer {
-	t := new(WGTracer)
+) *WGWfTracer {
+	t := new(WGWfTracer)
 	t.tracer = tracer
 	return t
 }
 
-// Type of WGTracer claims that it hooks to any request type
-func (t *WGTracer) Type() reflect.Type {
+// Type of WGWfTracer claims that it hooks to any request type
+func (t *WGWfTracer) Type() reflect.Type {
 	return nil
 }
 
-// Pos of WGTracer returns akita.AnyHookPos. Since WGTracer is not standard hook
+// Pos of WGWfTracer returns akita.AnyHookPos. Since WGWfTracer is not standard hook
 // for event or request, it has to use akita.AnyHookPos position.
-func (t *WGTracer) Pos() akita.HookPos {
+func (t *WGWfTracer) Pos() akita.HookPos {
 	return akita.AnyHookPos
 }
 
 // Func defines the behavior of the tracer when the tracer is invoked.
-func (t *WGTracer) Func(
+func (t *WGWfTracer) Func(
 	item interface{},
 	domain akita.Hookable,
 	info interface{},
@@ -62,6 +62,19 @@ func (t *WGTracer) Func(
 	case timing.HookPosWGEnd:
 		wg := item.(*timing.WorkGroup)
 		t.tracer.EndTask(wg.UID, float64(hInfo.Now))
+	case timing.HookPosWfStart:
+		wf := item.(*timing.Wavefront)
+		t.tracer.CreateTask(&Task{
+			ID:           wf.UID,
+			ParentTaskID: wf.WG.UID,
+			Type:         "WF",
+			What:         "WF",
+			Where:        domain.(akita.Component).Name(),
+			Start:        float64(hInfo.Now),
+		})
+	case timing.HookPosWfEnd:
+		wf := item.(*timing.Wavefront)
+		t.tracer.EndTask(wf.UID, float64(hInfo.Now))
 	}
 }
 
