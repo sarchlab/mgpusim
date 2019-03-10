514e15d0de013bd82fc4f40813d2ee1f69bd4e7c
trinayan <trinayan16101993@gmail.com>
2019-03-09 19:01:15 -0500

    Added CU flush request and flush complete response to protocol
    Former-commit-id: a451f54c4ac198c91871a16f000abd0ed71725bc
    Former-commit-id: 54b6301b099ee11d51ea226a8430c10d292ec8e8

--- protocol.go
+++ protocol.go
@@ -17,6 +17,16 @@ type CUPipelineRestart struct {
 	restartPipeline bool
 }
 
+type CUPipelineFlush struct {
+	*akita.ReqBase
+	flushPipeline bool
+}
+
+type CUPipelineFlushRsp struct {
+	*akita.ReqBase
+	flushPipelineComplete bool
+}
+
 func NewCUPipelineDrainReq(
 	time akita.VTimeInSec,
 	src, dst akita.Port,
@@ -64,6 +74,40 @@ func NewCUPipelineRestartReq(
 	req.SetDst(dst)
 
 	req.restartPipeline = true
+
+	return req
+}
+
+func NewCUPipelineFlush(
+	time akita.VTimeInSec,
+	src, dst akita.Port,
+) *CUPipelineFlush {
+	reqBase := akita.NewReqBase()
+	req := new(CUPipelineFlush)
+	req.ReqBase = reqBase
+
+	req.SetSendTime(time)
+	req.SetSrc(src)
+	req.SetDst(dst)
+
+	req.flushPipeline = true
+
+	return req
+}
+
+func NewCUPipelineFlushRsp(
+	time akita.VTimeInSec,
+	src, dst akita.Port,
+) *CUPipelineFlushRsp {
+	reqBase := akita.NewReqBase()
+	req := new(CUPipelineFlushRsp)
+	req.ReqBase = reqBase
+
+	req.SetSendTime(time)
+	req.SetSrc(src)
+	req.SetDst(dst)
+
+	req.flushPipelineComplete = true
 
 	return req
 }
