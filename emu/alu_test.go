01f53e0da276ecb83459c1462423f7354029dafe
Yifan Sun <sunyifan112358@gmail.com>
2019-03-24 15:10:14 -0400

    Fix unit tests in emu

--- emu/alu_test.go
+++ emu/alu_test.go
@@ -48,7 +48,7 @@ var _ = Describe("ALU", func() {
 		addrConverter = &mem.InterleavingConverter{
 			InterleavingSize:    1 * mem.GB,
 			TotalNumOfElements:  1,
-			CurrentElementIndex: 1,
+			CurrentElementIndex: 0,
 			Offset:              0,
 		}
 		sAccessor = newStorageAccessor(storage, mmu, addrConverter)
