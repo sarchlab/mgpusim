eb9dda06ed032c7d08b959bc104f97f8f9d9e5c7
Yifan Sun <sunyifan112358@gmail.com>
2019-04-07 19:52:35 -0400

    Some bug fixes

--- platform/build.go
+++ platform/build.go
@@ -2,6 +2,8 @@ package platform
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"gitlab.com/akita/akita"
 	"gitlab.com/akita/gcn3"
@@ -73,7 +75,7 @@ func BuildNR9NanoPlatform(
 	} else {
 		engine = akita.NewSerialEngine()
 	}
-	//engine.AcceptHook(akita.NewEventLogger(log.New(os.Stdout, "", 0)))
+	engine.AcceptHook(akita.NewEventLogger(log.New(os.Stdout, "", 0)))
 
 	mmu := vm.NewMMU("MMU", engine, &vm.DefaultPageTableFactory{})
 	mmu.Latency = 100
