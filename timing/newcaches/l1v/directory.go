4f0c80aa1ce4cfecf1732cb0f4c3059b8ee64fa5
Yifan Sun <sunyifan112358@gmail.com>
2019-04-25 21:35:35 -0400

    directory stage read hit
    Former-commit-id: 566b5321e3e13b161fd2cefe3930bd866eca8ec6
    Former-commit-id: 8969a25fe49d48a5dd6dd23ca09a3937b2de9a30

--- timing/newcaches/l1v/directory.go
+++ timing/newcaches/l1v/directory.go
@@ -2,11 +2,16 @@ package l1v
 
 import (
 	"gitlab.com/akita/akita"
+	"gitlab.com/akita/mem/cache"
 	"gitlab.com/akita/util"
 )
 
 type directory struct {
-	inBuf util.Buffer
+	inBuf         util.Buffer
+	dir           cache.Directory
+	mshr          cache.MSHR
+	bankBufs      []util.Buffer
+	log2BlockSize uint64
 }
 
 func (d *directory) Tick(now akita.VTimeInSec) bool {
@@ -15,5 +20,52 @@ func (d *directory) Tick(now akita.VTimeInSec) bool {
 		return false
 	}
 
+	trans := item.(*transaction)
+	if trans.read != nil {
+		return d.processRead(now, trans)
+	}
+
 	panic("not implemented")
+}
+
+func (d *directory) processRead(now akita.VTimeInSec, trans *transaction) bool {
+	read := trans.read
+	addr := read.Address
+	blockSize := uint64(1 << d.log2BlockSize)
+	cacheLineID := addr / blockSize * blockSize
+
+	mshrEntry := d.mshr.Query(cacheLineID)
+	if mshrEntry != nil {
+		mshrEntry.Requests = append(mshrEntry.Requests, trans)
+		d.inBuf.Pop()
+		return true
+	}
+
+	block := d.dir.Lookup(cacheLineID)
+	if block != nil && block.IsValid {
+		if block.IsLocked {
+			return false
+		}
+
+		bankNum := getBankNum(block, d.dir.WayAssociativity(), len(d.bankBufs))
+		bankBuf := d.bankBufs[bankNum]
+		if !bankBuf.CanPush() {
+			return false
+		}
+
+		trans.block = block
+		trans.bankAction = bankActionReadHit
+		block.ReadCount++
+		d.dir.Visit(block)
+		bankBuf.Push(trans)
+
+		return true
+	}
+
+	panic("not implemented")
+
+}
+
+func getBankNum(block *cache.Block, wayAssociativity, numBanks int) int {
+	return (block.SetID*wayAssociativity + block.WayID) % numBanks
 }
--- timing/newcaches/l1v/directory_test.go
+++ timing/newcaches/l1v/directory_test.go
@@ -4,20 +4,34 @@ import (
 	gomock "github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"gitlab.com/akita/mem"
+	"gitlab.com/akita/mem/cache"
+	"gitlab.com/akita/util"
 )
 
 var _ = Describe("Directory", func() {
 	var (
 		mockCtrl *gomock.Controller
 		inBuf    *MockBuffer
+		dir      *MockDirectory
+		mshr     *MockMSHR
+		bankBuf  *MockBuffer
 		d        *directory
 	)
 
 	BeforeEach(func() {
 		mockCtrl = gomock.NewController(GinkgoT())
 		inBuf = NewMockBuffer(mockCtrl)
+		dir = NewMockDirectory(mockCtrl)
+		dir.EXPECT().WayAssociativity().Return(4).AnyTimes()
+		mshr = NewMockMSHR(mockCtrl)
+		bankBuf = NewMockBuffer(mockCtrl)
 		d = &directory{
-			inBuf: inBuf,
+			inBuf:         inBuf,
+			dir:           dir,
+			mshr:          mshr,
+			bankBufs:      []util.Buffer{bankBuf},
+			log2BlockSize: 6,
 		}
 	})
 
@@ -29,6 +43,85 @@ var _ = Describe("Directory", func() {
 		inBuf.EXPECT().Peek().Return(nil)
 		madeProgress := d.Tick(10)
 		Expect(madeProgress).To(BeFalse())
+	})
+
+	Context("read mshr hit", func() {
+		var (
+			read  *mem.ReadReq
+			trans *transaction
+		)
+
+		BeforeEach(func() {
+			read = mem.NewReadReq(6, nil, nil, 0x104, 4)
+			trans = &transaction{
+				read: read,
+			}
+			inBuf.EXPECT().Peek().Return(trans)
+		})
+
+		It("Should add to mshr entry", func() {
+			mshrEntry := &cache.MSHREntry{}
+			mshr.EXPECT().Query(uint64(0x100)).Return(mshrEntry)
+			inBuf.EXPECT().Pop()
+
+			madeProgress := d.Tick(10)
+
+			Expect(madeProgress).To(BeTrue())
+			Expect(mshrEntry.Requests).To(ContainElement(trans))
+		})
+	})
+
+	Context("read hit", func() {
+		var (
+			block *cache.Block
+			read  *mem.ReadReq
+			trans *transaction
+		)
+
+		BeforeEach(func() {
+			block = &cache.Block{
+				IsValid: true,
+			}
+			read = mem.NewReadReq(6, nil, nil, 0x104, 4)
+			trans = &transaction{
+				read: read,
+			}
+			inBuf.EXPECT().Peek().Return(trans)
+			mshr.EXPECT().Query(gomock.Any()).Return(nil)
+		})
+
+		It("should send transaction to bank", func() {
+			dir.EXPECT().Lookup(uint64(0x100)).Return(block)
+			dir.EXPECT().Visit(block)
+			bankBuf.EXPECT().CanPush().Return(true)
+			bankBuf.EXPECT().Push(gomock.Any()).
+				Do(func(t *transaction) {
+					Expect(t.block).To(BeIdenticalTo(block))
+					Expect(t.bankAction).To(Equal(bankActionReadHit))
+				})
+
+			madeProgress := d.Tick(10)
+
+			Expect(madeProgress).To(BeTrue())
+			Expect(block.ReadCount).To(Equal(1))
+		})
+
+		It("should stall if cannot send to bank", func() {
+			dir.EXPECT().Lookup(uint64(0x100)).Return(block)
+			bankBuf.EXPECT().CanPush().Return(false)
+
+			madeProgress := d.Tick(10)
+
+			Expect(madeProgress).To(BeFalse())
+		})
+
+		It("should stall if block is locked", func() {
+			block.IsLocked = true
+			dir.EXPECT().Lookup(uint64(0x100)).Return(block)
+			madeProgress := d.Tick(10)
+			Expect(madeProgress).To(BeFalse())
+		})
+
 	})
 
 })
