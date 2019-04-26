c3582c57841eb4785b126e0dbd9ee6b9fa88bef8
Yifan Sun <sunyifan112358@gmail.com>
2019-04-26 11:11:39 -0400

    Add bottom parser test for processing done rspg
    Former-commit-id: 34289d47fb6f5631771837dcb0fddb5b599d61e9
    Former-commit-id: 382e6c66303923c237c8b974a8b4e73d220b75a1

--- timing/newcaches/l1v/bottomparser_test.go
+++ timing/newcaches/l1v/bottomparser_test.go
@@ -3,21 +3,57 @@ package l1v
 import (
 	gomock "github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
+	"gitlab.com/akita/mem"
 )
 
 var _ = Describe("Bottom Parser", func() {
 	var (
-		mockCtrl *gomock.Controller
+		mockCtrl          *gomock.Controller
+		bottomPort        *MockPort
+		postCTransactions []*transaction
+		p                 *bottomParser
 	)
 
 	BeforeEach(func() {
 		mockCtrl = gomock.NewController(GinkgoT())
-
+		bottomPort = NewMockPort(mockCtrl)
+		postCTransactions = nil
+		p = &bottomParser{
+			bottomPort:   bottomPort,
+			transactions: &postCTransactions,
+		}
 	})
 
 	AfterEach(func() {
 		mockCtrl.Finish()
+	})
+
+	It("should handle write done", func() {
+		write1 := mem.NewWriteReq(4, nil, nil, 0x100)
+		preCTrans1 := &transaction{
+			write: write1,
+		}
+		write2 := mem.NewWriteReq(4, nil, nil, 0x104)
+		preCTrans2 := &transaction{
+			write: write2,
+		}
+		writeToBottom := mem.NewWriteReq(6, nil, nil, 0x100)
+		postCTrans := &transaction{
+			writeToBottom:           writeToBottom,
+			preCoalesceTransactions: []*transaction{preCTrans1, preCTrans2},
+		}
+		postCTransactions = append(postCTransactions, postCTrans)
+		done := mem.NewDoneRsp(11, nil, nil, writeToBottom.GetID())
+
+		bottomPort.EXPECT().Peek().Return(done)
+
+		madeProgress := p.Tick(12)
+
+		Expect(madeProgress).To(BeTrue())
+		Expect(preCTrans1.doneFromBottom).To(BeIdenticalTo(done))
+		Expect(preCTrans2.doneFromBottom).To(BeIdenticalTo(done))
+		Expect(postCTransactions).NotTo(ContainElement(postCTrans))
 	})
 
 })
