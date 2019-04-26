947bf9093eec67013c4fd8603a58f79fd2d33620
Yifan Sun <sunyifan112358@gmail.com>
2019-04-26 12:31:26 -0400

    Briefly done with the bottom parser stage
    Former-commit-id: 1b77c383a78045c25b26c7b75e7053b21843d669
    Former-commit-id: 6721def6627505777e781f44570b2b1ad6f53e6c
    Former-commit-id: 4b2fea70fcce847fbf794135466dfeeeb710654b

--- timing/newcaches/l1v/bottomparser.go
+++ timing/newcaches/l1v/bottomparser.go
@@ -1,16 +1,120 @@
 package l1v
 
 import (
+	"log"
+	"reflect"
+
 	"gitlab.com/akita/akita"
+	"gitlab.com/akita/mem"
+	"gitlab.com/akita/mem/cache"
 	"gitlab.com/akita/util"
 )
 
 type bottomParser struct {
-	bottomPort   akita.Port
-	bankBufs     []util.Buffer
-	transactions *[]*transaction
+	bottomPort       akita.Port
+	mshr             cache.MSHR
+	bankBufs         []util.Buffer
+	transactions     *[]*transaction
+	log2BlockSize    uint64
+	wayAssociativity int
 }
 
 func (p *bottomParser) Tick(now akita.VTimeInSec) bool {
+	item := p.bottomPort.Peek()
+	if item == nil {
+		return false
+	}
+
+	switch rsp := item.(type) {
+	case *mem.DoneRsp:
+		return p.processDoneRsp(now, rsp)
+	case *mem.DataReadyRsp:
+		return p.processDataReady(now, rsp)
+	default:
+		log.Panicf("cannot process request of type %s", reflect.TypeOf(rsp))
+	}
+
 	panic("not implemented")
+}
+
+func (p *bottomParser) processDoneRsp(
+	now akita.VTimeInSec,
+	done *mem.DoneRsp,
+) bool {
+	trans := p.findTransactionByWriteToBottomID(done.GetRespondTo())
+
+	for _, t := range trans.preCoalesceTransactions {
+		t.doneFromBottom = done
+	}
+
+	p.removeTransaction(trans)
+	p.bottomPort.Retrieve(now)
+	return true
+}
+
+func (p *bottomParser) processDataReady(
+	now akita.VTimeInSec,
+	dr *mem.DataReadyRsp,
+) bool {
+	trans := p.findTransactionByReadToBottomID(dr.GetRespondTo())
+	bankBuf := p.getBankBuf(trans.block)
+	if !bankBuf.CanPush() {
+		return false
+	}
+
+	for _, preCTrans := range trans.preCoalesceTransactions {
+		preCTrans.dataReadyFromBottom = dr
+	}
+
+	addr := trans.Address()
+	cachelineID := (addr >> p.log2BlockSize) << p.log2BlockSize
+	p.mshr.Remove(cachelineID)
+
+	trans.bankAction = bankActionWriteFetched
+	bankBuf.Push(trans)
+
+	p.removeTransaction(trans)
+	p.bottomPort.Retrieve(now)
+	return true
+}
+
+func (p *bottomParser) findTransactionByWriteToBottomID(
+	id string,
+) *transaction {
+	for _, trans := range *p.transactions {
+		if trans.writeToBottom != nil && trans.writeToBottom.ID == id {
+			return trans
+		}
+	}
+	panic("trans not found")
+}
+
+func (p *bottomParser) findTransactionByReadToBottomID(
+	id string,
+) *transaction {
+	for _, trans := range *p.transactions {
+		if trans.readToBottom != nil && trans.readToBottom.ID == id {
+			return trans
+		}
+	}
+	panic("trans not found")
+}
+
+func (p *bottomParser) removeTransaction(trans *transaction) {
+	for i, t := range *p.transactions {
+		if t == trans {
+			*p.transactions = append(
+				(*p.transactions)[:i],
+				(*p.transactions)[i+1:]...)
+			return
+		}
+	}
+	panic("trans not found")
+}
+
+func (p *bottomParser) getBankBuf(block *cache.Block) util.Buffer {
+	numWaysPerSet := p.wayAssociativity
+	blockID := block.SetID*numWaysPerSet + block.WayID
+	bankID := blockID % len(p.bankBufs)
+	return p.bankBufs[bankID]
 }
