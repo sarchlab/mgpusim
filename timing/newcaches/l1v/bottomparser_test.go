2f125b447d72fb2bdee15998aa340f035d615067
Yifan Sun <sunyifan112358@gmail.com>
2019-04-26 12:31:26 -0400

    Briefly done with the bottom parser stage
    Former-commit-id: 77050e57c18d570539d808b657be16631b640319
    Former-commit-id: 3499bafb43c747c5191555dc4d08f3f091ce1eb1

--- timing/newcaches/l1v/bottomparser_test.go
+++ timing/newcaches/l1v/bottomparser_test.go
@@ -5,12 +5,16 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"gitlab.com/akita/mem"
+	"gitlab.com/akita/mem/cache"
+	"gitlab.com/akita/util"
 )
 
 var _ = Describe("Bottom Parser", func() {
 	var (
 		mockCtrl          *gomock.Controller
 		bottomPort        *MockPort
+		bankBuf           *MockBuffer
+		mshr              *MockMSHR
 		postCTransactions []*transaction
 		p                 *bottomParser
 	)
@@ -18,10 +22,16 @@ var _ = Describe("Bottom Parser", func() {
 	BeforeEach(func() {
 		mockCtrl = gomock.NewController(GinkgoT())
 		bottomPort = NewMockPort(mockCtrl)
+		bankBuf = NewMockBuffer(mockCtrl)
+		mshr = NewMockMSHR(mockCtrl)
 		postCTransactions = nil
 		p = &bottomParser{
-			bottomPort:   bottomPort,
-			transactions: &postCTransactions,
+			bottomPort:       bottomPort,
+			mshr:             mshr,
+			bankBufs:         []util.Buffer{bankBuf},
+			transactions:     &postCTransactions,
+			log2BlockSize:    6,
+			wayAssociativity: 4,
 		}
 	})
 
@@ -29,31 +39,100 @@ var _ = Describe("Bottom Parser", func() {
 		mockCtrl.Finish()
 	})
 
-	It("should handle write done", func() {
-		write1 := mem.NewWriteReq(4, nil, nil, 0x100)
-		preCTrans1 := &transaction{
-			write: write1,
-		}
-		write2 := mem.NewWriteReq(4, nil, nil, 0x104)
-		preCTrans2 := &transaction{
-			write: write2,
-		}
-		writeToBottom := mem.NewWriteReq(6, nil, nil, 0x100)
-		postCTrans := &transaction{
-			writeToBottom:           writeToBottom,
-			preCoalesceTransactions: []*transaction{preCTrans1, preCTrans2},
-		}
-		postCTransactions = append(postCTransactions, postCTrans)
-		done := mem.NewDoneRsp(11, nil, nil, writeToBottom.GetID())
-
-		bottomPort.EXPECT().Peek().Return(done)
-
+	It("should do nothing if no respond", func() {
+		bottomPort.EXPECT().Peek().Return(nil)
 		madeProgress := p.Tick(12)
+		Expect(madeProgress).To(BeFalse())
+	})
 
-		Expect(madeProgress).To(BeTrue())
-		Expect(preCTrans1.doneFromBottom).To(BeIdenticalTo(done))
-		Expect(preCTrans2.doneFromBottom).To(BeIdenticalTo(done))
-		Expect(postCTransactions).NotTo(ContainElement(postCTrans))
+	Context("write done", func() {
+		It("should handle write done", func() {
+			write1 := mem.NewWriteReq(4, nil, nil, 0x100)
+			preCTrans1 := &transaction{
+				write: write1,
+			}
+			write2 := mem.NewWriteReq(4, nil, nil, 0x104)
+			preCTrans2 := &transaction{
+				write: write2,
+			}
+			writeToBottom := mem.NewWriteReq(6, nil, nil, 0x100)
+			postCTrans := &transaction{
+				writeToBottom:           writeToBottom,
+				preCoalesceTransactions: []*transaction{preCTrans1, preCTrans2},
+			}
+			postCTransactions = append(postCTransactions, postCTrans)
+			done := mem.NewDoneRsp(11, nil, nil, writeToBottom.GetID())
+
+			bottomPort.EXPECT().Peek().Return(done)
+			bottomPort.EXPECT().Retrieve(gomock.Any())
+
+			madeProgress := p.Tick(12)
+
+			Expect(madeProgress).To(BeTrue())
+			Expect(preCTrans1.doneFromBottom).To(BeIdenticalTo(done))
+			Expect(preCTrans2.doneFromBottom).To(BeIdenticalTo(done))
+			Expect(postCTransactions).NotTo(ContainElement(postCTrans))
+		})
+	})
+
+	Context("data ready", func() {
+		var (
+			read1, read2           *mem.ReadReq
+			preCTrans1, preCTrans2 *transaction
+			postCRead              *mem.ReadReq
+			readToBottom           *mem.ReadReq
+			block                  *cache.Block
+			postCTrans             *transaction
+			dataReady              *mem.DataReadyRsp
+		)
+
+		BeforeEach(func() {
+			read1 = mem.NewReadReq(1, nil, nil, 0x100, 4)
+			read2 = mem.NewReadReq(1, nil, nil, 0x104, 4)
+			preCTrans1 = &transaction{read: read1}
+			preCTrans2 = &transaction{read: read2}
+			postCRead = mem.NewReadReq(0, nil, nil, 0x100, 64)
+			readToBottom = mem.NewReadReq(2, nil, nil, 0x100, 64)
+			dataReady = mem.NewDataReadyRsp(4, nil, nil, readToBottom.GetID())
+			block = &cache.Block{}
+			postCTrans = &transaction{
+				block:        block,
+				read:         postCRead,
+				readToBottom: readToBottom,
+				preCoalesceTransactions: []*transaction{
+					preCTrans1,
+					preCTrans2,
+				},
+			}
+			postCTransactions = append(postCTransactions, postCTrans)
+		})
+
+		It("should stall is bank is busy", func() {
+			bottomPort.EXPECT().Peek().Return(dataReady)
+			bankBuf.EXPECT().CanPush().Return(false)
+
+			madeProgress := p.Tick(12)
+
+			Expect(madeProgress).To(BeFalse())
+		})
+
+		It("should send transaction to bank", func() {
+			bottomPort.EXPECT().Peek().Return(dataReady)
+			bottomPort.EXPECT().Retrieve(gomock.Any())
+			bankBuf.EXPECT().CanPush().Return(true)
+			bankBuf.EXPECT().Push(gomock.Any()).
+				Do(func(trans *transaction) {
+					Expect(trans.bankAction).To(Equal(bankActionWriteFetched))
+				})
+			mshr.EXPECT().Remove(uint64(0x100))
+
+			madeProgress := p.Tick(12)
+
+			Expect(madeProgress).To(BeTrue())
+			Expect(preCTrans1.dataReadyFromBottom).To(BeIdenticalTo(dataReady))
+			Expect(preCTrans2.dataReadyFromBottom).To(BeIdenticalTo(dataReady))
+			Expect(postCTransactions).NotTo(ContainElement(postCTrans))
+		})
 	})
 
 })
