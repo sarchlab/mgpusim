c5279c96f2c59b2ec0f8801b516b1277542b5d34
Yifan Sun <sunyifan112358@gmail.com>
2019-05-04 09:26:44 -0400

    Cleanup

--- platform/build.go
+++ platform/build.go
@@ -2,8 +2,6 @@ package platform
 
 import (
 	"fmt"
-	"log"
-	"os"
 
 	"gitlab.com/akita/akita"
 	"gitlab.com/akita/gcn3"
@@ -75,7 +73,7 @@ func BuildNR9NanoPlatform(
 	} else {
 		engine = akita.NewSerialEngine()
 	}
-	engine.AcceptHook(akita.NewEventLogger(log.New(os.Stdout, "", 0)))
+	//engine.AcceptHook(akita.NewEventLogger(log.New(os.Stdout, "", 0)))
 
 	mmu := vm.NewMMU("MMU", engine, &vm.DefaultPageTableFactory{})
 	mmu.Latency = 100
