5858f448ce45a6e4e4cd7ea4792c814df7043170
Yifan Sun <sunyifan112358@gmail.com>
2019-06-20 22:48:22 -0400

    matrix multiplication support non-square matrix

--- samples/matrixmultiplication/main.go
+++ samples/matrixmultiplication/main.go
@@ -11,7 +11,9 @@ import (
 	"gitlab.com/akita/gcn3/samples/runner"
 )
 
-var lengthFlag = flag.Uint("length", 64, "The number of samples to filter.")
+var xFlag = flag.Uint("x", 64, "The height of the first matrix.")
+var yFlag = flag.Uint("y", 64, "The width of the first matrix and the height of the second matrix.")
+var zFlag = flag.Uint("z", 64, "The width of the second matrix.")
 
 func main() {
 	go func() {
@@ -23,12 +25,11 @@ func main() {
 	runner := new(runner.Runner).ParseFlag().Init()
 
 	benchmark := matrixmultiplication.NewBenchmark(runner.GPUDriver)
-	benchmark.X = uint32(*lengthFlag)
-	benchmark.Y = uint32(*lengthFlag)
-	benchmark.Z = uint32(*lengthFlag)
+	benchmark.X = uint32(*xFlag)
+	benchmark.Y = uint32(*yFlag)
+	benchmark.Z = uint32(*zFlag)
 
 	runner.AddBenchmark(benchmark)
 
 	runner.Run()
-
 }
