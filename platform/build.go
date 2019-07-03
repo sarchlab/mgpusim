d7eb4afb530705d1b48fd532d46973cf90e4c265
Ali Mosallaei <amosallaei19@gmail.com>
2019-07-03 14:42:32 +0000

    Reverted to original util package

--- platform/build.go
+++ platform/build.go
@@ -12,7 +12,7 @@ import (
 	"gitlab.com/akita/mem/idealmemcontroller"
 	"gitlab.com/akita/mem/vm/mmu"
 	"gitlab.com/akita/noc"
-	"gitlab.com/akita/util/tree/v0.1/tracing"
+	"gitlab.com/akita/tracing"
 )
 
 var UseParallelEngine bool
