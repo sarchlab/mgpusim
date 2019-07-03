1ff7137e743a6cb16c1ba568766b7de9d88a2537
Ali Mosallaei <amosallaei19@gmail.com>
2019-07-03 14:33:10 +0000

    Remove https from package link

--- platform/build.go
+++ platform/build.go
@@ -12,7 +12,7 @@ import (
 	"gitlab.com/akita/mem/idealmemcontroller"
 	"gitlab.com/akita/mem/vm/mmu"
 	"gitlab.com/akita/noc"
-	"https://gitlab.com/akita/util/tree/v0.1/tracing"
+	"gitlab.com/akita/util/tree/v0.1/tracing"
 )
 
 var UseParallelEngine bool
