2b039883b3689cfd042843478882cf1eadd8e093
Yifan Sun <sunyifan112358@gmail.com>
2019-07-03 20:36:36 -0400

    Done with new acceptance tests
    Former-commit-id: 92ac4b9c2d88c667ef97c360871529065b9b13db
    Former-commit-id: 5353bce8840760eaeade8eabd07fbd6dcb8badec
    Former-commit-id: 40ad4816fbfa691ab792520675e403e3c0a895a9

--- tests/acceptance/acceptance_test.py
+++ tests/acceptance/acceptance_test.py
@@ -49,22 +49,29 @@ class Test(object):
 
     def run_test(self, timing, parallel, gpus):
         fp = open(os.devnull, 'w')
-        cmd = ['./'+self.executable, self.size_args, '-verify', '-gpus='+gpus]
+        cmd = ['./'+self.executable, '-verify', '-gpus='+gpus]
+        cmd.extend(self.size_args)
+
         if timing:
             cmd.append('-timing')
 
         if parallel:
             cmd.append('-parallel')
 
-        p = subprocess.Popen(cmd, shell=True,
-                             cwd=self.path, stdout=fp, stderr=fp)
+        cmd_string = 'cd ' + self.path + ' && ' + ' '.join(cmd)
+        print('Running ' + cmd_string + ' ...')
+
+        p = subprocess.Popen(cmd, shell=False,
+                             cwd=self.path,
+                             stdout=fp, stderr=fp
+                             )
         p.wait()
 
         if p.returncode == 0:
-            print(colors.fg.green + ' '.join(cmd) + " Passed." + colors.reset)
+            print(colors.fg.green + 'Passed.' + colors.reset)
             return False
         else:
-            print(colors.fg.red + ' '.join(cmd) + " Failed." + colors.reset)
+            print(colors.fg.red + ' Failed.' + colors.reset)
             return True
 
 
@@ -73,184 +80,42 @@ def main():
 
     error |= compile('../../insts/gcn3disassembler')
 
-    fir = Test('../../samples/fir', 'fir', '-length=8192')
+    fir = Test('../../samples/fir', 'fir', ['-length=8192'])
     mm = Test('../../samples/matrixmultiplication',
-              'matrixmultiplication', '-x=128 -y=128 -z=128')
+              'matrixmultiplication', ['-x=128', '-y=128', '-z=128'])
+    km = Test('../../samples/kmeans', 'kmeans', [
+        '-points=1024',
+        '-features=32',
+        '-clusters=5',
+        '-max-iter=5'])
+    mt = Test('../../samples/matrixtranspose',
+              'matrixtranspose', ['-width=256'])
+    bs = Test('../../samples/bitonicsort',
+              'bitonicsort', ['-length=4096'])
+    aes = Test('../../samples/aes', 'aes', ['-length=16384'])
+    sc = Test('../../samples/simpleconvolution', 'simpleconvolution', [])
+    re = Test('../../samples/relu', 'relu', [])
+    mp = Test('../../samples/maxpooling', 'maxpooling', [])
+    cw = Test('../../samples/concurrentworkload', 'concurrentworkload', [])
 
     fir.test()
     mm.test()
-
-    # error |= compile('samples/fir/')
-    # # error |= run_test("FIR Disasm", '../../insts/gcn3disassembler/gcn3disassembler kernels.hsaco | diff kernels.disasm -', 'samples/fir')
-    # error |= run_test(
-    #     "FIR Emu",
-    #     './fir -verify -length=8192',
-    #     'samples/fir')
-    # error |= run_test(
-    #     "FIR Sim",
-    #     './fir -timing -verify -length=8192',
-    #     'samples/fir')
-    # error |= run_test(
-    #     "FIR Parallel Emu",
-    #     './fir -parallel -verify -length=8192',
-    #     'samples/fir')
-    # error |= run_test(
-    #     "FIR Parallel Sim",
-    #     './fir -timing -parallel -verify -length=8192',
-    #     'samples/fir')
-
-    # error |= compile('samples/kmeans/')
-    # # error |= run_test("KMeans Disasm", '../../insts/gcn3disassembler/gcn3disassembler kernels.hsaco | diff kernels.disasm -', 'samples/kmeans')
-    # error |= run_test(
-    #     "KMeans Emu",
-    #     './kmeans -verify -points=1024 -features=32 -clusters=5 -max-iter=5',
-    #     'samples/kmeans')
-    # error |= run_test(
-    #     "KMeans Sim",
-    #     './kmeans -timing -verify -points=1024 -features=32 -clusters=5 -max-iter=5',
-    #     'samples/kmeans')
-    # error |= run_test(
-    #     "KMeans Parallel Emu",
-    #     './kmeans -parallel -verify -points=1024 -features=32 -clusters=5 -max-iter=5',
-    #     'samples/kmeans')
-    # error |= run_test(
-    #     "KMeans Parallel Sim",
-    #     './kmeans -timing -parallel -verify -points=1024 -features=32 -clusters=5 -max-iter=5',
-    #     'samples/kmeans')
-
-    # error |= compile('samples/matrixtranspose/')
-    # # error |= run_test("MatrixTranspose Disasm", '../../insts/gcn3disassembler/gcn3disassembler kernels.hsaco | diff kernels.disasm -', 'samples/matrixtranspose')
-    # error |= run_test(
-    #     "MatrixTranspose Emu",
-    #     './matrixtranspose -verify -width=256',
-    #     'samples/matrixtranspose')
-    # error |= run_test(
-    #     "MatrixTranspose Sim",
-    #     './matrixtranspose -timing -verify -width=256',
-    #     'samples/matrixtranspose')
-    # error |= run_test(
-    #     "MatrixTranspose Parallel Emu",
-    #     './matrixtranspose --parallel -verify -width=256',
-    #     'samples/matrixtranspose')
-    # error |= run_test(
-    #     "MatrixTranspose Parallel Sim",
-    #     './matrixtranspose -timing --parallel -verify -width=256',
-    #     'samples/matrixtranspose')
-
-    # error |= compile('samples/bitonicsort/')
-    # # error |= run_test("BitonicSort Disasm", '../../insts/gcn3disassembler/gcn3disassembler kernels.hsaco | diff kernels.disasm -', 'samples/bitonicsort')
-    # error |= run_test(
-    #     "BitonicSort Emu",
-    #     './bitonicsort -length=4096 -verify',
-    #     'samples/bitonicsort')
-    # error |= run_test(
-    #     "BitonicSort Sim",
-    #     './bitonicsort -length=4096 -timing -verify',
-    #     'samples/bitonicsort')
-    # error |= run_test(
-    #     "BitonicSort Parallel Emu",
-    #     './bitonicsort -length=4096 -parallel -verify',
-    #     'samples/bitonicsort')
-    # error |= run_test(
-    #     "BitonicSort Parallel Sim",
-    #     './bitonicsort -length=4096 -timing -parallel -verify',
-    #     'samples/bitonicsort')
-
-    # error |= compile('samples/aes/')
-    # # # error |= run_test("AES Disasm",
-    # # # '../../insts/gcn3disassembler/gcn3disassembler kernels.hsaco | diff
-    # # # kernels.disasm -', 'samples/aes')
-    # error |= run_test("AES Emu", './aes -verify', 'samples/aes')
-    # error |= run_test("AES Sim", './aes -timing -verify', 'samples/aes')
-    # error |= run_test("AES Parallel Emu",
-    #                   './aes --parallel -verify', 'samples/aes')
-    # error |= run_test("AES Parallel Sim",
-    #                   './aes -timing --parallel -verify', 'samples/aes')
-
-    # error |= compile('samples/simpleconvolution/')
-    # # # error |= run_test("AES Disasm",
-    # # # '../../insts/gcn3disassembler/gcn3disassembler kernels.hsaco | diff
-    # # # kernels.disasm -', 'samples/aes')
-    # error |= run_test(
-    #     "Simple Convolution Emu",
-    #     './simpleconvolution -verify',
-    #     'samples/simpleconvolution')
-    # error |= run_test(
-    #     "Simple Convolution Sim",
-    #     './simpleconvolution -timing -verify',
-    #     'samples/simpleconvolution')
-    # error |= run_test(
-    #     "Simple Convolution Parallel Emu",
-    #     './simpleconvolution --parallel -verify',
-    #     'samples/simpleconvolution')
-    # error |= run_test(
-    #     "Simple Convolution Parallel Sim",
-    #     './simpleconvolution -timing --parallel -verify',
-    #     'samples/simpleconvolution')
-
-    # error |= compile('samples/relu/')
-    # error |= run_test(
-    #     "Relu Emu",
-    #     './relu -verify',
-    #     'samples/relu')
-    # error |= run_test(
-    #     "Relu Sim",
-    #     './relu -timing -verify',
-    #     'samples/relu')
-    # error |= run_test(
-    #     "Relu Parallel Emu",
-    #     './relu -parallel -verify',
-    #     'samples/relu')
-    # error |= run_test(
-    #     "Relu Parallel Sim",
-    #     './relu -timing -parallel -verify',
-    #     'samples/relu')
-
-    # error |= compile('samples/maxpooling/')
-    # error |= run_test(
-    #     "MaxPooling Emu",
-    #     './maxpooling -verify',
-    #     'samples/maxpooling')
-    # error |= run_test(
-    #     "MaxPooling Sim",
-    #     './maxpooling -timing -verify',
-    #     'samples/maxpooling')
-    # error |= run_test(
-    #     "MaxPooling Parallel Emu",
-    #     './maxpooling -parallel -verify',
-    #     'samples/maxpooling')
-    # error |= run_test(
-    #     "MaxPooling Parallel Sim",
-    #     './maxpooling -timing -parallel -verify',
-    #     'samples/maxpooling')
-
-    # error |= compile('samples/matrixmultiplication/')
-    # error |= run_test(
-    #     "Matrix Multiplication Emu",
-    #     './matrixmultiplication -verify',
-    #     'samples/matrixmultiplication')
-    # error |= run_test(
-    #     "Matrix Multiplication Sim",
-    #     './matrixmultiplication -timing -verify',
-    #     'samples/matrixmultiplication')
-    # error |= run_test(
-    #     "Matrix Multiplication Parallel Emu",
-    #     './matrixmultiplication -parallel -verify',
-    #     'samples/matrixmultiplication')
-    # error |= run_test(
-    #     "Matrix Multiplication Parallel Sim",
-    #     './matrixmultiplication -timing -parallel -verify',
-    #     'samples/matrixmultiplication')
-
-    # error |= compile('samples/concurrentworkload/')
-    # error |= run_test(
-    #     "Concurrent Workload Sim",
-    #     './concurrentworkload -timing -verify',
-    #     'samples/concurrentworkload')
-    # error |= run_test(
-    #     "Concurrent Workload Sim",
-    #     './concurrentworkload -timing -parallel -verify',
-    #     'samples/concurrentworkload')
+    km.test()
+    mt.compile()
+    mt.run_test(False, False, '1')
+    mt.run_test(False, True, '1')
+    mt.run_test(True, False, '1')
+    mt.run_test(True, True, '1')
+    aes.test()
+    bs.test()
+    sc.test()
+    re.test()
+    mp.test()
+    cw.compiles()
+    cw.run_test(False, False, '1')
+    cw.run_test(False, True, '1')
+    cw.run_test(True, False, '1')
+    cw.run_test(True, True, '1')
 
     # error |= compile('acceptancetests/cupipelinedraining')
     # error |= run_test('CU Pipeline Draining',
@@ -264,6 +129,7 @@ def main():
     # error |= run_test('TLB Shootdown',
     #                   './tlbshootdown -timing',
     #                   'acceptancetestes/tlbshootdown')
+
     if error:
         sys.exit(1)
 
