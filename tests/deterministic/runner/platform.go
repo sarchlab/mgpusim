812ef50c44130e2798c386d61d99b335abf56cce
Yifan Sun <sunyifan112358@gmail.com>
2019-07-05 11:40:21 -0400

    Test non-deterministic behavior with empty kernel benchmark
    Former-commit-id: b29a229a8173eb17d82fffa61c66f2ff805f8e02
    Former-commit-id: 9008acfb1a7942c9d77c40f1b23ac16b345d4c97
    Former-commit-id: bbca5603b3a32596ae21dbf2098f31b41a8a741b

---
+++ tests/deterministic/runner/platform.go
@@ -0,0 +1,90 @@
+package runner
+
+import (
+	"fmt"
+
+	"gitlab.com/akita/akita"
+	"gitlab.com/akita/gcn3/driver"
+	"gitlab.com/akita/gcn3/gpubuilder"
+	"gitlab.com/akita/mem"
+	"gitlab.com/akita/mem/cache"
+	"gitlab.com/akita/mem/vm/mmu"
+	"gitlab.com/akita/noc"
+	"gitlab.com/akita/vis/trace"
+)
+
+var UseParallelEngine bool
+var DebugISA bool
+var TraceVis bool
+var TraceMem bool
+
+func buildNR9NanoPlatformWithPerfectMemorySystem(
+	numGPUs int,
+) (
+	akita.Engine,
+	*driver.Driver,
+) {
+	var engine akita.Engine
+
+	if UseParallelEngine {
+		engine = akita.NewParallelEngine()
+	} else {
+		engine = akita.NewSerialEngine()
+	}
+	//engine.AcceptHook(akita.NewEventLogger(log.New(os.Stdout, "", 0)))
+
+	mmuBuilder := mmu.MakeBuilder().
+		WithEngine(engine).
+		WithFreq(1 * akita.GHz)
+	mmuComponent := mmuBuilder.Build("MMU")
+	gpuDriver := driver.NewDriver(engine, mmuComponent)
+
+	//connection := akita.NewDirectConnection(engine)
+	connection := noc.NewFixedBandwidthConnection(32, engine, 1*akita.GHz)
+	connection.SrcBufferCapacity = 40960000
+
+	gpuBuilder := gpubuilder.MakeR9NanoGPUBuilder().
+		WithEngine(engine).
+		WithExternalConn(connection).
+		WithMMU(mmuComponent).
+		WithNumCUPerShaderArray(4).
+		WithNumShaderArray(16).
+		WithNumMemoryBank(8)
+
+	if TraceVis {
+		tracer := trace.NewMongoDBTracer()
+		tracer.Init()
+		hook := trace.NewHook(tracer)
+		gpuBuilder.SetTraceHook(hook)
+
+		gpuDriver.AcceptHook(hook)
+	}
+
+	if TraceMem {
+		gpuBuilder.EnableMemTracing = true
+	}
+
+	rdmaAddressTable := new(cache.BankedLowModuleFinder)
+	rdmaAddressTable.BankSize = 4 * mem.GB
+	rdmaAddressTable.LowModules = append(rdmaAddressTable.LowModules, nil)
+	for i := 1; i < numGPUs+1; i++ {
+		name := fmt.Sprintf("GPU_%d", i)
+		memAddrOffset := uint64(i) * 4 * mem.GB
+		gpu := gpuBuilder.
+			WithMemAddrOffset(memAddrOffset).
+			Build(name, uint64(i))
+		gpuDriver.RegisterGPU(gpu, 4*mem.GB)
+		gpu.Driver = gpuDriver.ToGPUs
+
+		gpu.RDMAEngine.RemoteRDMAAddressTable = rdmaAddressTable
+		rdmaAddressTable.LowModules = append(
+			rdmaAddressTable.LowModules,
+			gpu.RDMAEngine.ToOutside)
+		connection.PlugIn(gpu.RDMAEngine.ToOutside)
+	}
+
+	connection.PlugIn(gpuDriver.ToGPUs)
+	connection.PlugIn(mmuComponent.ToTop)
+
+	return engine, gpuDriver
+}
