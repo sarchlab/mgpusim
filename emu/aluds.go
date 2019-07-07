766d390f078e1a7f7616c72274b4d5025aceb4f7
Yifan Sun <sunyifan112358@gmail.com>
2019-07-06 22:56:22 -0400

    ds opcode 13
    Former-commit-id: 4ef8974bcef6c123a3fecce7354270bba2bf11e8
    Former-commit-id: cb2c4d6db93a9845b880db7e9c5bb573b32bc9ba
    Former-commit-id: 61749c1304f4f610651b3bf1dcc9b6370aaf41d6

--- emu/aluds.go
+++ emu/aluds.go
@@ -7,6 +7,8 @@ import (
 func (u *ALUImpl) runDS(state InstEmuState) {
 	inst := state.Inst()
 	switch inst.Opcode {
+	case 13:
+		u.runDSWRITEB32(state)
 	case 14:
 		u.runDSWRITE2B32(state)
 	case 55:
@@ -19,6 +21,25 @@ func (u *ALUImpl) runDS(state InstEmuState) {
 		u.runDSREAD2B64(state)
 	default:
 		log.Panicf("Opcode %d for DS format is not implemented", inst.Opcode)
+	}
+}
+
+func (u *ALUImpl) runDSWRITEB32(state InstEmuState) {
+	inst := state.Inst()
+	sp := state.Scratchpad()
+	layout := sp.AsDS()
+	lds := u.LDS()
+
+	i := uint(0)
+	for i = 0; i < 64; i++ {
+		if !u.laneMasked(layout.EXEC, i) {
+			continue
+		}
+
+		addr0 := layout.ADDR[i] + uint32(inst.Offset0)*4
+		data0offset := uint(8 + 64*4)
+
+		copy(lds[addr0:addr0+4], sp[data0offset+i*16:data0offset+i*16+4])
 	}
 }
 
