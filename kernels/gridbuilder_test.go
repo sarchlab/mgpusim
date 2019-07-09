ed23833d9ef3f5cad1a70d2cf7981dc5ec4adbfa
Yifan Sun <sunyifan112358@gmail.com>
2019-07-09 15:19:48 -0400

    Problem not solved

--- kernels/gridbuilder_test.go
+++ kernels/gridbuilder_test.go
@@ -17,6 +17,71 @@ var _ = Describe("GridBuilder", func() {
 		builder = &gridBuilderImpl{}
 	})
 
+	It("should build partial wavefront", func() {
+		codeObject := new(insts.HsaCo)
+		packet := new(HsaKernelDispatchPacket)
+		packet.WorkgroupSizeX = 64
+		packet.WorkgroupSizeY = 1
+		packet.WorkgroupSizeZ = 1
+		packet.GridSizeX = 32
+		packet.GridSizeY = 1
+		packet.GridSizeZ = 1
+		builder.SetKernel(KernelLaunchInfo{
+			CodeObject: codeObject,
+			Packet:     packet,
+			PacketAddr: 0,
+		})
+
+		wg := builder.NextWG()
+
+		Expect(builder.NumWG()).To(Equal(1))
+		Expect(wg.SizeX).To(Equal(64))
+		Expect(wg.CurrSizeX).To(Equal(32))
+		Expect(wg.Wavefronts).To(HaveLen(1))
+		Expect(wg.WorkItems).To(HaveLen(32))
+		Expect(wg.Wavefronts[0].InitExecMask).
+			To(Equal(uint64(0x00000000ffffffff)))
+	})
+
+	It("should build partial 2d wavefront", func() {
+		codeObject := new(insts.HsaCo)
+		packet := new(HsaKernelDispatchPacket)
+		packet.WorkgroupSizeX = 8
+		packet.WorkgroupSizeY = 8
+		packet.WorkgroupSizeZ = 1
+		packet.GridSizeX = 12
+		packet.GridSizeY = 4
+		packet.GridSizeZ = 1
+		builder.SetKernel(KernelLaunchInfo{
+			CodeObject: codeObject,
+			Packet:     packet,
+			PacketAddr: 0,
+		})
+
+		Expect(builder.NumWG()).To(Equal(2))
+
+		wg1 := builder.NextWG()
+		Expect(wg1.SizeX).To(Equal(8))
+		Expect(wg1.SizeY).To(Equal(8))
+		Expect(wg1.CurrSizeX).To(Equal(8))
+		Expect(wg1.CurrSizeY).To(Equal(4))
+		Expect(wg1.Wavefronts).To(HaveLen(1))
+		Expect(wg1.WorkItems).To(HaveLen(32))
+		Expect(wg1.Wavefronts[0].InitExecMask).
+			To(Equal(uint64(0x00000000ffffffff)))
+
+		wg2 := builder.NextWG()
+		Expect(wg2.SizeX).To(Equal(8))
+		Expect(wg2.SizeY).To(Equal(8))
+		Expect(wg2.CurrSizeX).To(Equal(4))
+		Expect(wg2.CurrSizeY).To(Equal(4))
+		Expect(wg2.Wavefronts).To(HaveLen(1))
+		Expect(wg2.WorkItems).To(HaveLen(16))
+		Expect(wg2.Wavefronts[0].InitExecMask).
+			To(Equal(uint64(0x000000000f0f0f0f)))
+
+	})
+
 	It("should build 1D grid workgroup", func() {
 		codeObject := new(insts.HsaCo)
 		packet := new(HsaKernelDispatchPacket)
