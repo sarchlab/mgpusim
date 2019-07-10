016da7f02a5db5f0403b2c31a88710394ee8c5ce
CheolGyu Jin <talkingdeer12@kaist.ac.kr>
2019-07-10 18:52:23 +0900

    new f64 insts added
    Former-commit-id: d8b50c1437543a7969d171a4d96bcf01d13ed036
    Former-commit-id: ec13a767f1c4728bc3e8b56b133f991649cad0b7

--- emu/aluvop3a_test.go
+++ emu/aluvop3a_test.go
@@ -310,4 +310,100 @@ var _ = Describe("ALU", func() {
 		Expect(sp.DST[0]).To(Equal(uint64(0x0000000000001000)))
 		Expect(sp.DST[1]).To(Equal(uint64(0xfffffffff0001000)))
 	})
+
+	It("should run V_ADD_F64", func() {
+		state.inst = insts.NewInst()
+		state.inst.FormatType = insts.VOP3a
+		state.inst.Opcode = 640
+
+		sp := state.Scratchpad().AsVOP3A()
+		sp.SRC0[0] = uint64(math.Float64bits(2.0))
+		sp.SRC1[0] = uint64(math.Float64bits(3.1))
+		sp.EXEC = 0x1
+
+		alu.Run(state)
+
+		Expect(sp.DST[0]).To(Equal(uint64(math.Float64bits(float64(5.1)))))
+	})
+
+	It("should run V_FMA_F64", func() {
+		state.inst = insts.NewInst()
+		state.inst.FormatType = insts.VOP3a
+		state.inst.Opcode = 460
+
+		sp := state.Scratchpad().AsVOP3A()
+		sp.SRC0[0] = uint64(math.Float64bits(2.0))
+		sp.SRC1[0] = uint64(math.Float64bits(3.1))
+		sp.SRC2[0] = uint64(math.Float64bits(2.5))
+		sp.EXEC = 0x1
+
+		alu.Run(state)
+
+		Expect(sp.DST[0]).To(Equal(uint64(math.Float64bits(float64(8.7)))))
+	})
+
+	It("should run V_MUL_F64", func() {
+		state.inst = insts.NewInst()
+		state.inst.FormatType = insts.VOP3a
+		state.inst.Opcode = 641
+
+		sp := state.Scratchpad().AsVOP3A()
+		sp.SRC0[0] = uint64(math.Float64bits(2.0))
+		sp.SRC1[0] = uint64(math.Float64bits(3.1))
+		sp.EXEC = 0x1
+
+		alu.Run(state)
+
+		Expect(sp.DST[0]).To(Equal(uint64(math.Float64bits(float64(6.2)))))
+	})
+
+	It("should run V_DIV_FMAS_F64", func() {
+		state.inst = insts.NewInst()
+		state.inst.FormatType = insts.VOP3a
+		state.inst.Opcode = 483
+
+		sp := state.Scratchpad().AsVOP3A()
+		sp.SRC0[0] = uint64(math.Float64bits(2.0))
+		sp.SRC1[0] = uint64(math.Float64bits(1.1))
+		sp.SRC2[0] = uint64(math.Float64bits(4.0))
+		sp.EXEC = 0x1
+		sp.VCC = 1
+
+		alu.Run(state)
+
+		Expect(sp.DST[0]).To(Equal(uint64(math.Float64bits(float64(6.2) * math.Pow(2.0, 64)))))
+	})
+
+	It("should run V_DIV_FIXUP_F64", func() {
+		state.inst = insts.NewInst()
+		state.inst.FormatType = insts.VOP3a
+		state.inst.Opcode = 479
+
+		sp := state.Scratchpad().AsVOP3A()
+		sp.SRC0[0] = uint64(math.Float64bits(0))
+		sp.SRC1[0] = uint64(math.Float64bits(0))
+		sp.SRC2[0] = uint64(math.Float64bits(0))
+		sp.EXEC = 0x1
+
+		alu.Run(state)
+		// 0 / 0
+		Expect(sp.DST[0]).To(Equal(uint64(math.Float64bits(0xFFF8000000000000))))
+	})
+
+	It("should run V_DIV_FIXUP_F64", func() {
+		state.inst = insts.NewInst()
+		state.inst.FormatType = insts.VOP3a
+		state.inst.Opcode = 479
+
+		sp := state.Scratchpad().AsVOP3A()
+		sp.SRC0[0] = uint64(math.Float64bits(0))
+		sp.SRC1[0] = uint64(math.Float64bits(0x7FF0000000000000))
+		sp.SRC2[0] = uint64(math.Float64bits(0x7FF0000000000000))
+		sp.EXEC = 0x1
+
+		alu.Run(state)
+		// inf / inf
+		Expect(sp.DST[0]).To(Equal(uint64(math.Float64bits(0xFFF8000000000000))))
+	})
+
 })
