262f1126d61234e9d3b5173afbab228a131d211a
alitolga <alitolga.dincer@gmail.com>
2019-07-16 13:46:21 -0400

    Initial commit
    Former-commit-id: a66a8ccc95dfbdaa8a9b940cf9582a347eddaf9e
    Former-commit-id: c7d9179cff8058dc1470e908a24f700e43059f50

---
+++ samples/pagerank/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+
+	"gitlab.com/akita/gcn3/benchmarks/heteromark/pr"
+	"gitlab.com/akita/gcn3/samples/runner"
+)
+
+var numNodes = flag.Int("nodes", 16, "The number of nodes")
+var numConnections = flag.Int("connections", 8, "The number of connections")
+var maxIterations = flag.Int("iterations", 16, "The number of iterations")
+
+func main() {
+	flag.Parse()
+
+	runner := new(runner.Runner).ParseFlag().Init()
+
+	benchmark := pagerank.NewBenchmark(runner.GPUDriver)
+	benchmark.NumNodes = uint32(*numNodes)
+	benchmark.NumConnections = uint32(*numConnections)
+	benchmark.MaxIterations = uint32(*maxIterations)
+
+	runner.AddBenchmark(benchmark)
+
+	runner.Run()
+}
