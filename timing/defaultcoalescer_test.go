b8bc551c126de31020c345e2655370447c25e446
Yifan Sun <sunyifan112358@gmail.com>
2019-07-21 11:05:03 -0400

    Read across cachelines
    Former-commit-id: 1c514458ffe0b94f0c6eafa41dbf06db7b765830
    Former-commit-id: 1bdcdd386d9529da85ba09efd96f3acc085357c1

--- timing/defaultcoalescer_test.go
+++ timing/defaultcoalescer_test.go
@@ -43,7 +43,7 @@ var _ = Describe("Default Coalescer", func() {
 		inst := insts.NewInst()
 		inst.FormatType = insts.FLAT
 		inst.Opcode = 20 // flat_load_dword
-		inst.Dst = insts.NewRegOperand(0, 0, 1)
+		inst.Dst = insts.NewVRegOperand(0, 0, 1)
 		wf.SetDynamicInst(wavefront.NewInst(inst))
 
 		sp := wf.Scratchpad().AsFlat()
@@ -59,6 +59,29 @@ var _ = Describe("Default Coalescer", func() {
 		Expect(memTransactions[1].laneInfo).To(HaveLen(16))
 		Expect(memTransactions[2].laneInfo).To(HaveLen(16))
 		Expect(memTransactions[3].laneInfo).To(HaveLen(16))
+	})
+
+	It("should not generate cross-cache-line requests", func() {
+		inst := insts.NewInst()
+		inst.FormatType = insts.FLAT
+		inst.Opcode = 21 // flat_load_dwordx2
+		inst.Dst = insts.NewVRegOperand(0, 0, 1)
+		wf.SetDynamicInst(wavefront.NewInst(inst))
+
+		sp := wf.Scratchpad().AsFlat()
+		sp.EXEC = 0xffffffffffffffff
+		for i := 0; i < 64; i++ {
+			sp.ADDR[i] = uint64(0x1004 + i*4)
+		}
+
+		memTransactions := c.generateMemTransactions(wf)
+
+		Expect(memTransactions).To(HaveLen(5))
+		Expect(memTransactions[0].laneInfo).To(HaveLen(29))
+		Expect(memTransactions[1].laneInfo).To(HaveLen(32))
+		Expect(memTransactions[2].laneInfo).To(HaveLen(32))
+		Expect(memTransactions[3].laneInfo).To(HaveLen(32))
+		Expect(memTransactions[4].laneInfo).To(HaveLen(3))
 	})
 
 	It("should coalesce store instructions", func() {
