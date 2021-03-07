fdcabb2c55ef1399dce0dcb166d37a2a65fcadf0
JokerZz- <zhanghy000107@gmail.com>
2021-03-06 20:29:19 -0600

    feat: Add VGG16 multi-gpu

--- benchmarks/dnn/vgg16/benchmark.go
+++ benchmarks/dnn/vgg16/benchmark.go
@@ -1,25 +1,36 @@
-// Package vgg16 implements vgg16.
+// Package vgg16 implements VGG16 network training.
 package vgg16
 
 import (
+	"math"
+
+	"gitlab.com/akita/mgpusim/benchmarks/dnn/tensor"
+	"gitlab.com/akita/mgpusim/benchmarks/mccl"
+
 	"gitlab.com/akita/dnn/dataset/cifar10"
 	"gitlab.com/akita/dnn/layers"
 	"gitlab.com/akita/dnn/training"
 	"gitlab.com/akita/dnn/training/optimization"
-	"gitlab.com/akita/mgpusim/benchmarks/dnn/tensor"
+	"gitlab.com/akita/mgpusim/benchmarks/dnn/gputraining"
 	"gitlab.com/akita/mgpusim/driver"
-	"math"
 )
 
 // Benchmark defines the VGG16 network training benchmark.
 type Benchmark struct {
-	driver  *driver.Driver
-	context *driver.Context
-	to      *tensor.GPUOperator
-	gpus    []int
+	driver   *driver.Driver
+	ctx      *driver.Context
+	to       []*tensor.GPUOperator
+	gpus     []int
+	contexts []*driver.Context
 
-	network training.Network
-	trainer training.Trainer
+	networks []training.Network
+	trainer  gputraining.DataParallelismMultiGPUTrainer
+
+	BatchSize          int
+	Epoch              int
+	MaxBatchPerEpoch   int
+	EnableTesting      bool
+	EnableVerification bool
 }
 
 // NewBenchmark creates a new benchmark.
@@ -27,85 +38,152 @@ func NewBenchmark(driver *driver.Driver) *Benchmark {
 	b := new(Benchmark)
 
 	b.driver = driver
-	b.context = b.driver.Init()
-	b.to = tensor.NewGPUOperator(b.driver, b.context)
-	//b.to.EnableVerification()
-
-	b.network = training.Network{
-		Layers: []layers.Layer{
-			layers.NewConv2D(b.to, []int{3,32,32}, []int{64,3,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewConv2D(b.to, []int{64,32,32}, []int{64,64,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewMaxPoolingLayer(b.to, []int{2,2}, []int{0,0}, []int{2,2}),
-
-			layers.NewConv2D(b.to, []int{64,16,16}, []int{128,64,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewConv2D(b.to, []int{128,16,16}, []int{128,128,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewMaxPoolingLayer(b.to, []int{2,2}, []int{0,0}, []int{2,2}),
-
-			layers.NewConv2D(b.to, []int{128,8,8}, []int{256,128,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewConv2D(b.to, []int{256,8,8}, []int{256,256,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewConv2D(b.to, []int{256,8,8}, []int{256,256,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewMaxPoolingLayer(b.to, []int{2,2}, []int{0,0}, []int{2,2}),
-
-			layers.NewConv2D(b.to, []int{256,4,4}, []int{512,256,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewConv2D(b.to, []int{512,4,4}, []int{512,512,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewConv2D(b.to, []int{512,4,4}, []int{512,512,3,3}, []int{1,1}, []int{1,1}),
-			layers.NewReluLayer(b.to),
-			layers.NewMaxPoolingLayer(b.to, []int{2,2}, []int{0,0}, []int{2,2}),
-
-			//layers.NewConv2D(b.to, []int{512,2,2}, []int{512,512,2,2}, []int{1,1}, []int{1,1}),
-			//layers.NewReluLayer(b.to),
-			//layers.NewConv2D(b.to, []int{512,2,2}, []int{512,512,2,2}, []int{1,1}, []int{1,1}),
-			//layers.NewReluLayer(b.to),
-			//layers.NewConv2D(b.to, []int{512,2,2}, []int{512,512,2,2}, []int{1,1}, []int{1,1}),
-			//layers.NewReluLayer(b.to),
-			//layers.NewMaxPoolingLayer(b.to, []int{2,2}, []int{0,0}, []int{2,2}),
-
-			layers.NewFullyConnectedLayer(b.to, 2*2*512, 1*1*512),
-			layers.NewReluLayer(b.to),
-			layers.NewFullyConnectedLayer(b.to, 1*1*512, 10),
-		},
-	}
-
-	b.trainer = training.Trainer{
-		TO:              b.to,
-		DataSource:      cifar10.NewTrainingDataSource(b.to),
-		Network:         b.network,
-		LossFunc:        training.NewSoftmaxCrossEntropy(b.to),
-		OptimizationAlg: optimization.NewAdam(b.to, 0.001),
-		Tester: &training.Tester{
-			DataSource: cifar10.NewTestDataSource(b.to),
-			Network:    b.network,
-			BatchSize:  math.MaxInt32,
-		},
-		Epoch:         1,
-		BatchSize:     8,
-		ShowBatchInfo: true,
-	}
+	b.ctx = driver.Init()
 
 	return b
 }
 
 // SelectGPU selects the GPU to use.
 func (b *Benchmark) SelectGPU(gpuIDs []int) {
-	if len(gpuIDs) > 1 {
-		panic("multi-GPU is not supported by DNN workloads")
+	b.gpus = gpuIDs
+}
+
+func (b *Benchmark) init() {
+	for _, gpu := range b.gpus {
+		b.defineNetwork(gpu)
+	}
+
+	b.createTrainer()
+	b.randomizeParams()
+}
+
+func (b *Benchmark) defineNetwork(gpuID int) {
+	context := b.driver.InitWithExistingPID(b.ctx)
+	b.driver.SelectGPU(context, gpuID)
+	to := tensor.NewGPUOperator(b.driver, context)
+
+	if b.EnableVerification {
+		to.EnableVerification()
+	}
+
+	network := training.Network{
+		Layers: []layers.Layer{
+			layers.NewConv2D(to, []int{3, 32, 32}, []int{64, 3, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewConv2D(to, []int{64, 32, 32}, []int{64, 64, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewMaxPoolingLayer(to, []int{2, 2}, []int{0, 0}, []int{2, 2}),
+
+			layers.NewConv2D(to, []int{64, 16, 16}, []int{128, 64, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewConv2D(to, []int{128, 16, 16}, []int{128, 128, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewConv2D(to, []int{128, 16, 16}, []int{128, 128, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewMaxPoolingLayer(to, []int{2, 2}, []int{0, 0}, []int{2, 2}),
+
+			layers.NewConv2D(to, []int{128, 8, 8}, []int{256, 128, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewConv2D(to, []int{256, 8, 8}, []int{256, 256, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewConv2D(to, []int{256, 8, 8}, []int{256, 256, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewMaxPoolingLayer(to, []int{2, 2}, []int{0, 0}, []int{2, 2}),
+
+			layers.NewConv2D(to, []int{256, 4, 4}, []int{512, 256, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewConv2D(to, []int{512, 4, 4}, []int{512, 512, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewConv2D(to, []int{512, 4, 4}, []int{512, 512, 3, 3}, []int{1, 1}, []int{1, 1}),
+			layers.NewReluLayer(to),
+			layers.NewMaxPoolingLayer(to, []int{2, 2}, []int{0, 0}, []int{2, 2}),
+
+			layers.NewFullyConnectedLayer(to, 2*2*512, 1*1*512),
+			layers.NewReluLayer(to),
+			layers.NewFullyConnectedLayer(to, 1*1*512, 10),
+		},
+	}
+
+	b.networks = append(b.networks, network)
+	b.contexts = append(b.contexts, context)
+	b.to = append(b.to, to)
+}
+
+func (b *Benchmark) createTrainer() {
+	sources := make([]training.DataSource, len(b.networks))
+	alg := make([]optimization.Alg, len(b.networks))
+	testers := make([]*training.Tester, len(b.networks))
+	lossFuncs := make([]training.LossFunction, len(b.networks))
+
+	for i := 0; i < len(b.networks); i++ {
+		sources[i] = cifar10.NewTrainingDataSource(b.to[i])
+		alg[i] = optimization.NewAdam(b.to[i], 0.001)
+		lossFuncs[i] = training.NewSoftmaxCrossEntropy(b.to[i])
+
+		if b.EnableTesting {
+			testers[i] = &training.Tester{
+				DataSource: cifar10.NewTestDataSource(b.to[i]),
+				Network:    b.networks[i],
+				BatchSize:  math.MaxInt32,
+			}
+		}
+	}
+
+	b.trainer = gputraining.DataParallelismMultiGPUTrainer{
+		TensorOperators:  b.to,
+		DataSource:       sources,
+		Networks:         b.networks,
+		LossFunc:         lossFuncs,
+		OptimizationAlg:  alg,
+		Tester:           testers,
+		Epoch:            b.Epoch,
+		MaxBatchPerEpoch: b.MaxBatchPerEpoch,
+		BatchSize:        b.BatchSize,
+		ShowBatchInfo:    true,
+		GPUs:             b.gpus,
+		Contexts:         b.contexts,
+		Driver:           b.driver,
+	}
+}
+
+func (b *Benchmark) randomizeParams() {
+	initNet := b.networks[0]
+	for _, l := range initNet.Layers {
+		l.Randomize()
+	}
+
+	gpuNum := len(b.networks)
+
+	for i := range b.networks[0].Layers {
+		if b.networks[0].Layers[i].Parameters() == nil {
+			continue
+		}
+
+		params := make([]*tensor.Tensor, gpuNum)
+		datas := make([]driver.GPUPtr, gpuNum)
+
+		for j := 0; j < gpuNum; j++ {
+			params[j] = b.networks[j].Layers[i].Parameters().(*tensor.Tensor)
+		}
+
+		dataSizeArr := params[0].Size()
+		dataSize := 1
+		for i := 0; i < len(dataSizeArr); i++ {
+			dataSize *= dataSizeArr[i]
+		}
+
+		for i := 0; i < len(params); i++ {
+			datas[i] = params[i].Ptr()
+		}
+		comms := mccl.CommInitAllMultipleContexts(
+			gpuNum, b.driver, b.contexts, b.gpus)
+		mccl.BroadcastRing(b.driver, comms, 1, datas, dataSize)
 	}
 }
 
 // Run executes the benchmark.
 func (b *Benchmark) Run() {
-	for _, l := range b.network.Layers {
-		l.Randomize()
-	}
+	b.init()
 	b.trainer.Train()
 }
 
--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/rs/xid v1.2.1
 	github.com/tebeka/atexit v0.3.0
 	gitlab.com/akita/akita v1.12.1
-	gitlab.com/akita/dnn v0.5.2-0.20210304044001-23cd731cf811
+	gitlab.com/akita/dnn v0.5.2-0.20210305035643-5d0c4b839ac8
 	gitlab.com/akita/mem v1.11.3
 	gitlab.com/akita/noc v1.4.0
 	gitlab.com/akita/util v0.7.1
