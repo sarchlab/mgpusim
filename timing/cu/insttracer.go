9d96f4e36c4f71acb4037f4a6071a0a96531d94f
zohapeterson <zohapeterson>
2022-07-11 15:04:51 +0000

    Added new funcitonality to metrics and CU tracing data

--- timing/cu/insttracer.go
+++ timing/cu/insttracer.go
@@ -1,34 +1,103 @@
 package cu
 
 import (
-	"fmt"
-
 	"gitlab.com/akita/akita/v3/sim"
 	"gitlab.com/akita/akita/v3/tracing"
 )
 
 // InstTracer is a tracer that traces the time that VALU instructions take and VMem instructions take
 type InstTracer struct {
-	timeTeller    sim.TimeTeller
-	totalVALUTime float64
-	totalVMemTime float64
-	inflightInsts map[string]tracing.Task
+	timeTeller            sim.TimeTeller
+	SIMDCPIStackValues    SIMDCPIStack
+	AllInstCPIStackValues AllInstCPIStack
+	TimeManager           InstTimeManagement
+	CountManager          InstCountManagement
+	inflightInsts         map[string]tracing.Task
+}
+
+// SIMDCPIStack holds the cpi stack values of each type of instruction when looking at the SIMD specifically
+type SIMDCPIStack struct {
+	SIMDCPI     float64
+	LDSStack    float64
+	BranchStack float64
+	ScalarStack float64
+	VMemStack   float64
+	StackBase   float64
+}
+
+// AllInstCPIStack holds the cpi stack values of each type of instruction when considering each instruction in total
+type AllInstCPIStack struct {
+	AllInstCPI     float64
+	SIMDStack      float64
+	LDSStack       float64
+	BranchStack    float64
+	ScalarStack    float64
+	VMemStack      float64
+	TotalStackBase float64
+}
+
+// InstTimeManagement manages the time distribution in tracing
+type InstTimeManagement struct {
+	TotalVMemTime    float64
+	TotalBranchTime  float64
+	TotalScalarTime  float64
+	TotalSpecialTime float64
+	TotalLDSTime     float64
+	TotalTime        float64
+	TotalSIMDTime    float64
+	TotalOtherTime   float64
+	firstInstStart   float64
+}
+
+// InstCountManagement manages the instruction count in tracing
+type InstCountManagement struct {
+	TotalInstCount uint64
+	SIMDInstCount  uint64
+	OtherInstCount uint64
 }
 
 // NewInstTracer creates a new InstTracer
 func NewInstTracer(timeTeller sim.TimeTeller) *InstTracer {
 	return &InstTracer{
-		timeTeller:    timeTeller,
-		totalVALUTime: 0,
-		totalVMemTime: 0,
-		inflightInsts: make(map[string]tracing.Task),
+		timeTeller:            timeTeller,
+		SIMDCPIStackValues:    *newSIMDCPIStack(),
+		AllInstCPIStackValues: *newTotalCPIStack(),
+		TimeManager:           *newInstTimeManager(),
+		CountManager:          *newInstCountManager(),
+		inflightInsts:         make(map[string]tracing.Task),
 	}
+}
+
+// newSIMDCPIStack creates a new SIMD specific CPI stack
+func newSIMDCPIStack() *SIMDCPIStack {
+	return &SIMDCPIStack{
+		StackBase: 4,
+	}
+}
+
+// newTotalCPIStack creates an overall instruction CPI stack
+func newTotalCPIStack() *AllInstCPIStack {
+	return &AllInstCPIStack{}
+}
+
+// newInstTimeManager creates a new time manager
+func newInstTimeManager() *InstTimeManagement {
+	return &InstTimeManagement{}
+}
+
+// newInstCountManager creates a new instruction count manager
+func newInstCountManager() *InstCountManagement {
+	return &InstCountManagement{}
 }
 
 // StartTask begins the tracing for the InstTracer
 func (t *InstTracer) StartTask(task tracing.Task) {
 	if task.Kind != "inst" {
 		return
+	}
+
+	if t.CountManager.TotalInstCount == 0 {
+		t.TimeManager.firstInstStart = float64(t.timeTeller.CurrentTime())
 	}
 
 	task.StartTime = t.timeTeller.CurrentTime()
@@ -41,7 +110,7 @@ func (t *InstTracer) StepTask(task tracing.Task) {
 	// Do nothing
 }
 
-// EndTask does nothing for now
+// EndTask filters time into correct attribute and deletes the instruction from the tracer
 func (t *InstTracer) EndTask(task tracing.Task) {
 	orgTask, ok := t.inflightInsts[task.ID]
 
@@ -52,13 +121,66 @@ func (t *InstTracer) EndTask(task tracing.Task) {
 	orgTask.EndTime = t.timeTeller.CurrentTime()
 	timeDiff := orgTask.EndTime - orgTask.StartTime
 
+	t.calcTotalTime(float64(orgTask.EndTime))
+
 	if orgTask.What == "VALU" {
-		t.totalVALUTime += float64(timeDiff)
-	} else if orgTask.What == "VMem" {
-		t.totalVMemTime += float64(timeDiff)
+		t.TimeManager.TotalSIMDTime += float64(timeDiff)
+		t.CountManager.SIMDInstCount++
+	} else {
+		t.CountManager.OtherInstCount++
+
+		if orgTask.What == "VMem" {
+			t.TimeManager.TotalVMemTime += float64(timeDiff)
+		} else if orgTask.What == "Special" {
+			t.TimeManager.TotalSpecialTime += float64(timeDiff)
+		} else if orgTask.What == "Branch" {
+			t.TimeManager.TotalBranchTime += float64(timeDiff)
+		} else if orgTask.What == "Scalar" {
+			t.TimeManager.TotalScalarTime += float64(timeDiff)
+		} else if orgTask.What == "LDS" {
+			t.TimeManager.TotalLDSTime += float64(timeDiff)
+		}
 	}
 
-	delete(t.inflightInsts, task.ID)
+	t.CountManager.TotalInstCount++
 
-	fmt.Printf("%s, %0.10f, %0.10f, %s\n", task.ID, t.totalVALUTime, t.totalVMemTime, orgTask.Where)
+	t.calcTimeDistribution()
+
+	delete(t.inflightInsts, task.ID)
+}
+
+// calcTotalTime calculates the total time for the instructions to execute
+func (t *InstTracer) calcTotalTime(currentEndTime float64) {
+	t.TimeManager.TotalTime = currentEndTime - t.TimeManager.firstInstStart
+}
+
+// calcTimeDistribution calculates the time distribution for the instructions set
+func (t *InstTracer) calcTimeDistribution() {
+	t.TimeManager.TotalOtherTime = t.TimeManager.TotalTime - t.TimeManager.TotalSIMDTime
+}
+
+// CalcSIMDCPIStack calculates the SIMD Specific CPI Stack
+func (t *InstTracer) CalcSIMDCPIStack() {
+	t.SIMDCPIStackValues.SIMDCPI =
+		(t.TimeManager.TotalTime * float64(1000000000)) / float64(t.CountManager.SIMDInstCount)
+	t.SIMDCPIStackValues.BranchStack =
+		(t.TimeManager.TotalBranchTime / t.TimeManager.TotalTime) * t.SIMDCPIStackValues.SIMDCPI
+	t.SIMDCPIStackValues.ScalarStack =
+		(t.TimeManager.TotalScalarTime / t.TimeManager.TotalTime) * t.SIMDCPIStackValues.SIMDCPI
+	t.SIMDCPIStackValues.LDSStack =
+		(t.TimeManager.TotalLDSTime / t.TimeManager.TotalTime) * t.SIMDCPIStackValues.SIMDCPI
+}
+
+// CalcTotalCPIStack calculates the CPI Stack for all instructions
+func (t *InstTracer) CalcTotalCPIStack() {
+	t.AllInstCPIStackValues.AllInstCPI =
+		(t.TimeManager.TotalTime * float64(1000000000)) / float64(t.CountManager.TotalInstCount)
+	t.AllInstCPIStackValues.SIMDStack =
+		(t.TimeManager.TotalSIMDTime / t.TimeManager.TotalTime) * t.AllInstCPIStackValues.AllInstCPI
+	t.AllInstCPIStackValues.BranchStack =
+		(t.TimeManager.TotalBranchTime / t.TimeManager.TotalTime) * t.AllInstCPIStackValues.AllInstCPI
+	t.AllInstCPIStackValues.ScalarStack =
+		(t.TimeManager.TotalScalarTime / t.TimeManager.TotalTime) * t.AllInstCPIStackValues.AllInstCPI
+	t.AllInstCPIStackValues.LDSStack =
+		(t.TimeManager.TotalLDSTime / t.TimeManager.TotalTime) * t.AllInstCPIStackValues.AllInstCPI
 }
