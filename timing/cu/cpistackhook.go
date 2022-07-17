920492d7abb296bf66f42b795a2690cd96507192
Yifan Sun <sunyifan112358@gmail.com>
2022-07-17 12:53:29 -0400

    CU state

--- timing/cu/cpistackhook.go
+++ timing/cu/cpistackhook.go
@@ -10,14 +10,33 @@ import (
 
 // A CPIStackInstHook is a hook to the CU that captures what instructions are
 // issued in each cycle.
+//
+// The hook keep track of the state of the wavefronts. The state can be one of
+// the following:
+// - "idle": the wavefront is not doing anything
+// - "fetch": the wavefront is fetching an instruction
+// - "scalar-mem": the wavefront is fetching an instruction and is waiting for
+//  the scalar memory to be ready
+// - "vector-mem": the wavefront is fetching an instruction and is waiting for
+// the vector memory to be ready
+// - "lds": the wavefront is fetching an instruction and is waiting for the LDS
+// to be ready
+// - "scalar": the wavefront is executing a scalar instruction
+// - "vector": the wavefront is executing a vector instruction
 type CPIStackInstHook struct {
 	timeTeller sim.TimeTeller
 	cu         *ComputeUnit
 
+	state          string
 	inflightWfs    map[string]tracing.Task
 	firstWFStarted bool
 	firstWFStart   float64
 	lastWFEnd      float64
+	timeStack      map[string]float64
+	lastTaskTime   float64
+
+	simdInstCount uint64
+	allInstCount  uint64
 }
 
 // NewCPIStackInstHook creates a CPIStackInstHook object.
@@ -26,7 +45,10 @@ func NewCPIStackInstHook(cu *ComputeUnit, timeTeller sim.TimeTeller) *CPIStackIn
 		cu:         cu,
 		timeTeller: timeTeller,
 
+		state: "idle",
+
 		inflightWfs: make(map[string]tracing.Task),
+		timeStack:   make(map[string]float64),
 	}
 
 	atexit.Register(func() {
@@ -46,6 +68,16 @@ func (h *CPIStackInstHook) Report() {
 
 	fmt.Printf("%s, total_time, %.10f\n",
 		h.cu.Name(), h.lastWFEnd-h.firstWFStart)
+
+	fmt.Printf("%s, total_inst_count, %d\n",
+		h.cu.Name(), h.allInstCount)
+	fmt.Printf("%s, simd_inst_count, %d\n",
+		h.cu.Name(), h.simdInstCount)
+
+	for state, time := range h.timeStack {
+		fmt.Printf("%s, %s_time, %.10f\n",
+			h.cu.Name(), state, time)
+	}
 }
 
 // Func records issued instructions.
@@ -76,6 +108,11 @@ func (h *CPIStackInstHook) Func(ctx sim.HookCtx) {
 }
 
 func (h *CPIStackInstHook) handleTaskStart(task tracing.Task) {
+	defer func() { h.lastTaskTime = float64(h.timeTeller.CurrentTime()) }()
+
+	fmt.Printf("%.10f, %s, %s-%s\n",
+		h.timeTeller.CurrentTime(), h.cu.Name(), task.Kind, task.What)
+
 	switch task.Kind {
 	case "wavefront":
 		h.inflightWfs[task.ID] = task
@@ -83,7 +120,40 @@ func (h *CPIStackInstHook) handleTaskStart(task tracing.Task) {
 			h.firstWFStarted = true
 			h.firstWFStart = float64(h.timeTeller.CurrentTime())
 		}
+	case "inst":
+		h.handleInstStart(task)
+	case "fetch":
+		h.handleFetchStart(task)
 	}
+}
+
+func (h *CPIStackInstHook) handleInstStart(task tracing.Task) {
+	h.allInstCount++
+
+	if task.What == "VALU" {
+		h.simdInstCount++
+	}
+}
+
+func (h *CPIStackInstHook) handleFetchStart(task tracing.Task) {
+	switch h.state {
+	case "idle":
+		h.state = "fetch"
+		h.addStackTime("idle", h.timeDiff())
+	}
+}
+
+func (h *CPIStackInstHook) timeDiff() float64 {
+	return float64(h.timeTeller.CurrentTime()) - h.lastTaskTime
+}
+
+func (h *CPIStackInstHook) addStackTime(state string, time float64) {
+	_, ok := h.timeStack[state]
+	if !ok {
+		h.timeStack[state] = 0
+	}
+
+	h.timeStack[state] += time
 }
 
 func (h *CPIStackInstHook) handleTaskEnd(task tracing.Task) {
