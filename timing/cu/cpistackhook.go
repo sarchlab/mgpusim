ccc779c969718032f2f712248b0ab87aa891cc2a
zohapeterson <zohapeterson>
2022-07-19 15:52:53 +0000

    Modified CU CPI hook for more functionality

--- timing/cu/cpistackhook.go
+++ timing/cu/cpistackhook.go
@@ -1,8 +1,6 @@
 package cu
 
 import (
-	"fmt"
-
 	"github.com/tebeka/atexit"
 	"gitlab.com/akita/akita/v3/sim"
 	"gitlab.com/akita/akita/v3/tracing"
@@ -27,28 +25,55 @@ type CPIStackInstHook struct {
 	timeTeller sim.TimeTeller
 	cu         *ComputeUnit
 
-	state          string
-	inflightWfs    map[string]tracing.Task
-	firstWFStarted bool
-	firstWFStart   float64
-	lastWFEnd      float64
-	timeStack      map[string]float64
-	lastTaskTime   float64
+	state             string
+	inflightWfs       map[string]tracing.Task
+	firstWFStarted    bool
+	firstWFStart      float64
+	lastWFEnd         float64
+	timeStack         map[string]float64
+	lastTaskTime      float64
+	greatestTask      string
+	totalInFlightInst uint64
 
-	simdInstCount uint64
-	allInstCount  uint64
+	simdInstCount         uint64
+	allInstCount          uint64
+	inFlightInstBreakdown map[string]uint64
+	instChangeBreakdown   map[string]float64
+
+	taskHierarchy map[string]uint64
 }
 
 // NewCPIStackInstHook creates a CPIStackInstHook object.
 func NewCPIStackInstHook(cu *ComputeUnit, timeTeller sim.TimeTeller) *CPIStackInstHook {
 	h := &CPIStackInstHook{
-		cu:         cu,
 		timeTeller: timeTeller,
+		cu:         cu,
 
-		state: "idle",
+		state:        "idle",
+		greatestTask: "idle",
 
-		inflightWfs: make(map[string]tracing.Task),
-		timeStack:   make(map[string]float64),
+		inflightWfs:           make(map[string]tracing.Task),
+		timeStack:             make(map[string]float64),
+		inFlightInstBreakdown: map[string]uint64{},
+
+		instChangeBreakdown: map[string]float64{
+			"case0": 0,
+			"case1": 0,
+			"case2": 0,
+			"case3": 0,
+			"case4": 0,
+			"case5": 0,
+			"case6": 0,
+		},
+		taskHierarchy: map[string]uint64{
+			"idle":    0,
+			"fetch":   1,
+			"Special": 2,
+			"VMem":    3,
+			"LDS":     4,
+			"Scalar":  5,
+			"VALU":    6,
+		},
 	}
 
 	atexit.Register(func() {
@@ -66,18 +91,18 @@ func (h *CPIStackInstHook) Report() {
 		return
 	}
 
-	fmt.Printf("%s, total_time, %.10f\n",
-		h.cu.Name(), h.lastWFEnd-h.firstWFStart)
+	// fmt.Printf("%s, total_time, %.10f\n",
+	// 	h.cu.Name(), h.lastWFEnd-h.firstWFStart)
 
-	fmt.Printf("%s, total_inst_count, %d\n",
-		h.cu.Name(), h.allInstCount)
-	fmt.Printf("%s, simd_inst_count, %d\n",
-		h.cu.Name(), h.simdInstCount)
+	// fmt.Printf("%s, total_inst_count, %d\n",
+	// 	h.cu.Name(), h.allInstCount)
+	// fmt.Printf("%s, simd_inst_count, %d\n",
+	// 	h.cu.Name(), h.simdInstCount)
 
-	for state, time := range h.timeStack {
-		fmt.Printf("%s, %s_time, %.10f\n",
-			h.cu.Name(), state, time)
-	}
+	// for state, time := range h.timeStack {
+	// 	fmt.Printf("%s, %s_time, %.10f\n",
+	// 		h.cu.Name(), state, time)
+	// }
 }
 
 // Func records issued instructions.
@@ -109,8 +134,10 @@ func (h *CPIStackInstHook) Func(ctx sim.HookCtx) {
 func (h *CPIStackInstHook) handleTaskStart(task tracing.Task) {
 	defer func() { h.lastTaskTime = float64(h.timeTeller.CurrentTime()) }()
 
-	fmt.Printf("%.10f, %s, %s-%s\n",
-		h.timeTeller.CurrentTime(), h.cu.Name(), task.Kind, task.What)
+	// fmt.Printf("%.10f, %s, %s-%s\n",
+	// h.timeTeller.CurrentTime(), h.cu.Name(), task.Kind, task.What)
+
+	h.updateInFlightInst(true, task)
 
 	switch task.Kind {
 	case "wavefront":
@@ -155,8 +182,23 @@ func (h *CPIStackInstHook) addStackTime(state string, time float64) {
 	h.timeStack[state] += time
 }
 
+func (h *CPIStackInstHook) updateInFlightInst(beginning bool, task tracing.Task) {
+	if task.What != "*mem.ReadReq" && task.What != "*mem.WriteReq" && task.What != "*protocol.MapWGReq" && task.What != "wavefront" {
+		if beginning {
+			h.inFlightInstBreakdown[task.What]++
+			h.totalInFlightInst++
+		} else {
+			h.inFlightInstBreakdown[task.What]--
+			h.totalInFlightInst--
+		}
+	}
+}
+
 func (h *CPIStackInstHook) handleTaskEnd(task tracing.Task) {
 	_, ok := h.inflightWfs[task.ID]
+
+	h.updateInFlightInst(false, task)
+
 	if ok {
 		delete(h.inflightWfs, task.ID)
 		h.lastWFEnd = float64(h.timeTeller.CurrentTime())
