34d8d0705d7a2578a2ffd2ac9f378d98ecb66f59
Yifan Sun <sunyifan112358@gmail.com>
2022-12-19 22:26:28 -0500

    Convert CPI Hook to a Tracer

--- timing/cu/cubuilder.go
+++ timing/cu/cubuilder.go
@@ -111,9 +111,6 @@ func (b *Builder) Build(name string) *ComputeUnit {
 	b.equipVectorMemoryUnit(cu)
 	b.equipRegisterFiles(cu)
 
-	cpiStackInstHook := NewCPIStackInstHook(cu, b.engine)
-	cu.AcceptHook(cpiStackInstHook)
-
 	return cu
 }
 
