d4a3161c28ff33b8a1a1e14e86ff0c875521f6de
Yifan Sun <sunyifan112358@gmail.com>
2023-12-06 09:21:24 -0500

    Include accelsim_tracing in linting

--- acceclsim_tracing/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/sarchlab/mgpusim/accelsim_tracing
-
-go 1.16
-
-require github.com/davecgh/go-spew v1.1.1
---
+++ accelsim_tracing/trace/kernel.go
@@ -0,0 +1,26 @@
+package trace
+
+import (
+	"path"
+
+	"github.com/sarchlab/mgpusim/v3/accelsim_tracing/gpu"
+)
+
+type kernel struct { // trace execs interface
+	parent *Trace
+
+	rawText    string
+	filePath   string
+	traceGroup *traceGroup
+}
+
+func (te *kernel) Type() string {
+	return "kernel"
+}
+
+func (te *kernel) Execute(gpu *gpu.GPU) error {
+	tg := NewTraceGroup().WithFilePath(path.Join(te.parent.traceDirPath, te.filePath))
+	tg.Build()
+	err := tg.Exec(gpu)
+	return err
+}
