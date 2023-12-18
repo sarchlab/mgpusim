e0717d9aab967c5ffefd15575e74a23351ebd226
Yifan Sun <sunyifan112358@gmail.com>
2023-12-18 10:25:54 -0500

    Fix unit test

--- timing/cu/issuearbiter.go
+++ timing/cu/issuearbiter.go
@@ -46,33 +46,6 @@ func (a *IssueArbiter) Arbitrate(
 		}
 	}
 
-	// for len(wfToIssue) == 0 {
-	// 	a.moveToNextSIMD(wfPools)
-	// 	for len(wfPools[a.lastSIMDID].wfs) == 0 {
-	// 		if a.lastSIMDID == originalSIMDID {
-	// 			break
-	// 		}
-	// 		a.moveToNextSIMD(wfPools)
-	// 	}
-
-	// 	typeMask := make([]bool, 7)
-	// 	wfPool := wfPools[a.lastSIMDID]
-	// 	for _, wf := range wfPool.wfs {
-	// 		if wf.State != wavefront.WfReady || wf.InstToIssue == nil {
-	// 			continue
-	// 		}
-
-	// 		if typeMask[wf.InstToIssue.ExeUnit] == false {
-	// 			wfToIssue = append(wfToIssue, wf)
-	// 			typeMask[wf.InstToIssue.ExeUnit] = true
-	// 		}
-	// 	}
-
-	// 	if a.lastSIMDID == originalSIMDID {
-	// 		break
-	// 	}
-	// }
-
 	return wfToIssue
 }
 
