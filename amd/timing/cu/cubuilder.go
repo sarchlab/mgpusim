968a2618cbed53cf8582ad14402ff43b0ee49bf5
Sabila Al Jannat <sajannat@wm.edu>
2025-07-14 13:17:37 -0400

    yuzawa_single_core

--- amd/timing/cu/cubuilder.go
+++ amd/timing/cu/cubuilder.go
@@ -29,7 +29,7 @@ type Builder struct {
 }
 
 // MakeBuilder returns a default builder object
-func MakeBuilder() Builder {
+func MakeBuilder() *Builder {
 	var b Builder
 	b.freq = 1000 * sim.MHz
 	b.simdCount = 4
@@ -37,29 +37,29 @@ func MakeBuilder() Builder {
 	b.vgprCount = []int{16384, 16384, 16384, 16384}
 	b.log2CachelineSize = 6
 
-	return b
+	return &b
 }
 
 // WithEngine sets the engine to use.
-func (b Builder) WithEngine(engine sim.Engine) Builder {
+func (b *Builder) WithEngine(engine sim.Engine) *Builder {
 	b.engine = engine
 	return b
 }
 
 // WithFreq sets the frequency.
-func (b Builder) WithFreq(f sim.Freq) Builder {
+func (b *Builder) WithFreq(f sim.Freq) *Builder {
 	b.freq = f
 	return b
 }
 
 // WithSIMDCount sets the number of SIMD unit in the ComputeUnit.
-func (b Builder) WithSIMDCount(n int) Builder {
+func (b *Builder) WithSIMDCount(n int) *Builder {
 	b.simdCount = n
 	return b
 }
 
 // WithVGPRCount sets the number of VGPRs associated with each SIMD Unit.
-func (b Builder) WithVGPRCount(counts []int) Builder {
+func (b *Builder) WithVGPRCount(counts []int) *Builder {
 	if len(counts) != b.simdCount {
 		panic("counts must have a length that equals to the SIMD count")
 	}
@@ -69,19 +69,19 @@ func (b Builder) WithVGPRCount(counts []int) Builder {
 }
 
 // WithSGPRCount equals the number of SGPRs in the Compute Unit.
-func (b Builder) WithSGPRCount(count int) Builder {
+func (b *Builder) WithSGPRCount(count int) *Builder {
 	b.sgprCount = count
 	return b
 }
 
 // WithLog2CachelineSize sets the cacheline size as a power of 2.
-func (b Builder) WithLog2CachelineSize(n uint64) Builder {
+func (b *Builder) WithLog2CachelineSize(n uint64) *Builder {
 	b.log2CachelineSize = n
 	return b
 }
 
 // WithVisTracer adds a tracer to the builder.
-func (b Builder) WithVisTracer(t tracing.Tracer) Builder {
+func (b *Builder) WithVisTracer(t tracing.Tracer) *Builder {
 	b.enableVisTracing = true
 	b.visTracer = t
 	return b
