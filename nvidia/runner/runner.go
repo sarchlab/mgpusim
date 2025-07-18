af54950561abcf3cf34f2ddba2d7d92bcc4025e1
EnzeXu <xezpku@gmail.com>
2025-07-18 08:28:01 -0400

    test: trace support for debug

--- nvidia/runner/runner.go
+++ nvidia/runner/runner.go
@@ -2,6 +2,8 @@ package runner
 
 import (
 	"github.com/sarchlab/akita/v4/sim"
+	"github.com/sarchlab/akita/v4/simulation"
+	"github.com/sarchlab/akita/v4/tracing"
 	"github.com/sarchlab/mgpusim/v4/nvidia/benchmark"
 	"github.com/sarchlab/mgpusim/v4/nvidia/driver"
 	"github.com/sarchlab/mgpusim/v4/nvidia/platform"
@@ -10,6 +12,7 @@ import (
 type Runner struct {
 	platform   *platform.Platform
 	benchmarks []*benchmark.Benchmark
+	simulation *simulation.Simulation
 }
 
 func (r *Runner) AddBenchmark(benchmark *benchmark.Benchmark) {
@@ -17,15 +20,22 @@ func (r *Runner) AddBenchmark(benchmark *benchmark.Benchmark) {
 }
 
 func (r *Runner) Run() {
+	// simulationBuilder := simulation.MakeBuilder()
+	// r.simulation = simulationBuilder.Build()
+	// tracing.StartTask(r.simulation.ID(), "", )
+
 	for _, benchmark := range r.benchmarks {
 		execs := benchmark.TraceExecs
 		for _, exec := range execs {
 			exec.Run(r.Driver())
 		}
 	}
-
+	r.configureVisTracing()
+	r.Driver().LogSimulationStart()
 	r.Driver().TickLater()
 	r.Engine().Run()
+	r.Driver().LogSimulationTerminate()
+	r.simulation.Terminate()
 	// 	r.Engine().Finished()
 }
 
@@ -35,4 +45,11 @@ func (r *Runner) Driver() *driver.Driver {
 
 func (r *Runner) Engine() sim.Engine {
 	return r.platform.Engine
+}
+
+func (r *Runner) configureVisTracing() {
+	visTracer := r.simulation.GetVisTracer()
+	for _, comp := range r.simulation.Components() {
+		tracing.CollectTrace(comp.(tracing.NamedHookable), visTracer)
+	}
 }
