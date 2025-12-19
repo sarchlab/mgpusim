20afbf817bfd91be82ad4c87176c2a45e7728ab8
EnzeXu <xezpku@gmail.com>
2025-12-19 01:44:27 -0500

    test: rerun: rollback to 20/100/400

--- nvidia/sm/builder.go
+++ nvidia/sm/builder.go
@@ -222,7 +222,7 @@ func (b *SMBuilder) buildL1VCaches() {
 		builder := writearound.MakeBuilder().
 			WithEngine(b.engine).
 			WithFreq(b.freq).
-			WithBankLatency(40). // cycle was 60
+			WithBankLatency(20). // cycle was 20 40
 			WithNumBanks(1).
 			WithLog2BlockSize(b.log2CacheLineSize).
 			WithWayAssociativity(4).
